ab1e6e9f61609b7e3688ed7efd15e4cb62ae0f59
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-16 08:16:20 +0000

    Drop 0.1.0 code before commting 0.2.0

--- plugins/egf.releng/build_egf.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set JAVA_HOME=D:\jdk\jdk1.5.0
-set view=D:\dev\eclipse\workspace-3.5
-call %view%/egf.releng/build.bat D:\eclipse-3.5-M5 %view%/egf.builder D:/prod_egf/builds %view% D:/prod_egf/org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar %view%/egf.releng/build.xml D:/prod_egf/testhost
