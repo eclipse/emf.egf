58ba7ce53f3e8519474cdd292b0fdfff301a74e7
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-23 13:53:13 +0000

    added: new log Methods

--- org.eclipse.egf.common/src/org/eclipse/egf/common/activator/EGFAbstractPlugin.java
+++ org.eclipse.egf.common/src/org/eclipse/egf/common/activator/EGFAbstractPlugin.java
@@ -1,4 +1,6 @@
-/*******************************************************************************
+/**
+ * <copyright>
+ *
  *  Copyright (c) 2009 Thales Corporate Services S.A.S.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
@@ -7,110 +9,75 @@
  * 
  *  Contributors:
  *      Thales Corporate Services S.A.S - initial API and implementation
- *******************************************************************************/
+ * 
+ * </copyright>
+ */
+
 package org.eclipse.egf.common.activator;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.osgi.util.NLS;
-
-import org.eclipse.egf.common.constant.ICommonConstants;
-import org.eclipse.egf.common.helper.StatusHelper;
 
 /**
- * @author fournier
+ * Adds logging methods
+ * 
+ * @author Guiu
+ * 
  */
 public abstract class EGFAbstractPlugin extends Plugin {
-  
-  public void log(Throwable t) {
-    log(newErrorStatus(t.getMessage(), t));
-  }
 
-  /**
-   * Returns a new error status for this plug-in with the given message
-   * @param message the message to be included in the status
-   * @param exception the exception to be included in the status or <code>null</code> if none
-   * @return a new error status
-   */
-  public IStatus newErrorStatus(String message, Throwable exception) {
-    return new Status(IStatus.ERROR, getPluginID(), ICommonConstants.INTERNAL_ERROR, message, exception);
-  }
-
-  /**
-   * Logs the given message to the platform log.
-   * 
-   * If you have an exception in hand, call log(String, Throwable) instead.
-   * 
-   * If you have a status object in hand call log(String, IStatus) instead.
-   * 
-   * @param message A high level UI message describing when the problem happened.
-   */
-  public void log(String message) {
-    getLog().log(StatusHelper.newStatus(getPluginID(), IStatus.ERROR, message, null));
-  }
-
-  /**
-   * Logs the given message and throwable to the platform log.
-   * 
-   * If you have a status object in hand call log(String, IStatus) instead.
-   * 
-   * 
-   * @param message A high level UI message describing when the problem happened.
-   * @param t The throwable from where the problem actually occurred.
-   */
-  public void log(String message, Throwable t) {
-    log(message, StatusHelper.newStatus(getPluginID(), IStatus.ERROR, message, t));
-  }
-
-  /**
-   * Logs the given throwable to the platform log, indicating the class and method from where it is being logged (this is not necessarily where it occurred).
-   * 
-   * @param clazz The calling class.
-   * @param methodName The calling method name.
-   * @param t The throwable from where the problem actually occurred.
-   */
-  public void log(Class<?> clazz, String methodName, Throwable t) {
-    log(NLS.bind("Exception in {0}.{1}: {2}", new Object []{ clazz.getName(), methodName, t }), t); //$NON-NLS-1$
-  }
-
-  /**
-   * Logs the given message and status to the platform log.
-   * 
-   * @param status The status describing the problem. Must not be null.
-   */
-  public void log(IStatus status) {
-    log(null, status);
-  }
-
-  /**
-   * Log the given exception along with the provided message and severity indicator
-   */
-  public void log(int severity, String message, Throwable e) {
-    log(new Status(severity, getPluginID(), 0, message, e));
-  }
-
-  /**
-   * Logs the given message and status to the platform log.
-   * 
-   * @param message A high level UI message describing when the problem happened. May be <code>null</code>.
-   * @param status The status describing the problem. Must not be null.
-   */
-  public void log(String message, IStatus status) {
-    // 1FTUHE0: ITPCORE:ALL - API - Status & logging - loss of semantic info
-    if (message != null) {
-      getLog().log(StatusHelper.newStatus(getPluginID(), IStatus.ERROR, message, null));
+    public void log(IStatus status) {
+        getLog().log(status);
     }
-    getLog().log(status);
-  }   
-    
-  /**
-   * Get the plug-in ID according to MANIFEST.MF definition.
-   * 
-   * @return a String containing the plug-in ID.
-   */
-  public String getPluginID() {
-    return getBundle().getSymbolicName();
-  }
-    
+
+	public void log(String message, Throwable t) {
+		getLog().log(newStatus(IStatus.INFO, message, t));
+	}
+
+	public void log(Throwable t) {
+		getLog().log(newStatus(IStatus.INFO, t.getMessage(), t));
+	}
+
+	public void log(String message) {
+		getLog().log(newStatus(IStatus.INFO, message, null));
+	}
+
+	public void logError(String message, Throwable t) {
+		getLog().log(newStatus(IStatus.ERROR, message, t));
+	}
+
+	public void logError(Throwable t) {
+		getLog().log(newStatus(IStatus.ERROR, t.getMessage(), t));
+	}
+
+	public void logError(String message) {
+		getLog().log(newStatus(IStatus.ERROR, message, null));
+	}
+
+	public void logWarning(String message, Throwable t) {
+		getLog().log(newStatus(IStatus.WARNING, message, t));
+	}
+
+	public void logWarning(Throwable t) {
+		getLog().log(newStatus(IStatus.WARNING, t.getMessage(), t));
+	}
+
+	public void logWarning(String message) {
+		getLog().log(newStatus(IStatus.WARNING, message, null));
+	}
+
+	private IStatus newStatus(int severity, String message, Throwable exception) {
+		return new Status(severity, getPluginID(), 0, message, exception);
+	}
+
+	/**
+	 * Get the plug-in ID according to MANIFEST.MF definition.
+	 * 
+	 * @return a String containing the plug-in ID.
+	 */
+	public String getPluginID() {
+		return getBundle().getSymbolicName();
+	}
+
 }
--- org.eclipse.egf.common/src/org/eclipse/egf/common/activator/EGFCommonPlugin.java
+++ org.eclipse.egf.common/src/org/eclipse/egf/common/activator/EGFCommonPlugin.java
@@ -16,78 +16,74 @@ import org.eclipse.egf.common.helper.IUserEnforcedHelper;
 import org.eclipse.egf.common.internal.helper.DefaultUserHelper;
 import org.osgi.framework.BundleContext;
 
-
 /**
  * Common plug-in activator.
+ * 
  * @author Guillaume Brocard
  */
 public class EGFCommonPlugin extends EGFAbstractPlugin {
-  
-  /**
-   * Extension point "user_helper" short id.
-   */
-  protected static final String EXTENSION_POINT_SHORT_ID_USER_HELPER = "user_helper"; //$NON-NLS-1$
-  
-  /**
-   * Shared instance.
-   */
-  private static EGFCommonPlugin __plugin;  
-    
-  /**
-   * User enforced helper.
-   */
-  private IUserEnforcedHelper _userHelper;  
-      
-  /**
-   * Get shared instance.
-   * @return
-   */
-  public static EGFCommonPlugin getDefault() {
-    return __plugin;
-  }
 
-  @Override
-  public void start(BundleContext context_p) throws Exception {
-    super.start(context_p);
-    __plugin = this;
-  }
+    /**
+     * Extension point "user_helper" short id.
+     */
+    protected static final String EXTENSION_POINT_SHORT_ID_USER_HELPER = "user_helper"; //$NON-NLS-1$
 
-  @Override
-  public void stop(BundleContext context_p) throws Exception {
-    __plugin = null;
-    super.stop(context_p);
-  }
-  
-  /**
-   * Get user enforced helper unique implementation.
-   * @return null if none could be found.
-   */
-  public IUserEnforcedHelper getUserEnforcedHelper() {
-    // Lazy loading. Search for the implementation.
-    if (_userHelper == null) {
-      // Get extensions abiding to user helper extension point.
-      IConfigurationElement[] configurationElements = ExtensionPointHelper.getConfigurationElements(
-        getPluginID(), 
-        EXTENSION_POINT_SHORT_ID_USER_HELPER
-      );
-      if (configurationElements != null && configurationElements.length > 0) {
-        // There should be one implementation only !
-        // So take the first one, as expected.
-        Object instantiatedClass = ExtensionPointHelper.createInstance(
-          configurationElements[0], 
-          ExtensionPointHelper.ATT_CLASS
-        );
-        // Make sure this is the correct resulting type.
-        if (instantiatedClass instanceof IUserEnforcedHelper) {
-          _userHelper = (IUserEnforcedHelper) instantiatedClass;
-        }
-      }
-      // Could not find any user helper, use default one.
-      if (_userHelper == null) {
-        _userHelper = new DefaultUserHelper();
-      }
+    /**
+     * Shared instance.
+     */
+    private static EGFCommonPlugin __plugin;
+
+    /**
+     * User enforced helper.
+     */
+    private IUserEnforcedHelper _userHelper;
+
+    /**
+     * Get shared instance.
+     * 
+     * @return
+     */
+    public static EGFCommonPlugin getDefault() {
+        return __plugin;
     }
-    return _userHelper;
-  }    
-  
+
+    @Override
+    public void start(BundleContext context_p) throws Exception {
+        super.start(context_p);
+        __plugin = this;
+    }
+
+    @Override
+    public void stop(BundleContext context_p) throws Exception {
+        __plugin = null;
+        super.stop(context_p);
+    }
+
+    /**
+     * Get user enforced helper unique implementation.
+     * 
+     * @return null if none could be found.
+     */
+    public IUserEnforcedHelper getUserEnforcedHelper() {
+        // Lazy loading. Search for the implementation.
+        if (_userHelper == null) {
+            // Get extensions abiding to user helper extension point.
+            IConfigurationElement[] configurationElements = ExtensionPointHelper.getConfigurationElements(getPluginID(), EXTENSION_POINT_SHORT_ID_USER_HELPER);
+            if (configurationElements != null && configurationElements.length > 0) {
+                // There should be one implementation only !
+                // So take the first one, as expected.
+                Object instantiatedClass = ExtensionPointHelper.createInstance(configurationElements[0], ExtensionPointHelper.ATT_CLASS);
+                // Make sure this is the correct resulting type.
+                if (instantiatedClass instanceof IUserEnforcedHelper) {
+                    _userHelper = (IUserEnforcedHelper) instantiatedClass;
+                }
+            }
+            // Could not find any user helper, use default one.
+            if (_userHelper == null) {
+                _userHelper = new DefaultUserHelper();
+            }
+        }
+        return _userHelper;
+    }
+
 }
