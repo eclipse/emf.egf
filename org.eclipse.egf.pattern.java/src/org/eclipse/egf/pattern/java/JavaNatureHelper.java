c5f0bbf22faa8556fa0529fed1086a23fa9a7b71
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-24 15:55:55 +0000

    PlatformManager update.

--- org.eclipse.egf.pattern.java/src/org/eclipse/egf/pattern/java/JavaNatureHelper.java
+++ org.eclipse.egf.pattern.java/src/org/eclipse/egf/pattern/java/JavaNatureHelper.java
@@ -36,7 +36,7 @@ public class JavaNatureHelper {
      */
     public static String getClassName(Pattern pattern) throws PatternException {
         try {
-            String content = FileHelper_to_be_upgraded.getContent(PatternHelper.getPlatformFactoryComponent(pattern), pattern.getHeaderMethod().getPatternFilePath());
+            String content = FileHelper_to_be_upgraded.getContent(PatternHelper.getPlatformFcore(pattern), pattern.getHeaderMethod().getPatternFilePath());
             Matcher matcher = compile.matcher(content);
             if (matcher.matches()) {
                 String pack = matcher.group(1);
