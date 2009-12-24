90e9e40228a41a429e3322dc10cdeae9d90ad236
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-24 15:44:45 +0000

    PlatformManager update.

--- org.eclipse.egf.core.platform/src/org/eclipse/egf/core/platform/resource/IResourceFactoryComponentListener.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.egf.core.platform.resource;
-
-public interface IResourceFactoryComponentListener {
-	
-  public void factoryComponentChanged(IResourceFactoryComponentDelta delta);
-
-}
