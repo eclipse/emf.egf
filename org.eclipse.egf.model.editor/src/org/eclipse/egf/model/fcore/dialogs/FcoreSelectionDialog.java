143ee1b21423566e0bd642a0671043e27e89f103
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-24 15:57:16 +0000

    PlatformManager update.

--- org.eclipse.egf.model.editor/src/org/eclipse/egf/model/fcore/dialogs/FcoreSelectionDialog.java
+++ org.eclipse.egf.model.editor/src/org/eclipse/egf/model/fcore/dialogs/FcoreSelectionDialog.java
@@ -1,14 +1,14 @@
 /**
- *  Copyright (c) 2009 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  */
-package org.eclipse.egf.model.factorycomponent.dialogs;
+package org.eclipse.egf.model.fcore.dialogs;
 
 import java.util.Comparator;
 
@@ -16,8 +16,8 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.egf.core.platform.EGFPlatformPlugin;
-import org.eclipse.egf.core.platform.pde.IPlatformFactoryComponent;
+import org.eclipse.egf.core.EGFCorePlugin;
+import org.eclipse.egf.core.fcore.IPlatformFcore;
 import org.eclipse.egf.model.edit.EGFModelsEditPlugin;
 import org.eclipse.egf.model.editor.EGFModelsEditorPlugin;
 import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
@@ -34,25 +34,28 @@ import org.eclipse.ui.dialogs.FilteredItemsSelectionDialog;
  * @author Xavier Maysonnave
  * 
  */
-public class FactoryComponentSelectionDialog extends FilteredItemsSelectionDialog {
+public class FcoreSelectionDialog extends FilteredItemsSelectionDialog {
 
-  private static final String DIALOG_SETTINGS = "org.eclipse.egf.model.factorycomponent.presentation.FactoryComponentSelectionDialog"; //$NON-NLS-1$
+  private static final String DIALOG_SETTINGS = "org.eclipse.egf.model.fcore.presentation.FcoreSelectionDialog"; //$NON-NLS-1$
 
-  private class FactoryComponentSearchItemsFilter extends ItemsFilter {
+  private class FcoreSearchItemsFilter extends ItemsFilter {
+    @Override
     public boolean isConsistentItem(Object item) {
       return true;
     }
 
+    @Override
     public boolean matchItem(Object item) {
       String id = null;
-      if (item instanceof IPlatformFactoryComponent) {
-        IPlatformFactoryComponent model = (IPlatformFactoryComponent) item;
+      if (item instanceof IPlatformFcore) {
+        IPlatformFcore model = (IPlatformFcore) item;
         id = model.getURI().toString();
       }
 
       return (matches(id));
     }
 
+    @Override
     protected boolean matches(String text) {
       String pattern = patternMatcher.getPattern();
       if (pattern.indexOf("*") != 0 & pattern.indexOf("?") != 0 & pattern.indexOf(".") != 0) {//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
@@ -63,13 +66,13 @@ public class FactoryComponentSelectionDialog extends FilteredItemsSelectionDialo
     }
   }
 
-  private class FactoryComponentSearchComparator implements Comparator<IPlatformFactoryComponent> {
-    public int compare(IPlatformFactoryComponent fc1, IPlatformFactoryComponent fc2) {
+  private class FcoreSearchComparator implements Comparator<IPlatformFcore> {
+    public int compare(IPlatformFcore fc1, IPlatformFcore fc2) {
       return fc1.compareTo(fc2);
     }
   }
 
-  private IPlatformFactoryComponent[] _fcs;
+  private IPlatformFcore[] _fcores;
 
   private ILabelProvider _labelProvider = new LabelProvider() {
     @Override
@@ -79,10 +82,10 @@ public class FactoryComponentSelectionDialog extends FilteredItemsSelectionDialo
 
     @Override
     public String getText(Object element) {
-      if (element instanceof IPlatformFactoryComponent == false) {
+      if (element instanceof IPlatformFcore == false) {
         return super.getText(element);
       }
-      return ((IPlatformFactoryComponent) element).getURI().toString();
+      return ((IPlatformFcore) element).getURI().toString();
     }
   };
 
@@ -94,56 +97,60 @@ public class FactoryComponentSelectionDialog extends FilteredItemsSelectionDialo
 
     @Override
     public String getText(Object element) {
-      if (element instanceof IPlatformFactoryComponent == false) {
+      if (element instanceof IPlatformFcore == false) {
         return super.getText(element);
       }
       StringBuffer buffer = new StringBuffer();
-      IPlatformFactoryComponent fc = (IPlatformFactoryComponent) element;
-      if (fc.getPlatformPlugin().isTarget()) {
+      IPlatformFcore fc = (IPlatformFcore) element;
+      if (fc.getPlatformBundle().isTarget()) {
         buffer.append(" [Target]"); //$NON-NLS-1$
       } else {
         buffer.append(" [Workspace]"); //$NON-NLS-1$
       }
       buffer.append(" ["); //$NON-NLS-1$
-      buffer.append(fc.getPlatformPlugin().getLocation());
+      buffer.append(fc.getPlatformBundle().getBundleLocation());
       buffer.append("]"); //$NON-NLS-1$      
       return buffer.toString();
     }
   };
 
-  public FactoryComponentSelectionDialog(Shell parentShell, boolean multipleSelection) {
+  public FcoreSelectionDialog(Shell parentShell, boolean multipleSelection) {
     this(parentShell, getElements(), multipleSelection);
   }
 
-  public FactoryComponentSelectionDialog(Shell parentShell, IPlatformFactoryComponent[] fcs, boolean multipleSelection) {
+  public FcoreSelectionDialog(Shell parentShell, IPlatformFcore[] factoryComponents, boolean multipleSelection) {
     super(parentShell, multipleSelection);
-    _fcs = fcs;
-    setTitle(EGFModelsEditorPlugin.INSTANCE.getString("_UI_FactoryComponentSelection_label"));
-    setMessage(EGFModelsEditorPlugin.INSTANCE.getString("_UI_SelectRegisteredFactoryComponentURI"));
+    _fcores = factoryComponents;
+    setTitle(EGFModelsEditorPlugin.INSTANCE.getString("_UI_FcoreSelection_label")); //$NON-NLS-1$
+    setMessage(EGFModelsEditorPlugin.INSTANCE.getString("_UI_SelectRegisteredFcoreURI")); //$NON-NLS-1$
     setListLabelProvider(_labelProvider);
     setDetailsLabelProvider(_detailsLabelProvider);
   }
 
-  private static IPlatformFactoryComponent[] getElements() {
-    return EGFPlatformPlugin.getDefault().getFactoryComponents();
+  private static IPlatformFcore[] getElements() {
+    return EGFCorePlugin.getPlatformFcores();
   }
 
+  @Override
   protected Control createExtendedContentArea(Composite parent) {
     return null;
   }
 
+  @Override
   protected ItemsFilter createFilter() {
-    return new FactoryComponentSearchItemsFilter();
+    return new FcoreSearchItemsFilter();
   }
 
+  @Override
   protected void fillContentProvider(AbstractContentProvider contentProvider, ItemsFilter itemsFilter, IProgressMonitor progressMonitor) throws CoreException {
-    for (int i = 0; i < _fcs.length; i++) {
-      contentProvider.add(_fcs[i], itemsFilter);
+    for (int i = 0; i < _fcores.length; i++) {
+      contentProvider.add(_fcores[i], itemsFilter);
       progressMonitor.worked(1);
     }
     progressMonitor.done();
   }
 
+  @Override
   protected IDialogSettings getDialogSettings() {
     IDialogSettings settings = EGFModelsEditorPlugin.getPlugin().getDialogSettings().getSection(DIALOG_SETTINGS);
     if (settings == null) {
@@ -152,25 +159,21 @@ public class FactoryComponentSelectionDialog extends FilteredItemsSelectionDialo
     return settings;
   }
 
+  @Override
   public String getElementName(Object item) {
-    if (item instanceof IPlatformFactoryComponent) {
-      IPlatformFactoryComponent fc = (IPlatformFactoryComponent) item;
+    if (item instanceof IPlatformFcore) {
+      IPlatformFcore fc = (IPlatformFcore) item;
       return fc.getURI().toString();
     }
     return null;
   }
 
-  protected Comparator<IPlatformFactoryComponent> getItemsComparator() {
-    return new FactoryComponentSearchComparator();
+  @Override
+  protected Comparator<IPlatformFcore> getItemsComparator() {
+    return new FcoreSearchComparator();
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * org.eclipse.ui.dialogs.FilteredItemsSelectionDialog#validateItem(java.lang
-   * .Object)
-   */
+  @Override
   protected IStatus validateItem(Object item) {
     return new Status(IStatus.OK, EGFModelsEditorPlugin.getPlugin().getSymbolicName(), 0, "", null); //$NON-NLS-1$
   }
