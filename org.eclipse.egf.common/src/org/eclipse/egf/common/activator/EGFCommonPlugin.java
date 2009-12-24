267c6fec93f06c4e923c4c63897ef81a994d0016
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-24 15:38:41 +0000

    The static method getEGFLoggers() has been moved from EGFAbstractPlugin.

--- org.eclipse.egf.common/src/org/eclipse/egf/common/activator/EGFCommonPlugin.java
+++ org.eclipse.egf.common/src/org/eclipse/egf/common/activator/EGFCommonPlugin.java
@@ -10,10 +10,15 @@
  */
 package org.eclipse.egf.common.activator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.egf.common.helper.ExtensionPointHelper;
 import org.eclipse.egf.common.helper.IUserEnforcedHelper;
 import org.eclipse.egf.common.internal.helper.DefaultUserHelper;
+import org.eclipse.egf.common.log.IEGFLogger;
+import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -27,6 +32,11 @@ public class EGFCommonPlugin extends EGFAbstractPlugin {
    * Extension point "user_helper" short id.
    */
   protected static final String EXTENSION_POINT_SHORT_ID_USER_HELPER = "user_helper"; //$NON-NLS-1$
+
+  /**
+   * EGF Registered loggers.
+   */
+  private static List<IEGFLogger> _egfLoggers;
 
   /**
    * User enforced helper.
@@ -72,7 +82,7 @@ public class EGFCommonPlugin extends EGFAbstractPlugin {
       if (configurationElements != null && configurationElements.length > 0) {
         // There should be one implementation only !
         // So take the first one, as expected.
-        Object instantiatedClass = ExtensionPointHelper.createInstance(configurationElements[0], ExtensionPointHelper.ATT_CLASS);
+        Object instantiatedClass = ExtensionPointHelper.createInstance(configurationElements[0]);
         // Make sure this is the correct resulting type.
         if (instantiatedClass instanceof IUserEnforcedHelper) {
           _userHelper = (IUserEnforcedHelper) instantiatedClass;
@@ -84,6 +94,32 @@ public class EGFCommonPlugin extends EGFAbstractPlugin {
       }
     }
     return _userHelper;
+  }
+
+  /**
+   * Get egf loggers implementations.
+   * 
+   * @return an empty list if none could be found.
+   */
+  public static List<IEGFLogger> getEGFLoggers() {
+    // Lazy loading. Search for the implementation.
+    if (_egfLoggers == null) {
+      _egfLoggers = new ArrayList<IEGFLogger>();
+      // Get EGF logger extension points.
+      for (IConfigurationElement configurationElement : ExtensionPointHelper.getConfigurationElements(EGFCommonPlugin.getDefault().getPluginID(), EXTENSION_POINT_LOGGER)) {
+        Object object = ExtensionPointHelper.createInstance(configurationElement);
+        // Make sure this is the correct resulting type.
+        if (object instanceof IEGFLogger == false) {
+          getDefault().logError(NLS.bind("Wrong Class {0}", object.getClass().getName()));
+          getDefault().logInfo("This Class should be a sub-type of ''org.eclipse.egf.common.log.IEGFLogger''.", 1);
+          getDefault().logInfo(NLS.bind("Bundle ''{0}''", ExtensionPointHelper.getNamespace(configurationElement)), 1);
+          getDefault().logInfo(NLS.bind("Extension-point ''{0}''", configurationElement.getName()), 1);
+          continue;
+        }
+        _egfLoggers.add((IEGFLogger) object);
+      }
+    }
+    return _egfLoggers;
   }
 
 }
