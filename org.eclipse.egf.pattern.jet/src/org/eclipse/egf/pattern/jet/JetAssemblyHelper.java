910dd213a54fdc28cbe31f5bbee375fe01cf2b21
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-24 15:03:53 +0000

    java & jet translations resolve the extension id from the query object to find the class to use

--- org.eclipse.egf.pattern.jet/src/org/eclipse/egf/pattern/jet/JetAssemblyHelper.java
+++ org.eclipse.egf.pattern.jet/src/org/eclipse/egf/pattern/jet/JetAssemblyHelper.java
@@ -29,6 +29,7 @@ import org.eclipse.egf.pattern.ParameterMatcher;
 import org.eclipse.egf.pattern.PatternHelper;
 import org.eclipse.egf.pattern.execution.AssemblyHelper;
 import org.eclipse.egf.pattern.query.ParameterTypeHelper;
+import org.eclipse.egf.pattern.query.QueryManager;
 
 /**
  * @author Thomas Guiu
@@ -95,7 +96,7 @@ public class JetAssemblyHelper extends AssemblyHelper {
     }
 
     @Override
-    protected void beginOrchestration() {
+    protected void beginOrchestration() throws PatternException {
         content.append("<%").append("PatternContext ctx = (PatternContext)argument;").append(CharacterConstants.LINE_SEPARATOR).append("%>");
         super.beginOrchestration();
         content.append("<%").append(START_MARKER).append("%>");
@@ -106,7 +107,7 @@ public class JetAssemblyHelper extends AssemblyHelper {
      * add this stuff. TODO query is not supported yet.
      */
     @Override
-    protected void endOrchestration() {
+    protected void endOrchestration() throws PatternException {
         content.append("<%").append(END_MARKER).append("%>");
         if (pattern.getParameters().isEmpty())
             return;
@@ -141,7 +142,7 @@ public class JetAssemblyHelper extends AssemblyHelper {
         // 3- Add additional code for parameter names handling
         int startIndex = content.indexOf(START_MARKER);
         if (startIndex == -1)
-            throw new IllegalStateException(Messages.assembly_error2);
+            throw new PatternException(Messages.assembly_error2);
 
         localContent.setLength(0);
         localContent.append(CharacterConstants.LINE_SEPARATOR);
@@ -158,7 +159,7 @@ public class JetAssemblyHelper extends AssemblyHelper {
         return parameter.getName() + "List";
     }
 
-    private void appendQueryCode(StringBuilder localContent, PatternParameter parameter) {
+    private void appendQueryCode(StringBuilder localContent, PatternParameter parameter) throws PatternException {
         Query query = parameter.getQuery();
         localContent.append("Map<String, String> queryCtx = new HashMap<String, String>();").append(CharacterConstants.LINE_SEPARATOR);
         if (query.getQueryContext() != null) {
@@ -168,7 +169,7 @@ public class JetAssemblyHelper extends AssemblyHelper {
         }
 
         localContent.append("List<Object> ").append(getParameterListName(parameter)).append(" = ");
-        localContent.append("new ").append(query.getDelegateClass()).append("().executeQuery(queryCtx, ctx);").append(CharacterConstants.LINE_SEPARATOR);
+        localContent.append("new ").append(QueryManager.INSTANCE.getQueryManagerClassName(query.getExtensionId())).append("().executeQuery(queryCtx, ctx);").append(CharacterConstants.LINE_SEPARATOR);
     }
 
 }
