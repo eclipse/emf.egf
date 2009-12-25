2dea21431c4bc080b9dbd6dbe76fda1bc9c85703
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-25 21:29:58 +0000

    update task implementation and id attributes.

--- examples/workspace/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
+++ examples/workspace/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
@@ -1,6 +1,5 @@
 package org.eclipse.egf.example.task.h1;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.production.InvocationException;
 import org.eclipse.egf.core.production.context.IProductionContext;
@@ -8,15 +7,15 @@ import org.eclipse.egf.core.production.task.IProductionTask;
 
 public class H1 implements IProductionTask {
 
-  public void preExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void preExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     Activator.getDefault().logInfo("preExecute H1"); //$NON-NLS-1$
   }
 
-  public void doExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void doExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     Activator.getDefault().logInfo("doExecute H1"); //$NON-NLS-1$	  		  
   }
 
-  public void postExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void postExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     Activator.getDefault().logInfo("postExecute H1"); //$NON-NLS-1$	  		  
   }
 
