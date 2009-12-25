9c48d2ec1db8ecd575c44fc5e327a8bb3c42084d
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-25 20:34:10 +0000

    Fix the copyright header.

--- org.eclipse.egf.core.production/src/org/eclipse/egf/core/production/session/ProjectBundleSession.java
+++ org.eclipse.egf.core.production/src/org/eclipse/egf/core/production/session/ProjectBundleSession.java
@@ -1,14 +1,12 @@
 /**
- * Copyright (c) 2009 Obeo.
+ * Copyright (c) 2008 IBM Corporation, Obeo and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Obeo - initial API and implementation
- * Inspired from
- * org.eclipse.acceleo.common.internal.utils.workspace.AcceleoWorkspaceUtil
+ * IBM Corporation - initial API and implementation
  */
 package org.eclipse.egf.core.production.session;
 
