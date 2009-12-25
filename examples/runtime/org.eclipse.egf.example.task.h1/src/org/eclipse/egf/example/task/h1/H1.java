3c80aae214847ab28281bea62c7ca93097fe2524
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-25 21:30:38 +0000

    update task implementation and id attributes.

--- examples/runtime/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
+++ examples/runtime/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
@@ -1,6 +1,5 @@
 package org.eclipse.egf.example.task.h1;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.common.constant.IDiagnostic;
 import org.eclipse.egf.core.production.InvocationException;
@@ -9,15 +8,15 @@ import org.eclipse.egf.core.production.task.IProductionTask;
 
 public class H1 implements IProductionTask {
 
-  public void preExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void preExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     Activator.getDefault().logInfo("preExecute Modified H1"); //$NON-NLS-1$
   }
 
-  public void doExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void doExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     throw new InvocationException(IDiagnostic.ERROR, productionContext.getName(), "I'm a task who raised an exception"); //$NON-NLS-1$
   }
 
-  public void postExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void postExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     Activator.getDefault().logInfo("postExecute Modified H1"); //$NON-NLS-1$	  		  
   }
 
--- examples/runtime/org.eclipse.egf.example.task.h2/src/org/eclipse/egf/example/task/h2/H2.java
+++ examples/runtime/org.eclipse.egf.example.task.h2/src/org/eclipse/egf/example/task/h2/H2.java
@@ -1,6 +1,5 @@
 package org.eclipse.egf.example.task.h2;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.EGFCorePlugin;
 import org.eclipse.egf.core.production.InvocationException;
@@ -9,15 +8,15 @@ import org.eclipse.egf.core.production.task.IProductionTask;
 
 public class H2 implements IProductionTask {
 
-  public void preExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void preExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     EGFCorePlugin.getDefault().logInfo("preExecute H2"); //$NON-NLS-1$
   }
 
-  public void doExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void doExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     EGFCorePlugin.getDefault().logInfo("doExecute H2"); //$NON-NLS-1$	  		  
   }
 
-  public void postExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws CoreException, InvocationException {
+  public void postExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     EGFCorePlugin.getDefault().logInfo("postExecute H2"); //$NON-NLS-1$	  		  
   }
 
