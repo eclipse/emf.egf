91287adef47cefff5203ef9336b6777256a4040c
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-25 21:38:05 +0000

    InvocationException handling.

--- examples/runtime/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
+++ examples/runtime/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
@@ -1,7 +1,6 @@
 package org.eclipse.egf.example.task.h1;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.egf.common.constant.IDiagnostic;
 import org.eclipse.egf.core.production.InvocationException;
 import org.eclipse.egf.core.production.context.IProductionContext;
 import org.eclipse.egf.core.production.task.IProductionTask;
@@ -13,7 +12,7 @@ public class H1 implements IProductionTask {
   }
 
   public void doExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
-    throw new InvocationException(IDiagnostic.ERROR, productionContext.getName(), "I'm a task who raised an exception"); //$NON-NLS-1$
+    throw new InvocationException("I'm a task who raised an exception"); //$NON-NLS-1$
   }
 
   public void postExecute(final IProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
