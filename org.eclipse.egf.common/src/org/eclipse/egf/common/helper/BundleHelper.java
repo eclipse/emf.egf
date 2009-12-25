8d42f5a0468aa527f5127b428a669a08bbf7016c
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-25 21:46:16 +0000

    Improve exception handling.

--- org.eclipse.egf.common/src/org/eclipse/egf/common/helper/BundleHelper.java
+++ org.eclipse.egf.common/src/org/eclipse/egf/common/helper/BundleHelper.java
@@ -1,12 +1,12 @@
 /**
- *  Copyright (c) 2009 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  */
 package org.eclipse.egf.common.helper;
 
@@ -34,21 +34,17 @@ public class BundleHelper {
    *          which can load given class name.
    * @return an instance of given class name or null if instantiation failed.
    */
-  public static Object instantiate(String fullyQualifiedClassName_p, Bundle bundle_p) {
+  public static Object instantiate(String fullyQualifiedClassName_p, Bundle bundle_p) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
     Object result = null;
     // Precondition.
     if (bundle_p == null) {
       return result;
     }
-    try {
-      // Try loading a class according to the class name.
-      Class<?> clazz = loadClass(fullyQualifiedClassName_p, bundle_p);
-      // Try instantiating an object of loaded class.
-      if (clazz != null) {
-        result = clazz.newInstance();
-      }
-    } catch (Throwable exception_p) {
-      // Failed silently.
+    // Try loading a class according to the class name.
+    Class<?> clazz = loadClass(fullyQualifiedClassName_p, bundle_p);
+    // Try instantiating an object of loaded class.
+    if (clazz != null) {
+      result = clazz.newInstance();
     }
     return result;
   }
@@ -63,19 +59,13 @@ public class BundleHelper {
    * @return loaded Class according to given class name or null if loading
    *         failed.
    */
-  public static Class<?> loadClass(String fullyQualifiedClassName_p, Bundle bundle_p) {
+  public static Class<?> loadClass(String fullyQualifiedClassName_p, Bundle bundle_p) throws ClassNotFoundException {
     Class<?> clazz = null;
     // Precondition.
     if (bundle_p == null) {
       return clazz;
     }
-    try {
-      // Try loading a class according to the class name.
-      clazz = bundle_p.loadClass(fullyQualifiedClassName_p);
-    } catch (Throwable exception_p) {
-      // Failed silently.
-    }
-    return clazz;
+    return bundle_p.loadClass(fullyQualifiedClassName_p);
   }
 
   /**
