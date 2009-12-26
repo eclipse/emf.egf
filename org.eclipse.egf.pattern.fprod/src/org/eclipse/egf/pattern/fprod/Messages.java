46f20bc914f7646fd044dc702074373bb8feea87
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-26 07:15:18 +0000

    new projet to connect pattern with fprod orchestration

---
+++ org.eclipse.egf.pattern.fprod/src/org/eclipse/egf/pattern/fprod/Messages.java
@@ -0,0 +1,35 @@
+/**
+ * <copyright>
+ *
+ *  Copyright (c) 2009 Thales Corporate Services S.A.S.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *      Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ * </copyright>
+ */
+
+package org.eclipse.egf.pattern.fprod;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Thomas Guiu
+ * 
+ */
+public class Messages extends NLS {
+
+    public static String translation_job_label;
+
+    public static String Missing_pattern_error1;
+
+    private static final String BUNDLE_NAME = "org.eclipse.egf.pattern.fprod.PatternFprodMessages";//$NON-NLS-1$
+    static {
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+}
---
+++ org.eclipse.egf.pattern.fprod/src/org/eclipse/egf/pattern/fprod/tasks/PatternTask1.java
@@ -0,0 +1,58 @@
+package org.eclipse.egf.pattern.fprod.tasks;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egf.core.producer.InvocationException;
+import org.eclipse.egf.fprod.producer.context.ITaskProductionContext;
+import org.eclipse.egf.fprod.producer.invocation.ITaskProduction;
+import org.eclipse.egf.model.PatternContext;
+import org.eclipse.egf.model.PatternException;
+import org.eclipse.egf.model.fcore.ActivityContract;
+import org.eclipse.egf.model.pattern.Pattern;
+import org.eclipse.egf.pattern.engine.PatternHelper;
+import org.eclipse.egf.pattern.extension.ExtensionHelper;
+import org.eclipse.egf.pattern.extension.PatternExtension;
+import org.eclipse.egf.pattern.extension.ExtensionHelper.MissingExtensionException;
+import org.eclipse.egf.pattern.fprod.Messages;
+
+public class PatternTask1 implements ITaskProduction {
+
+    private Pattern pattern;
+
+    public void preExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
+        String patternId = context.getInputValue("patternId", String.class); //$NON-NLS-1$
+        pattern = PatternHelper.getPattern(patternId);
+        if (pattern == null)
+            throw new InvocationException(Messages.bind(Messages.Missing_pattern_error1, patternId));
+    }
+
+    public void doExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
+        try {
+            PatternExtension extension = ExtensionHelper.getExtension(pattern.getNature());
+            String reason = extension.canExecute(pattern);
+            PatternContext ctx = new PatternContext();
+
+            for (ActivityContract ac : context.getInputValueKeys()) {
+                ac.toString();
+                System.out.println(ac.getName());
+            }
+            // TODO init du pattern Context: lecture des input/inout depuis le
+            // productionCtx et ajout dans patternCtx
+            if (reason == null)
+                extension.createEngine(pattern).execute(ctx);
+            else
+                throw new InvocationException(reason);
+            // TODO récupération des résultat de l'exec du pattern Context:
+            // lecture des inout/out depuis le patternCtx et ajout dans
+            // productionCtx
+        } catch (MissingExtensionException e) {
+            throw new InvocationException(e);
+        } catch (PatternException e) {
+            throw new InvocationException(e);
+        }
+    }
+
+    public void postExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
+        pattern = null;
+    }
+
+}
