2b72987187d9dc12a20ea75aed3a17c36aeccc63
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-26 07:36:25 +0000

    getActiveWorkbenchShell enhancement.

--- org.eclipse.egf.common.ui/src/org/eclipse/egf/common/ui/EGFCommonUIPlugin.java
+++ org.eclipse.egf.common.ui/src/org/eclipse/egf/common/ui/EGFCommonUIPlugin.java
@@ -46,7 +46,12 @@ public class EGFCommonUIPlugin extends EGFAbstractUIPlugin {
    */
   public static Shell getActiveWorkbenchShell() {
     IWorkbenchWindow window = getActiveWorkbenchWindow();
-    if (window != null) {
+    if (window == null) {
+      IWorkbenchWindow[] windows = getDefault().getWorkbench().getWorkbenchWindows();
+      if (windows.length > 0) {
+        return windows[0].getShell();
+      }
+    } else {
       return window.getShell();
     }
     return null;
