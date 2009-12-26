87d7c1f95613c7ac27ca6686a5890443688e96bc
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-26 07:58:22 +0000

    Examples update.

--- examples/workspace/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
+++ examples/workspace/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
@@ -1,32 +1,44 @@
 package org.eclipse.egf.example.task.h1;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.producer.InvocationException;
 import org.eclipse.egf.fprod.producer.context.ITaskProductionContext;
 import org.eclipse.egf.fprod.producer.invocation.ITaskProduction;
+import org.eclipse.emf.codegen.ecore.generator.GeneratorAdapterFactory;
 import org.eclipse.emf.codegen.ecore.genmodel.generator.GenModelGeneratorAdapterFactory;
 
 public class H1 implements ITaskProduction {
 
-  private Integer quantity;
+  private Integer _quantity;
 
-  private Float price;
+  private Float _price;
 
-  private Float amount;
+  private Float _amount;
 
+  private Collection<String> _parameters;
+
+  @SuppressWarnings("unchecked")
   public void preExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
-    quantity = context.getInputValue("quantity", Integer.class); //$NON-NLS-1$
-    price = context.getInputValue("price", Float.class); //$NON-NLS-1$
-    context.getOutputValue("generatorAdapterFactory", GenModelGeneratorAdapterFactory.class); //$NON-NLS-1$
+    _quantity = context.getInputValue("quantity", Integer.class); //$NON-NLS-1$
+    _price = context.getInputValue("price", Float.class); //$NON-NLS-1$
+    _parameters = context.getInputValue("parameters", Collection.class); //$NON-NLS-1$
+    context.getOutputValue("generatorAdapterFactory", GeneratorAdapterFactory.class); //$NON-NLS-1$
   }
 
   public void doExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
-    amount = quantity * price;
+    _amount = _quantity * _price;
   }
 
   public void postExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
-    context.setOutputValue("amount", amount); //$NON-NLS-1$  		  
+    context.setOutputValue("amount", _amount); //$NON-NLS-1$  		  
     context.setOutputValue("generatorAdapterFactory", new GenModelGeneratorAdapterFactory()); //$NON-NLS-1$
+    Collection<String> parameters = new ArrayList<String>();
+    parameters.add("param1"); //$NON-NLS-1$
+    parameters.add("param2"); //$NON-NLS-1$
+    context.setOutputValue("parameters", parameters); //$NON-NLS-1$
   }
 
 }
