29681363fa0661cdff8dabfa8ad0b46f995b106c
Thomas Guiu <thomas.guiu@soyatec.com>
2009-12-26 07:00:00 +0000

    Model enhancement.

--- examples/runtime/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
+++ examples/runtime/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/H1.java
@@ -2,7 +2,8 @@ package org.eclipse.egf.example.task.h1;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.producer.InvocationException;
-import org.eclipse.egf.core.producer.context.IProductionContext;
+import org.eclipse.egf.fprod.producer.context.ITaskProductionContext;
+import org.eclipse.egf.fprod.producer.invocation.ITaskProduction;
 import org.eclipse.emf.codegen.ecore.genmodel.generator.GenModelGeneratorAdapterFactory;
 
 public class H1 implements ITaskProduction {
@@ -13,17 +14,17 @@ public class H1 implements ITaskProduction {
 
   private Float amount;
 
-  public void preExecute(final IProductionContext<Task> context, final IProgressMonitor monitor_p) throws InvocationException {
+  public void preExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
     quantity = context.getInputValue("quantity", Integer.class); //$NON-NLS-1$
     price = context.getInputValue("price", Float.class); //$NON-NLS-1$
     context.getOutputValue("generatorAdapterFactory", GenModelGeneratorAdapterFactory.class); //$NON-NLS-1$   
   }
 
-  public void doExecute(final IProductionContext<Task> context, final IProgressMonitor monitor_p) throws InvocationException {
+  public void doExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
     amount = quantity * price;
   }
 
-  public void postExecute(final IProductionContext<Task> context, final IProgressMonitor monitor_p) throws InvocationException {
+  public void postExecute(final ITaskProductionContext context, final IProgressMonitor monitor_p) throws InvocationException {
     context.setOutputValue("amount", amount); //$NON-NLS-1$       
     context.setOutputValue("generatorAdapterFactory", new GenModelGeneratorAdapterFactory()); //$NON-NLS-1$
   }
--- examples/runtime/org.eclipse.egf.example.task.h2/src/org/eclipse/egf/example/task/h2/H2.java
+++ examples/runtime/org.eclipse.egf.example.task.h2/src/org/eclipse/egf/example/task/h2/H2.java
@@ -3,21 +3,20 @@ package org.eclipse.egf.example.task.h2;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.EGFCorePlugin;
 import org.eclipse.egf.core.producer.InvocationException;
-import org.eclipse.egf.core.producer.context.IProductionContext;
-import org.eclipse.egf.model.fprod.Task;
-import org.eclipse.egf.model.fprod.task.IProductionTask;
+import org.eclipse.egf.fprod.producer.context.ITaskProductionContext;
+import org.eclipse.egf.fprod.producer.invocation.ITaskProduction;
 
-public class H2 implements IProductionTask {
+public class H2 implements ITaskProduction {
 
-  public void preExecute(final IProductionContext<Task> productionContext, final IProgressMonitor monitor_p) throws InvocationException {
+  public void preExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     EGFCorePlugin.getDefault().logInfo("preExecute H2"); //$NON-NLS-1$
   }
 
-  public void doExecute(final IProductionContext<Task> productionContext, final IProgressMonitor monitor_p) throws InvocationException {
+  public void doExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     throw new InvocationException("I'm a task who raised an exception"); //$NON-NLS-1$
   }
 
-  public void postExecute(final IProductionContext<Task> productionContext, final IProgressMonitor monitor_p) throws InvocationException {
+  public void postExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor_p) throws InvocationException {
     EGFCorePlugin.getDefault().logInfo("postExecute H2"); //$NON-NLS-1$	  		  
   }
 
