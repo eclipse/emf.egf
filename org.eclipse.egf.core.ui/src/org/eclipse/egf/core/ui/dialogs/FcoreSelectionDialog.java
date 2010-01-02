0fb504fe528735440e7f620e7240ca52ede421c5
Thomas Guiu <thomas.guiu@soyatec.com>
2010-01-02 01:36:57 +0000

    When dialog are opened, do not run a full search, wait for user input.

--- org.eclipse.egf.core.ui/src/org/eclipse/egf/core/ui/dialogs/FcoreSelectionDialog.java
+++ org.eclipse.egf.core.ui/src/org/eclipse/egf/core/ui/dialogs/FcoreSelectionDialog.java
@@ -130,15 +130,16 @@ public class FcoreSelectionDialog extends FilteredItemsSelectionDialog {
       return false;
     }
 
-    @Override
-    protected boolean matches(String text) {
-      String pattern = patternMatcher.getPattern();
-      if (pattern.indexOf("*") != 0 & pattern.indexOf("?") != 0 & pattern.indexOf(".") != 0) {//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        pattern = "*" + pattern; //$NON-NLS-1$
-        patternMatcher.setPattern(pattern);
-      }
-      return patternMatcher.matches(text);
-    }
+    // Activate to run a full load when dialog is opened
+    // @Override
+    // protected boolean matches(String text) {
+    // String pattern = patternMatcher.getPattern();
+    //      if (pattern.indexOf("*") != 0 & pattern.indexOf("?") != 0 & pattern.indexOf(".") != 0) {//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    //        pattern = "*" + pattern; //$NON-NLS-1$
+    // patternMatcher.setPattern(pattern);
+    // }
+    // return patternMatcher.matches(text);
+    // }
 
   }
 
--- org.eclipse.egf.model.editor/src/org/eclipse/egf/model/editor/dialogs/ActivitySelectionDialog.java
+++ org.eclipse.egf.model.editor/src/org/eclipse/egf/model/editor/dialogs/ActivitySelectionDialog.java
@@ -160,15 +160,16 @@ public class ActivitySelectionDialog extends FilteredItemsSelectionDialog {
       return false;
     }
 
-    @Override
-    protected boolean matches(String text) {
-      String pattern = patternMatcher.getPattern();
-      if (pattern.indexOf("*") != 0 & pattern.indexOf("?") != 0 & pattern.indexOf(".") != 0) {//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        pattern = "*" + pattern; //$NON-NLS-1$
-        patternMatcher.setPattern(pattern);
-      }
-      return patternMatcher.matches(text);
-    }
+    // Activate to run a full load when dialog is opened
+    // @Override
+    // protected boolean matches(String text) {
+    // String pattern = patternMatcher.getPattern();
+    //    if (pattern.indexOf("*") != 0 & pattern.indexOf("?") != 0 & pattern.indexOf(".") != 0) {//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    //      pattern = "*" + pattern; //$NON-NLS-1$
+    // patternMatcher.setPattern(pattern);
+    // }
+    // return patternMatcher.matches(text);
+    // }
 
   }
 
