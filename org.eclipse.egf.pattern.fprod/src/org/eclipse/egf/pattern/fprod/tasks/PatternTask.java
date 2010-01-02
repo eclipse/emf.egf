5eee5128d64b8132cfd1cba87d0173ec7c2a2523
Thomas Guiu <thomas.guiu@soyatec.com>
2010-01-02 12:29:07 +0000

    add support for libraries in PatternTask

--- org.eclipse.egf.pattern.fprod/src/org/eclipse/egf/pattern/fprod/tasks/PatternTask.java
+++ org.eclipse.egf.pattern.fprod/src/org/eclipse/egf/pattern/fprod/tasks/PatternTask.java
@@ -1,11 +1,18 @@
 package org.eclipse.egf.pattern.fprod.tasks;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.producer.InvocationException;
 import org.eclipse.egf.fprod.producer.context.ITaskProductionContext;
 import org.eclipse.egf.model.pattern.Pattern;
 import org.eclipse.egf.model.pattern.PatternContext;
+import org.eclipse.egf.model.pattern.PatternElement;
 import org.eclipse.egf.model.pattern.PatternException;
+import org.eclipse.egf.pattern.collector.PatternCollector;
 import org.eclipse.egf.pattern.extension.ExtensionHelper;
 import org.eclipse.egf.pattern.extension.PatternExtension;
 import org.eclipse.egf.pattern.extension.ExtensionHelper.MissingExtensionException;
@@ -13,28 +20,34 @@ import org.eclipse.egf.pattern.fprod.Messages;
 
 public class PatternTask extends AbstractPatternTask {
 
-    private Pattern pattern;
+    private final LinkedHashSet<Pattern> patterns = new LinkedHashSet<Pattern>();
 
     public void preExecute(final ITaskProductionContext context, final IProgressMonitor monitor) throws InvocationException {
         String patternId = context.getInputValue(PatternContext.PATTERN_ID, String.class); //$NON-NLS-1$
-        pattern = helper.getPattern(patternId);
-        if (pattern == null)
+        Set<String> ids = new HashSet<String>();
+        ids.add(patternId);
+        Map<String, PatternElement> patternElements = helper.getPatternElements(ids);
+        if (patternElements.isEmpty())
             throw new InvocationException(Messages.bind(Messages.Missing_pattern_error1, patternId));
+        PatternElement patternElement = patternElements.get(patternId);
+        // to replace libraries by their contents
+        PatternCollector.INSTANCE.collect(new PatternElement[] { patternElement }, patterns);
     }
 
     public void doExecute(final ITaskProductionContext context, final IProgressMonitor monitor) throws InvocationException {
         try {
-            PatternExtension extension = ExtensionHelper.getExtension(pattern.getNature());
-            String reason = extension.canExecute(pattern);
             PatternContext ctx = createPatternContext(context);
-
             readContext(context, ctx);
 
-            if (reason == null)
-                extension.createEngine(pattern).execute(ctx);
-            else
-                throw new InvocationException(reason);
+            for (Pattern pattern : patterns) {
+                PatternExtension extension = ExtensionHelper.getExtension(pattern.getNature());
+                String reason = extension.canExecute(pattern);
 
+                if (reason == null)
+                    extension.createEngine(pattern).execute(ctx);
+                else
+                    throw new InvocationException(reason);
+            }
             writeContext(context, ctx);
 
         } catch (MissingExtensionException e) {
@@ -46,6 +59,6 @@ public class PatternTask extends AbstractPatternTask {
 
     public void postExecute(final ITaskProductionContext context, final IProgressMonitor monitor) throws InvocationException {
         super.postExecute(context, monitor);
-        pattern = null;
+        patterns.clear();
     }
 }
