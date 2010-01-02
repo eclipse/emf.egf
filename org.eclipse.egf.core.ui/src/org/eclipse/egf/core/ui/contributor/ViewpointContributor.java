7b8b270f4e37c1d7c21baf0484cac5e17bcfc288
Thomas Guiu <thomas.guiu@soyatec.com>
2010-01-02 01:27:41 +0000

    NON-NLS string

--- org.eclipse.egf.core.ui/src/org/eclipse/egf/core/ui/contributor/ViewpointContributor.java
+++ org.eclipse.egf.core.ui/src/org/eclipse/egf/core/ui/contributor/ViewpointContributor.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
@@ -28,25 +28,25 @@ import org.eclipse.ui.part.EditorActionBarContributor;
  */
 public class ViewpointContributor {
 
-    public static final String EXTENSION_ID = "org.eclipse.egf.core.ui.viewpoint.editor.contributor";
-    protected ISelection selection;
-    protected IWorkbenchPage page;
-    protected EditorActionBarContributor parent;
-    protected IEditorPart activeEditorPart;
+  public static final String EXTENSION_ID = "org.eclipse.egf.core.ui.viewpoint.editor.contributor"; //$NON-NLS-1$
+  protected ISelection selection;
+  protected IWorkbenchPage page;
+  protected EditorActionBarContributor parent;
+  protected IEditorPart activeEditorPart;
 
-    public void setParentContributor(EditorActionBarContributor parent) {
-        this.parent = parent;
-    }
+  public void setParentContributor(EditorActionBarContributor parent) {
+    this.parent = parent;
+  }
 
-    public void menuAboutToShow(IMenuManager menuManager) {
+  public void menuAboutToShow(IMenuManager menuManager) {
 
-    }
+  }
 
-    public void setActiveEditor(IEditorPart part) {
-        activeEditorPart = part;
-    }
+  public void setActiveEditor(IEditorPart part) {
+    activeEditorPart = part;
+  }
 
-    public void selectionChanged(SelectionChangedEvent event) {
-        selection = event.getSelection();
-    }
+  public void selectionChanged(SelectionChangedEvent event) {
+    selection = event.getSelection();
+  }
 }
