0fdba61d9891e903849baa40adbc52127d50bb4f
Thomas Guiu <thomas.guiu@soyatec.com>
2010-01-17 02:46:41 +0000

    EGF standardization.

--- org.eclipse.egf.pattern.ui.java/src/org/eclipse/pattern/java/ui/Activator.java
+++ org.eclipse.egf.pattern.ui.java/src/org/eclipse/pattern/java/ui/Activator.java
@@ -14,72 +14,80 @@
  */
 package org.eclipse.pattern.java.ui;
 
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.egf.common.ui.activator.EGFAbstractUIPlugin;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.osgi.framework.BundleContext;
 
 /**
  * @author Yahong Song - Soyatec
  * 
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends EGFAbstractUIPlugin {
 
-    // The plug-in ID
-    public static final String PLUGIN_ID = "org.eclipse.pattern.java.ui";
+  // The shared instance
+  private static Activator _plugin;
 
-    // The shared instance
-    private static Activator plugin;
+  /**
+   * 
+   * Returns the currently active window for this workbench (if any). Returns
+   * <code>null</code> if there is no active workbench window. Returns
+   * <code>null</code> if called from a non-UI thread.
+   * 
+   * @return the active workbench window, or <code>null</code> if there is
+   *         no active workbench window or if called from a non-UI thread
+   */
+  public static IWorkbenchWindow getActiveWorkbenchWindow() {
+    return getDefault().getWorkbench().getActiveWorkbenchWindow();
+  }
 
-    /**
-     * The constructor
-     */
-    public Activator() {
+  /**
+   * Returns the currently active shell for this workbench (if any).
+   * 
+   * @return the active workbench shell.
+   */
+  public static Shell getActiveWorkbenchShell() {
+    IWorkbenchWindow window = getActiveWorkbenchWindow();
+    if (window == null) {
+      IWorkbenchWindow[] windows = getDefault().getWorkbench().getWorkbenchWindows();
+      if (windows.length > 0) {
+        return windows[0].getShell();
+      }
+    } else {
+      return window.getShell();
     }
+    return null;
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * 
-     * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-     * )
-     */
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
+  /**
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext )
+   */
+  @Override
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    _plugin = this;
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * 
-     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-     * )
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
+  /**
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext )
+   */
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    super.stop(context);
+    _plugin = null;
+  }
 
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static Activator getDefault() {
-        return plugin;
-    }
+  /**
+   * Returns the shared instance
+   * 
+   * @return the shared instance
+   */
+  public static Activator getDefault() {
+    return _plugin;
+  }
 
-    /**
-     * Returns an image descriptor for the image file at the given
-     * plug-in relative path
-     * 
-     * @param path
-     *            the path
-     * @return the image descriptor
-     */
-    public static ImageDescriptor getImageDescriptor(String path) {
-        return imageDescriptorFromPlugin(PLUGIN_ID, path);
-    }
 }
