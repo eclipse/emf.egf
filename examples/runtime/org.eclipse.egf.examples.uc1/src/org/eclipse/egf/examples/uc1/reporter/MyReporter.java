09a8c841213b7458b168015a6f6286062df0dc81
Thomas Guiu <thomas.guiu@soyatec.com>
2010-01-17 20:59:39 +0000

    update: MyReporter - Output on standard and EGF console

--- examples/runtime/org.eclipse.egf.examples.uc1/src/org/eclipse/egf/examples/uc1/reporter/MyReporter.java
+++ examples/runtime/org.eclipse.egf.examples.uc1/src/org/eclipse/egf/examples/uc1/reporter/MyReporter.java
@@ -2,13 +2,18 @@ package org.eclipse.egf.examples.uc1.reporter;
 
 import java.util.Map;
 
+import org.eclipse.egf.core.EGFCorePlugin;
 import org.eclipse.egf.model.pattern.PatternContext;
 import org.eclipse.egf.model.pattern.PatternExecutionReporter;
 
 public class MyReporter implements PatternExecutionReporter {
 
   public void executionFinished(String output, PatternContext context) {
-    System.out.println("Result of pattern:\n" + output); //$NON-NLS-1$
+		String outputMessage = new String ("Result of pattern:\n"+output);
+		// Message on the default console
+		System.out.println(outputMessage);
+		// Message on the EGF console
+	    EGFCorePlugin.getDefault().logInfo(outputMessage); 		  
   }
 
   public void loopFinished(String output, PatternContext context, Map<String, Object> parameterValues) {
