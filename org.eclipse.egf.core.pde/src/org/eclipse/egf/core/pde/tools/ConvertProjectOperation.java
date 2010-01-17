bd51cf2329b5be4cd1bacda16e6dea8bbf83e19b
Thomas Guiu <thomas.guiu@soyatec.com>
2010-01-17 10:38:38 +0000

    Wrong conversion from Simple Project to Plugin Project. build.properties doesn't properly set the META-INF/ entry.
    build.properties feed improvement. 
    Add existing project root folder as binary entry.

--- org.eclipse.egf.core.pde/src/org/eclipse/egf/core/pde/tools/ConvertProjectOperation.java
+++ org.eclipse.egf.core.pde/src/org/eclipse/egf/core/pde/tools/ConvertProjectOperation.java
@@ -25,6 +25,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -81,11 +82,13 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
 
   private boolean _createEGFNature;
 
-  private String fLibraryName;
+  private String _library;
 
-  private String[] fSrcEntries;
+  private List<String> _sources;
 
-  private String[] fLibEntries;
+  private List<String> _libraries;
+
+  private List<String> _directories;
 
   private List<IClasspathEntry> _classpathEntries;
 
@@ -246,7 +249,7 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
       // Set Project Description
       _project.setDescription(projectDescription, subMonitor.newChild(100, SubMonitor.SUPPRESS_NONE));
 
-      setupClasspathEntries(monitor);
+      setupEntries(monitor);
       setupLibraryName();
 
       // Manifest
@@ -269,13 +272,17 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
 
   }
 
-  private void setupClasspathEntries(IProgressMonitor monitor) throws CoreException {
+  private void setupEntries(IProgressMonitor monitor) throws CoreException {
     SubMonitor subMonitor = SubMonitor.convert(monitor, EGFCoreMessages.ConvertProjectOperation_setupClasspath, 400);
     try {
+
       // Current variables
       boolean isInitiallyEmpty = _classpathEntries.isEmpty();
       List<String> sources = new UniqueEList<String>();
       List<String> libraries = new UniqueEList<String>();
+      List<String> directories = new UniqueEList<String>();
+
+      // Classpath analysis
       for (IClasspathEntry currentClassPath : _classpathEntries) {
         int contentType = currentClassPath.getEntryKind();
         // Process existing source folder
@@ -312,7 +319,7 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
       subMonitor.worked(100);
 
       // Create a source folder if necessary
-      if (_createJavaProject && sources.isEmpty() && addSourceFolders() != null) {
+      if ((_hasJavaNature || _createJavaProject) && sources.isEmpty() && addSourceFolders() != null) {
         for (String sourceFolder : addSourceFolders()) {
           IPath src = new Path(sourceFolder);
           IContainer sourceContainer = _project.getFolder(src);
@@ -332,32 +339,44 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
         subMonitor.worked(100);
       }
 
-      fSrcEntries = sources.toArray(new String[sources.size()]);
-      fLibEntries = libraries.toArray(new String[libraries.size()]);
-      if (isInitiallyEmpty) {
-        IClasspathEntry jreClasspathEntry = JavaCore.newVariableEntry(new Path(JavaRuntime.JRELIB_VARIABLE), new Path(JavaRuntime.JRESRC_VARIABLE), new Path(JavaRuntime.JRESRCROOT_VARIABLE));
-        for (Iterator<IClasspathEntry> i = _classpathEntries.iterator(); i.hasNext();) {
-          IClasspathEntry classpathEntry = i.next();
-          if (classpathEntry.getPath().isPrefixOf(jreClasspathEntry.getPath())) {
-            i.remove();
-          }
+      // Folder analysis
+      for (IResource resource : _project.members()) {
+        if (resource instanceof IContainer == false) {
+          continue;
         }
-        String jreContainer = JavaRuntime.JRE_CONTAINER;
-        String complianceLevel = CodeGenUtil.EclipseUtil.getJavaComplianceLevel(_project);
-        if (JavaCore.VERSION_1_5.equals(complianceLevel)) {
-          jreContainer += "/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/J2SE-1.5"; //$NON-NLS-1$
-        } else if (JavaCore.VERSION_1_6.equals(complianceLevel)) {
-          jreContainer += "/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.6"; //$NON-NLS-1$
-        } else if (JavaCore.VERSION_1_7.equals(complianceLevel)) {
-          jreContainer += "/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.7"; //$NON-NLS-1$        
+        String path = resource.getFullPath().removeFirstSegments(1).toString();
+        if (sources.contains(path) == false) {
+          directories.add(path + "/"); //$NON-NLS-1$
         }
-        _classpathEntries.add(JavaCore.newContainerEntry(new Path(jreContainer)));
       }
-      _classpathEntries.add(JavaCore.newContainerEntry(new Path("org.eclipse.pde.core.requiredPlugins"))); //$NON-NLS-1$
+
+      _sources = sources;
+      _libraries = libraries;
+      _directories = directories;
 
       // Finally setup classpath if necessary
       if (_hasJavaNature || _createJavaProject) {
         try {
+          if (isInitiallyEmpty) {
+            IClasspathEntry jreClasspathEntry = JavaCore.newVariableEntry(new Path(JavaRuntime.JRELIB_VARIABLE), new Path(JavaRuntime.JRESRC_VARIABLE), new Path(JavaRuntime.JRESRCROOT_VARIABLE));
+            for (Iterator<IClasspathEntry> i = _classpathEntries.iterator(); i.hasNext();) {
+              IClasspathEntry classpathEntry = i.next();
+              if (classpathEntry.getPath().isPrefixOf(jreClasspathEntry.getPath())) {
+                i.remove();
+              }
+            }
+            String jreContainer = JavaRuntime.JRE_CONTAINER;
+            String complianceLevel = CodeGenUtil.EclipseUtil.getJavaComplianceLevel(_project);
+            if (JavaCore.VERSION_1_5.equals(complianceLevel)) {
+              jreContainer += "/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/J2SE-1.5"; //$NON-NLS-1$
+            } else if (JavaCore.VERSION_1_6.equals(complianceLevel)) {
+              jreContainer += "/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.6"; //$NON-NLS-1$
+            } else if (JavaCore.VERSION_1_7.equals(complianceLevel)) {
+              jreContainer += "/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.7"; //$NON-NLS-1$        
+            }
+            _classpathEntries.add(JavaCore.newContainerEntry(new Path(jreContainer)));
+          }
+          _classpathEntries.add(JavaCore.newContainerEntry(new Path("org.eclipse.pde.core.requiredPlugins"))); //$NON-NLS-1$          
           IJavaProject javaProject = JavaCore.create(_project);
           javaProject.setRawClasspath(_classpathEntries.toArray(new IClasspathEntry[_classpathEntries.size()]), subMonitor.newChild(100, SubMonitor.SUPPRESS_NONE));
         } catch (JavaModelException jme) {
@@ -379,15 +398,15 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
   }
 
   private void setupLibraryName() {
-    if (isOldTarget() || (fLibEntries.length > 0 && fSrcEntries.length > 0)) {
+    if (isOldTarget() || (_libraries.size() > 0 && _sources.size() > 0)) {
       String libName = _project.getName();
       int i = libName.lastIndexOf("."); //$NON-NLS-1$
       if (i != -1) {
         libName = libName.substring(i + 1);
       }
-      fLibraryName = libName + ".jar"; //$NON-NLS-1$
+      _library = libName + ".jar"; //$NON-NLS-1$
     } else {
-      fLibraryName = "."; //$NON-NLS-1$
+      _library = "."; //$NON-NLS-1$
     }
   }
 
@@ -457,22 +476,25 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
     if (_project.getFile(ICoreConstants.PLUGIN_FILENAME_DESCRIPTOR).exists()) {
       addToken(entry, ICoreConstants.PLUGIN_FILENAME_DESCRIPTOR);
     }
-    if (_project.getFile(EGFCommonConstants.BUNDLE_FILENAME_DIRECTORY_DESCRIPTOR).exists()) {
+    if (_project.getFile(EGFCommonConstants.BUNDLE_FILENAME_DESCRIPTOR).exists()) {
       addToken(entry, EGFCommonConstants.BUNDLE_FILENAME_DIRECTORY_DESCRIPTOR);
     }
-    for (int i = 0; i < fLibEntries.length; i++) {
-      addToken(entry, fLibEntries[i]);
+    for (String directory : _directories) {
+      addToken(entry, directory);
     }
-    if (fSrcEntries.length > 0) {
-      addToken(entry, fLibraryName);
-      IBuildEntry source = build.getEntry(IBuildEntry.JAR_PREFIX + fLibraryName);
-      if (source == null) {
-        source = model.getFactory().createEntry(IBuildEntry.JAR_PREFIX + fLibraryName);
+    for (String library : _libraries) {
+      addToken(entry, library);
+    }
+    if (_sources.size() > 0) {
+      addToken(entry, _library);
+      IBuildEntry buildEntry = build.getEntry(IBuildEntry.JAR_PREFIX + _library);
+      if (buildEntry == null) {
+        buildEntry = model.getFactory().createEntry(IBuildEntry.JAR_PREFIX + _library);
       }
-      for (int i = 0; i < fSrcEntries.length; i++) {
-        addToken(source, fSrcEntries[i]);
+      for (String source : _sources) {
+        addToken(buildEntry, source);
       }
-      build.add(source);
+      build.add(buildEntry);
     }
     if (entry.getTokens().length > 0) {
       build.add(entry);
@@ -571,17 +593,17 @@ public class ConvertProjectOperation extends WorkspaceModifyOperation {
     IPluginBase base = model.getPluginBase();
 
     if (missingInfo) {
-      if (fLibraryName != null && fLibraryName.equals(".") == false) { //$NON-NLS-1$
+      if (_library != null && _library.equals(".") == false) { //$NON-NLS-1$
         IPluginLibrary library = factory.createLibrary();
-        library.setName(fLibraryName);
+        library.setName(_library);
         library.setExported(true);
         base.add(library);
       }
-      for (int i = 0; i < fLibEntries.length; i++) {
-        IPluginLibrary library = factory.createLibrary();
-        library.setName(fLibEntries[i]);
-        library.setExported(true);
-        base.add(library);
+      for (String library : _libraries) {
+        IPluginLibrary pluginLibrary = factory.createLibrary();
+        pluginLibrary.setName(library);
+        pluginLibrary.setExported(true);
+        base.add(pluginLibrary);
       }
       if (TargetPlatformHelper.getTargetVersion() >= 3.1) {
         bundle.setHeader(Constants.BUNDLE_MANIFESTVERSION, "2"); //$NON-NLS-1$
