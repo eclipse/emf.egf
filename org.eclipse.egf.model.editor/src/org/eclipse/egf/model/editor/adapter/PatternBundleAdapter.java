ea771a5cf54e808acf87faceb3967c2996748859
Thomas Guiu <thomas.guiu@soyatec.com>
2010-03-06 23:49:01 +0000

    operation should be forked.

--- org.eclipse.egf.model.editor/src/org/eclipse/egf/model/editor/adapter/PatternBundleAdapter.java
+++ org.eclipse.egf.model.editor/src/org/eclipse/egf/model/editor/adapter/PatternBundleAdapter.java
@@ -75,7 +75,7 @@ public class PatternBundleAdapter extends EContentAdapter {
       };
       // asynchronous operation
       try {
-        new ProgressMonitorDialog(_shell).run(false, true, operation);
+        new ProgressMonitorDialog(_shell).run(true, true, operation);
       } catch (InterruptedException ie) {
         // Nothing to do
       } catch (Exception e) {
