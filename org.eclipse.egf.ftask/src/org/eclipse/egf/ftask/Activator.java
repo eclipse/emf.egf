b70df37483ceae3e7707bfaa3ea379473b3a3e00
Thomas Guiu <thomas.guiu@soyatec.com>
2010-03-16 23:51:36 +0000

    initial commit

---
+++ org.eclipse.egf.ftask/src/org/eclipse/egf/ftask/Activator.java
@@ -0,0 +1,69 @@
+/**
+ * <copyright>
+ *
+ *  Copyright (c) 2009 Thales Corporate Services S.A.S.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *      Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.eclipse.egf.ftask;
+
+import org.eclipse.egf.common.activator.EGFAbstractPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Thomas Guiu
+ * 
+ */
+public class Activator extends EGFAbstractPlugin {
+
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.eclipse.egf.ftask";
+
+    // The shared instance
+    private static Activator plugin;
+
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+}
