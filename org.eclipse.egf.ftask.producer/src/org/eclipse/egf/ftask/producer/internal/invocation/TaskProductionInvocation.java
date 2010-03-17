4f1032662059557c6914f721f161d2218dac3318
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-03-17 20:07:09 +0000

    moved from model.ftask

--- org.eclipse.egf.ftask.producer/src/org/eclipse/egf/ftask/producer/internal/invocation/TaskProductionInvocation.java
+++ org.eclipse.egf.ftask.producer/src/org/eclipse/egf/ftask/producer/internal/invocation/TaskProductionInvocation.java
@@ -13,45 +13,44 @@ package org.eclipse.egf.ftask.producer.internal.invocation;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.producer.InvocationException;
-import org.eclipse.egf.ftask.invoker.TaskInvoker;
 import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
 import org.eclipse.egf.ftask.producer.invocation.ITaskProductionInvocation;
 import org.eclipse.egf.model.ftask.Task;
+import org.eclipse.egf.model.ftask.task.TaskHook;
 import org.osgi.framework.Bundle;
 
 /**
- * This class is responsible to process user defined
- * {@link org.eclipse.egf.ftask.producer.invocation.ITaskProductionInvocation}.
+ * This class is responsible to process user defined {@link org.eclipse.egf.ftask.producer.invocation.ITaskProductionInvocation}.
  * 
  * @author Xavier Maysonnave
  */
 public class TaskProductionInvocation implements ITaskProductionInvocation {
 
-    /**
-     * Bundle
-     */
-    private Bundle bundle;
+  /**
+   * Bundle
+   */
+  private Bundle bundle;
 
-    /**
-     * ITaskProductionContext
-     */
-    private ITaskProductionContext productionContext;
+  /**
+   * ITaskProductionContext
+   */
+  private ITaskProductionContext productionContext;
 
-    /**
-     * Task
-     */
-    private Task task;
+  /**
+   * Task
+   */
+  private Task task;
 
-    public TaskProductionInvocation(Bundle bundle, ITaskProductionContext productionContext, Task task) {
-        Assert.isNotNull(bundle);
-        Assert.isNotNull(productionContext);
-        this.bundle = bundle;
-        this.productionContext = productionContext;
-        this.task = task;
-    }
+  public TaskProductionInvocation(Bundle bundle, ITaskProductionContext productionContext, Task task) {
+    Assert.isNotNull(bundle);
+    Assert.isNotNull(productionContext);
+    this.bundle = bundle;
+    this.productionContext = productionContext;
+    this.task = task;
+  }
 
-    public void invoke(final IProgressMonitor monitor) throws InvocationException {
-        TaskInvoker.HELPER.getInvoker(task).invoke(bundle, productionContext, task, monitor);
-    }
+  public void invoke(final IProgressMonitor monitor) throws InvocationException {
+    TaskHook.HELPER.getHook(task).invoke(bundle, productionContext, task, monitor);
+  }
 
 }
