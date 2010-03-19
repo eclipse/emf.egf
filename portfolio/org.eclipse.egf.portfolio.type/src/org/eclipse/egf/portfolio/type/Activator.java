c2e0e435de40a938d24ede04795497e0358472f4
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-03-19 10:29:52 +0000

    manifest.mf and build.properties normalization.

--- portfolio/org.eclipse.egf.portfolio.type/src/org/eclipse/egf/portfolio/type/Activator.java
+++ portfolio/org.eclipse.egf.portfolio.type/src/org/eclipse/egf/portfolio/type/Activator.java
@@ -1,50 +1,45 @@
 package org.eclipse.egf.portfolio.type;
 
-import org.eclipse.core.runtime.Plugin;
+import org.eclipse.egf.common.activator.EGFAbstractPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class Activator extends EGFAbstractPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.egf.portfolio.type";
+  // The shared instance
+  private static Activator _plugin;
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    _plugin = this;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    _plugin = null;
+    super.stop(context);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+  /**
+   * Returns the shared instance
+   * 
+   * @return the shared instance
+   */
+  public static Activator getDefault() {
+    return _plugin;
+  }
 
 }
