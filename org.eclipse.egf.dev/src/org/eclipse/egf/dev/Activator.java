f83b9852827decb06ec39b9b31ff4f59754426f2
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-03-31 07:43:47 +0000

    Dump errors to error log

--- org.eclipse.egf.dev/src/org/eclipse/egf/dev/Activator.java
+++ org.eclipse.egf.dev/src/org/eclipse/egf/dev/Activator.java
@@ -1,57 +1,36 @@
+/**
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ */
 package org.eclipse.egf.dev;
 
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.egf.common.ui.activator.EGFAbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-/**
- * The activator class controls the plug-in life cycle
- */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends EGFAbstractUIPlugin {
 
-  // The shared instance
-  private static Activator _plugin;
+    private static Activator plugin;
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void start(BundleContext context) throws Exception {
-    super.start(context);
-    _plugin = this;
-  }
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void stop(BundleContext context) throws Exception {
-    _plugin = null;
-    super.stop(context);
-  }
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
 
-  /**
-   * Returns the shared instance
-   * 
-   * @return the shared instance
-   */
-  public static Activator getDefault() {
-    return _plugin;
-  }
+    public static Activator getDefault() {
+        return plugin;
+    }
 
-  /**
-   * Returns an image descriptor for the image file at the given
-   * plug-in relative path
-   * 
-   * @param path
-   *          the path
-   * @return the image descriptor
-   */
-  public static ImageDescriptor getImageDescriptor(String path) {
-    return imageDescriptorFromPlugin(getDefault().getBundle().getSymbolicName(), path);
-  }
 }
--- org.eclipse.egf.dev/src/org/eclipse/egf/dev/pattern/actions/TranslateAllAction.java
+++ org.eclipse.egf.dev/src/org/eclipse/egf/dev/pattern/actions/TranslateAllAction.java
@@ -1,3 +1,18 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ * </copyright>
+ */
+
 package org.eclipse.egf.dev.pattern.actions;
 
 import java.util.ArrayList;
@@ -6,6 +21,7 @@ import java.util.Set;
 
 import org.eclipse.egf.core.EGFCorePlugin;
 import org.eclipse.egf.core.fcore.IPlatformFcore;
+import org.eclipse.egf.dev.Activator;
 import org.eclipse.egf.model.pattern.Pattern;
 import org.eclipse.egf.pattern.collector.PatternCollector;
 import org.eclipse.egf.pattern.engine.TranslationHelper;
@@ -20,79 +36,62 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 /**
- * Our sample action implements workbench action delegate.
- * The action proxy will be created by the workbench and
- * shown in the UI. When the user tries to use the action,
- * this delegate will be created and execution will be
- * delegated to it.
- * 
- * @see IWorkbenchWindowActionDelegate
+ * @author Thomas Guiu
  */
 public class TranslateAllAction implements IWorkbenchWindowActionDelegate {
 
-  private IWorkbenchWindow _window;
+    private IWorkbenchWindow window;
 
-  /**
-   * The constructor.
-   */
-  public TranslateAllAction() {
-    // Nothing to do
-  }
-
-  /**
-   * The action has been activated. The argument of the
-   * method represents the 'real' action sitting
-   * in the workbench UI.
-   * 
-   * @see IWorkbenchWindowActionDelegate#run
-   */
-  public void run(IAction action) {
-    // List<Pattern> result = new ArrayList<Pattern>(200);
-    Set<Pattern> result = new HashSet<Pattern>(200);
-    IPlatformFcore[] platformFcores = EGFCorePlugin.getPlatformFcores();
-    ResourceSet resourceSet = new ResourceSetImpl();
-    try {
-      for (IPlatformFcore platformFcore : platformFcores) {
-        URI uri = platformFcore.getURI();
-        Resource res = resourceSet.getResource(uri, true);
-        PatternCollector.INSTANCE.collect(res.getContents(), result, PatternCollector.EMPTY_ID_SET);
-      }
-      new TranslationHelper().translate(new ArrayList<Pattern>(result));
-    } catch (Exception e) {
-      MessageDialog.openError(_window.getShell(), "Error", e.getMessage()); //$NON-NLS-1$
+    public TranslateAllAction() {
     }
 
-  }
+    public void run(IAction action) {
+        Set<Pattern> result = new HashSet<Pattern>(200);
+        IPlatformFcore[] platformFcores = EGFCorePlugin.getPlatformFcores();
+        ResourceSet resourceSet = new ResourceSetImpl();
+        try {
+            for (IPlatformFcore platformFcore : platformFcores) {
+                URI uri = platformFcore.getURI();
+                Resource res = resourceSet.getResource(uri, true);
+                PatternCollector.INSTANCE.collect(res.getContents(), result, PatternCollector.EMPTY_ID_SET);
+            }
+            new TranslationHelper().translate(new ArrayList<Pattern>(result));
+        } catch (Exception e) {
+            MessageDialog.openError(window.getShell(), "Error", e.getMessage()); //$NON-NLS-1$
+            Activator.getDefault().logError(e);
+        }
 
-  /**
-   * Selection in the workbench has been changed. We
-   * can change the state of the 'real' action here
-   * if we want, but this can only happen after
-   * the delegate has been created.
-   * 
-   * @see IWorkbenchWindowActionDelegate#selectionChanged
-   */
-  public void selectionChanged(IAction action, ISelection selection) {
-    // Nothing to do
-  }
+    }
 
-  /**
-   * We can use this method to dispose of any system
-   * resources we previously allocated.
-   * 
-   * @see IWorkbenchWindowActionDelegate#dispose
-   */
-  public void dispose() {
-    // Nothing to do
-  }
+    /**
+     * Selection in the workbench has been changed. We
+     * can change the state of the 'real' action here
+     * if we want, but this can only happen after
+     * the delegate has been created.
+     * 
+     * @see IWorkbenchWindowActionDelegate#selectionChanged
+     */
+    public void selectionChanged(IAction action, ISelection selection) {
+        // Nothing to do
+    }
 
-  /**
-   * We will cache window object in order to
-   * be able to provide parent shell for the message dialog.
-   * 
-   * @see IWorkbenchWindowActionDelegate#init
-   */
-  public void init(IWorkbenchWindow window) {
-    _window = window;
-  }
+    /**
+     * We can use this method to dispose of any system
+     * resources we previously allocated.
+     * 
+     * @see IWorkbenchWindowActionDelegate#dispose
+     */
+    public void dispose() {
+        // Nothing to do
+    }
+
+    /**
+     * We will cache window object in order to
+     * be able to provide parent shell for the message dialog.
+     * 
+     * @see IWorkbenchWindowActionDelegate#init
+     */
+    public void init(IWorkbenchWindow window) {
+        window = window;
+    }
 }
