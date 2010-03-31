01aa9db76e92a5aaeeec734f535cdd9dfc1e3327
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-03-31 07:37:41 +0000

    normalization.

--- org.eclipse.egf.dev/src/org/eclipse/egf/dev/pattern/actions/TranslateAllAction.java
+++ org.eclipse.egf.dev/src/org/eclipse/egf/dev/pattern/actions/TranslateAllAction.java
@@ -29,66 +29,70 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
  * @see IWorkbenchWindowActionDelegate
  */
 public class TranslateAllAction implements IWorkbenchWindowActionDelegate {
-    private IWorkbenchWindow window;
 
-    /**
-     * The constructor.
-     */
-    public TranslateAllAction() {
+  private IWorkbenchWindow _window;
+
+  /**
+   * The constructor.
+   */
+  public TranslateAllAction() {
+    // Nothing to do
+  }
+
+  /**
+   * The action has been activated. The argument of the
+   * method represents the 'real' action sitting
+   * in the workbench UI.
+   * 
+   * @see IWorkbenchWindowActionDelegate#run
+   */
+  public void run(IAction action) {
+    // List<Pattern> result = new ArrayList<Pattern>(200);
+    Set<Pattern> result = new HashSet<Pattern>(200);
+    IPlatformFcore[] platformFcores = EGFCorePlugin.getPlatformFcores();
+    ResourceSet resourceSet = new ResourceSetImpl();
+    try {
+      for (IPlatformFcore platformFcore : platformFcores) {
+        URI uri = platformFcore.getURI();
+        Resource res = resourceSet.getResource(uri, true);
+        PatternCollector.INSTANCE.collect(res.getContents(), result, PatternCollector.EMPTY_ID_SET);
+      }
+      new TranslationHelper().translate(new ArrayList<Pattern>(result));
+    } catch (Exception e) {
+      MessageDialog.openError(_window.getShell(), "Error", e.getMessage()); //$NON-NLS-1$
     }
 
-    /**
-     * The action has been activated. The argument of the
-     * method represents the 'real' action sitting
-     * in the workbench UI.
-     * 
-     * @see IWorkbenchWindowActionDelegate#run
-     */
-    public void run(IAction action) {
-        // List<Pattern> result = new ArrayList<Pattern>(200);
-        Set<Pattern> result = new HashSet<Pattern>(200);
-        IPlatformFcore[] platformFcores = EGFCorePlugin.getPlatformFcores();
-        ResourceSet resourceSet = new ResourceSetImpl();
-        try {
-            for (IPlatformFcore platformFcore : platformFcores) {
-                URI uri = platformFcore.getURI();
-                Resource res = resourceSet.getResource(uri, true);
-                PatternCollector.INSTANCE.collect(res.getContents(), result, PatternCollector.EMPTY_ID_SET);
-            }
-            new TranslationHelper().translate(new ArrayList<Pattern>(result));
-        } catch (Exception e) {
-            MessageDialog.openError(window.getShell(), "Error", e.getMessage());
-        }
+  }
 
-    }
+  /**
+   * Selection in the workbench has been changed. We
+   * can change the state of the 'real' action here
+   * if we want, but this can only happen after
+   * the delegate has been created.
+   * 
+   * @see IWorkbenchWindowActionDelegate#selectionChanged
+   */
+  public void selectionChanged(IAction action, ISelection selection) {
+    // Nothing to do
+  }
 
-    /**
-     * Selection in the workbench has been changed. We
-     * can change the state of the 'real' action here
-     * if we want, but this can only happen after
-     * the delegate has been created.
-     * 
-     * @see IWorkbenchWindowActionDelegate#selectionChanged
-     */
-    public void selectionChanged(IAction action, ISelection selection) {
-    }
+  /**
+   * We can use this method to dispose of any system
+   * resources we previously allocated.
+   * 
+   * @see IWorkbenchWindowActionDelegate#dispose
+   */
+  public void dispose() {
+    // Nothing to do
+  }
 
-    /**
-     * We can use this method to dispose of any system
-     * resources we previously allocated.
-     * 
-     * @see IWorkbenchWindowActionDelegate#dispose
-     */
-    public void dispose() {
-    }
-
-    /**
-     * We will cache window object in order to
-     * be able to provide parent shell for the message dialog.
-     * 
-     * @see IWorkbenchWindowActionDelegate#init
-     */
-    public void init(IWorkbenchWindow window) {
-        this.window = window;
-    }
+  /**
+   * We will cache window object in order to
+   * be able to provide parent shell for the message dialog.
+   * 
+   * @see IWorkbenchWindowActionDelegate#init
+   */
+  public void init(IWorkbenchWindow window) {
+    _window = window;
+  }
 }
