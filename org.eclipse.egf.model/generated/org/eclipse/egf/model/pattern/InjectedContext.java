e03bc0c9e0b3cc0d9df41f76c29dea6b767ac185
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-04-02 08:23:51 +0000

    remove useless import. add missing annotations.

--- org.eclipse.egf.model/generated/org/eclipse/egf/model/pattern/InjectedContext.java
+++ org.eclipse.egf.model/generated/org/eclipse/egf/model/pattern/InjectedContext.java
@@ -1,18 +1,16 @@
 /**
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  */
 package org.eclipse.egf.model.pattern;
-
-import org.eclipse.egf.model.types.Type;
 
 import org.eclipse.emf.ecore.EObject;
 
@@ -20,35 +18,38 @@ import org.eclipse.emf.ecore.EObject;
  * <!-- begin-user-doc -->
  * A representation of the model object '<em><b>Injected Context</b></em>'.
  * <!-- end-user-doc -->
- *
- *
+ * 
+ * 
  * @see org.eclipse.egf.model.pattern.PatternPackage#getInjectedContext()
  * @model interface="true" abstract="true"
  * @generated
  */
 public interface InjectedContext extends EObject {
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @model kind="operation" required="true"
-     * @generated
-     */
-    String getType();
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @model kind="operation" required="true"
+   * @generated
+   */
+  String getType();
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @model kind="operation" required="true"
-     * @generated
-     */
-    String getName();
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @model kind="operation" required="true"
+   * @generated
+   */
+  String getName();
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @model kind="operation" required="true"
-     * @generated
-     */
-    Pattern getPattern();
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @model kind="operation" required="true"
+   * @generated
+   */
+  Pattern getPattern();
 
 } // InjectedContext
--- org.eclipse.egf.model/generated/org/eclipse/egf/model/pattern/impl/PatternInjectedCallImpl.java
+++ org.eclipse.egf.model/generated/org/eclipse/egf/model/pattern/impl/PatternInjectedCallImpl.java
@@ -15,7 +15,6 @@ package org.eclipse.egf.model.pattern.impl;
 import org.eclipse.egf.model.pattern.InjectedContext;
 import org.eclipse.egf.model.pattern.PatternInjectedCall;
 import org.eclipse.egf.model.pattern.PatternPackage;
-import org.eclipse.egf.model.pattern.PatternVariable;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
@@ -27,119 +26,128 @@ import org.eclipse.emf.ecore.impl.ENotificationImpl;
  * <p>
  * The following features are implemented:
  * <ul>
- *   <li>{@link org.eclipse.egf.model.pattern.impl.PatternInjectedCallImpl#getContext <em>Context</em>}</li>
+ * <li>{@link org.eclipse.egf.model.pattern.impl.PatternInjectedCallImpl#getContext <em>Context</em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @generated
  */
 public class PatternInjectedCallImpl extends AbstractPatternCallImpl implements PatternInjectedCall {
-    /**
-     * The cached value of the '{@link #getContext() <em>Context</em>}' reference.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @see #getContext()
-     * @generated
-     * @ordered
-     */
-    protected InjectedContext context;
+  /**
+   * The cached value of the '{@link #getContext() <em>Context</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @see #getContext()
+   * @generated
+   * @ordered
+   */
+  protected InjectedContext context;
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    protected PatternInjectedCallImpl() {
-        super();
-    }
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  protected PatternInjectedCallImpl() {
+    super();
+  }
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    protected EClass eStaticClass() {
-        return PatternPackage.Literals.PATTERN_INJECTED_CALL;
-    }
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass() {
+    return PatternPackage.Literals.PATTERN_INJECTED_CALL;
+  }
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public InjectedContext getContext() {
-        return context;
-    }
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  public InjectedContext getContext() {
+    return context;
+  }
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public void setContext(InjectedContext newContext) {
-        InjectedContext oldContext = context;
-        context = newContext;
-        if (eNotificationRequired())
-            eNotify(new ENotificationImpl(this, Notification.SET, PatternPackage.PATTERN_INJECTED_CALL__CONTEXT, oldContext, context));
-    }
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  public void setContext(InjectedContext newContext) {
+    InjectedContext oldContext = context;
+    context = newContext;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, PatternPackage.PATTERN_INJECTED_CALL__CONTEXT, oldContext, context));
+  }
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public Object eGet(int featureID, boolean resolve, boolean coreType) {
-        switch (featureID) {
-        case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
-            return getContext();
-        }
-        return super.eGet(featureID, resolve, coreType);
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType) {
+    switch (featureID) {
+    case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
+      return getContext();
     }
+    return super.eGet(featureID, resolve, coreType);
+  }
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public void eSet(int featureID, Object newValue) {
-        switch (featureID) {
-        case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
-            setContext((InjectedContext) newValue);
-            return;
-        }
-        super.eSet(featureID, newValue);
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  @Override
+  public void eSet(int featureID, Object newValue) {
+    switch (featureID) {
+    case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
+      setContext((InjectedContext) newValue);
+      return;
     }
+    super.eSet(featureID, newValue);
+  }
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public void eUnset(int featureID) {
-        switch (featureID) {
-        case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
-            setContext((InjectedContext) null);
-            return;
-        }
-        super.eUnset(featureID);
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID) {
+    switch (featureID) {
+    case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
+      setContext((InjectedContext) null);
+      return;
     }
+    super.eUnset(featureID);
+  }
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public boolean eIsSet(int featureID) {
-        switch (featureID) {
-        case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
-            return context != null;
-        }
-        return super.eIsSet(featureID);
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID) {
+    switch (featureID) {
+    case PatternPackage.PATTERN_INJECTED_CALL__CONTEXT:
+      return context != null;
     }
+    return super.eIsSet(featureID);
+  }
 
 } // PatternInjectedCallImpl
