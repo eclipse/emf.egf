d46636cf2af3fe62973a7cf3ccef37816632316e
Thomas Guiu <thomas.guiu@soyatec.com>
2010-04-23 14:10:00 +0000

    add pde requiredPlugins constants.

--- plugins/org.eclipse.egf.common/src/org/eclipse/egf/common/constant/EGFCommonConstants.java
+++ plugins/org.eclipse.egf.common/src/org/eclipse/egf/common/constant/EGFCommonConstants.java
@@ -106,6 +106,9 @@ public interface EGFCommonConstants {
   public final static String BUNDLE_FILENAME_DIRECTORY_DESCRIPTOR = "META-INF/"; //$NON-NLS-1$  
 
   /** Constant for the string <code>META-INF/MANIFEST.MF</code> */
-  public final static String BUNDLE_FILENAME_DESCRIPTOR = BUNDLE_FILENAME_DIRECTORY_DESCRIPTOR + "MANIFEST.MF"; //$NON-NLS-1$  
+  public final static String BUNDLE_FILENAME_DESCRIPTOR = BUNDLE_FILENAME_DIRECTORY_DESCRIPTOR + "MANIFEST.MF"; //$NON-NLS-1$
+
+  /** Constant for the string <code>org.eclipse.pde.core.requiredPlugins</code> */
+  public final static String REQUIRED_PLUGINS_CLASSPATH_CONTAINER = "org.eclipse.pde.core.requiredPlugins"; //$NON-NLS-1$
 
 }
