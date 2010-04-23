97344082616fbfc5cf1d5297c0640ae92811026e
Thomas Guiu <thomas.guiu@soyatec.com>
2010-04-23 14:07:25 +0000

    remove wrong PLUGIN_ID

--- plugins/org.eclipse.egf.pattern.ftask/src/org/eclipse/egf/pattern/ftask/Activator.java
+++ plugins/org.eclipse.egf.pattern.ftask/src/org/eclipse/egf/pattern/ftask/Activator.java
@@ -8,49 +8,40 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends EGFAbstractPlugin {
 
-    // The plug-in ID
-    public static final String PLUGIN_ID = "org.eclipse.egf.pattern.fprod";
+  // The shared instance
+  private static Activator __plugin;
 
-    // The shared instance
-    private static Activator plugin;
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    __plugin = this;
+  }
 
-    /**
-     * The constructor
-     */
-    public Activator() {
-    }
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    __plugin = null;
+    super.stop(context);
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-     * )
-     */
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-     * )
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
-
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static Activator getDefault() {
-        return plugin;
-    }
+  /**
+   * Returns the shared instance
+   * 
+   * @return the shared instance
+   */
+  public static Activator getDefault() {
+    return __plugin;
+  }
 
 }
