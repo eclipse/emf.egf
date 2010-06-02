690078d991f39ef00ba3e8ab836a3e3adb2ba15d
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-02 10:57:26 +0000

    initial commit

---
+++ examples/workspace/org.eclipse.egf.usecase.pattern.uc2/generated/egf/uc2_1/main/uc2_1_ClassPattern.java
@@ -0,0 +1,100 @@
+package egf.uc2_1.main;
+
+import org.eclipse.egf.common.helper.*;
+import java.util.*;
+import org.eclipse.emf.ecore.*;
+import org.eclipse.egf.model.pattern.*;
+import org.eclipse.egf.pattern.execution.*;
+import org.eclipse.egf.pattern.query.*;
+
+public class uc2_1_ClassPattern 
+{
+  protected static String nl;
+  public static synchronized uc2_1_ClassPattern create(String lineSeparator)
+  {
+    nl = lineSeparator;
+    uc2_1_ClassPattern result = new uc2_1_ClassPattern();
+    nl = null;
+    return result;
+  }
+
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  protected final String TEXT_1 = "* Class ";
+  protected final String TEXT_2 = ": ";
+  protected final String TEXT_3 = NL;
+  protected final String TEXT_4 = NL;
+
+	public uc2_1_ClassPattern()
+	{
+	//Here is the constructor
+StringBuffer stringBuffer = new StringBuffer();
+
+    // add initialisation of the pattern variables (declaration has been already done).
+    
+	}
+  	
+  	public String generate(Object argument) throws Exception
+  {
+    final StringBuffer stringBuffer = new StringBuffer();
+    
+    InternalPatternContext ctx = (InternalPatternContext)argument;
+Map<String, String> queryCtx = null;
+IQuery.ParameterDescription paramDesc = null;
+
+    
+List<Object> aClassList = null;
+//this pattern can only be called by another (i.e. it's not an entry point in execution)
+
+
+for (Object aClassParameter : aClassList ) {
+
+this.aClass = (org.eclipse.emf.ecore.EClass)aClassParameter;
+
+
+    orchestration(ctx);
+    
+}
+if (ctx.useReporter()){
+    ctx.getReporter().executionFinished(ctx.getExecutionBuffer().toString(), ctx);
+    ctx.clearBuffer();
+}
+    
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append(TEXT_4);
+    return stringBuffer.toString();
+  }
+public String orchestration(PatternContext ctx) throws Exception  {
+InternalPatternContext ictx = (InternalPatternContext)ctx;
+int executionIndex = ictx.getExecutionBuffer().length();
+
+    
+method_body(ictx.getBuffer(), ictx);
+    
+String loop = ictx.getBuffer().toString();
+if (ictx.useReporter()){
+    ictx.getExecutionBuffer().append(ictx.getBuffer().substring(ictx.getExecutionCurrentIndex()));
+    ictx.setExecutionCurrentIndex(0);
+    Map<String, Object> parameterValues = new HashMap<String, Object>();
+    parameterValues.put("aClass", this.aClass);
+    String outputWithCallBack = ictx.getExecutionBuffer().substring(executionIndex);
+    ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+    ictx.clearBuffer();}
+return loop;
+} 
+
+protected org.eclipse.emf.ecore.EClass aClass = null;
+public void set_aClass(org.eclipse.emf.ecore.EClass object) {
+this.aClass = object;
+}
+public Map<String, Object> getParameters() {
+final Map<String, Object> parameters = new HashMap<String, Object>();
+parameters.put("aClass", this.aClass);
+return parameters; }
+
+    protected void method_body(final StringBuffer stringBuffer, final PatternContext ctx)throws Exception {
+
+    stringBuffer.append(TEXT_1);
+    stringBuffer.append(aClass.getName());
+    stringBuffer.append(TEXT_2);
+    }
+    }
