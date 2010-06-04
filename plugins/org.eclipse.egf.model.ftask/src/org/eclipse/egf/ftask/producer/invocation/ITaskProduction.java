13995f8508a5296a6213caff3c5709935e54bc59
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-04 16:20:50 +0000

    format update.

--- plugins/org.eclipse.egf.model.ftask/src/org/eclipse/egf/ftask/producer/invocation/ITaskProduction.java
+++ plugins/org.eclipse.egf.model.ftask/src/org/eclipse/egf/ftask/producer/invocation/ITaskProduction.java
@@ -20,25 +20,25 @@ import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
  */
 public interface ITaskProduction {
 
-  /**
-   * Pre execute this task.
-   * 
-   * @param monitor_p
-   */
-  public void preExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor) throws InvocationException;
+    /**
+     * Pre execute this task.
+     * 
+     * @param monitor
+     */
+    public void preExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor) throws InvocationException;
 
-  /**
-   * Do execute this task.
-   * 
-   * @param monitor_p
-   */
-  public void doExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor) throws InvocationException;
+    /**
+     * Do execute this task.
+     * 
+     * @param monitor
+     */
+    public void doExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor) throws InvocationException;
 
-  /**
-   * Post execute this task.
-   * 
-   * @param monitor_p
-   */
-  public void postExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor) throws InvocationException;
+    /**
+     * Post execute this task.
+     * 
+     * @param monitor
+     */
+    public void postExecute(final ITaskProductionContext productionContext, final IProgressMonitor monitor) throws InvocationException;
 
 }
