5f2c5632f6e338cf9742a552d4ac3a8bfe594b04
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-04 18:55:22 +0000

    remove ExtensionHelper dependency

--- plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/templateEditor/TemplateExtensionRegistry.java
+++ plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/templateEditor/TemplateExtensionRegistry.java
@@ -1,15 +1,15 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
- *      XiaoRu Chen, Soyatec 
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ * XiaoRu Chen, Soyatec
  * 
  * </copyright>
  */
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.egf.common.helper.ExtensionPointHelper;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.egf.model.pattern.Pattern;
 import org.eclipse.egf.model.pattern.PatternNature;
 import org.eclipse.egf.pattern.ui.Activator;
@@ -30,10 +30,14 @@ import org.eclipse.egf.pattern.ui.Activator;
  * 
  */
 public class TemplateExtensionRegistry {
-    protected static final String EXTENSION_POINT_EDITOR = "templateEditor";
-    protected static final String EXTENSION_Id = "id";
-    protected static final String EXTENSION_POINT_NATURE = "nature";
-    private static Map<String, String> editors;
+
+    protected static final String EXTENSION_POINT_EDITOR = "templateEditor"; //$NON-NLS-1$
+
+    protected static final String EXTENSION_Id = "id"; //$NON-NLS-1$
+
+    protected static final String EXTENSION_POINT_NATURE = "nature"; //$NON-NLS-1$
+
+    private static Map<String, String> _editors;
 
     public static String getEditor(Pattern pattern) {
         PatternNature nature = pattern.getNature();
@@ -46,15 +50,15 @@ public class TemplateExtensionRegistry {
     }
 
     public static Map<String, String> getEditors() {
-        if (editors == null)
-            editors = new HashMap<String, String>();
-        for (IConfigurationElement configurationElement : ExtensionPointHelper.getConfigurationElements(Activator.getDefault().getPluginID(), EXTENSION_POINT_EDITOR)) {
-            String editorId = ExtensionPointHelper.getAttributeValue(configurationElement, EXTENSION_Id);
-            String natureName = ExtensionPointHelper.getAttributeValue(configurationElement, EXTENSION_POINT_NATURE);
-
-            editors.put(natureName, editorId);
+        if (_editors == null) {
+            _editors = new HashMap<String, String>();
         }
-        return editors;
+        for (IConfigurationElement element : Platform.getExtensionRegistry().getConfigurationElementsFor(Activator.getDefault().getPluginID(), EXTENSION_POINT_EDITOR)) {
+            String editorId = element.getAttribute(EXTENSION_Id);
+            String natureName = element.getAttribute(EXTENSION_POINT_NATURE);
+            _editors.put(natureName, editorId);
+        }
+        return _editors;
     }
 
 }
