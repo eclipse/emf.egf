d67f0531cfa112413bbf3e8f1da81aaa2e47798e
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-04 15:39:37 +0000

    new platform genmodel support.

---
+++ plugins/org.eclipse.egf.core/src/org/eclipse/egf/core/internal/genmodel/PlatformGenModel.java
@@ -0,0 +1,79 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ * </copyright>
+ * 
+ */
+package org.eclipse.egf.core.internal.genmodel;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.egf.common.helper.URIHelper;
+import org.eclipse.egf.core.genmodel.IPlatformGenModel;
+import org.eclipse.egf.core.platform.pde.IPlatformBundle;
+import org.eclipse.egf.core.platform.pde.PlatformExtensionPointURI;
+import org.eclipse.emf.common.util.URI;
+
+public final class PlatformGenModel extends PlatformExtensionPointURI implements IPlatformGenModel {
+
+    private String _generatedPackage;
+
+    private String _genModel;
+
+    private URI _genModelURI;
+
+    public PlatformGenModel(IPlatformBundle bundle, String id, String className, String genModel) {
+        super(bundle, id);
+        Assert.isNotNull(className);
+        Assert.isLegal(className.trim().length() != 0);
+        _generatedPackage = className.trim();
+        if (genModel != null && genModel.trim().length() != 0) {
+            _genModel = genModel.trim();
+            _genModelURI = URIHelper.getPlatformURI(getPlatformBundle().getBundleId(), URI.decode(_genModel), false);
+        }
+    }
+
+    public String getNamespace() {
+        return getURI().toString();
+    }
+
+    public String getGeneratedPackage() {
+        return _generatedPackage;
+    }
+
+    public String getGenModel() {
+        return _genModel;
+    }
+
+    public URI getGenModelURI() {
+        return _genModelURI;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (super.equals(object) == false) {
+            return false;
+        }
+        if (object instanceof IPlatformGenModel == false) {
+            return false;
+        }
+        IPlatformGenModel platformGenModel = (IPlatformGenModel) object;
+        if (platformGenModel.getGeneratedPackage().equals(getGeneratedPackage()) == false) {
+            return false;
+        }
+        if (platformGenModel.getGenModel() != null && getGenModel() != null) {
+            if (platformGenModel.getGenModel().equals(getGenModel()) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ plugins/org.eclipse.egf.core/src/org/eclipse/egf/core/internal/genmodel/PlatformGenModelFactory.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ */
+package org.eclipse.egf.core.internal.genmodel;
+
+import org.eclipse.egf.core.genmodel.IPlatformGenModel;
+import org.eclipse.egf.core.genmodel.IPlatformGenModelConstants;
+import org.eclipse.egf.core.platform.pde.IPlatformBundle;
+import org.eclipse.egf.core.platform.pde.IPlatformExtensionPointFactory;
+import org.eclipse.pde.core.plugin.IPluginAttribute;
+import org.eclipse.pde.core.plugin.IPluginElement;
+
+/**
+ * @author Xavier Maysonnave
+ * 
+ */
+public final class PlatformGenModelFactory implements IPlatformExtensionPointFactory<IPlatformGenModel> {
+
+    public IPlatformGenModel createExtensionPoint(IPlatformBundle platformBundle, IPluginElement pluginElement) {
+        if (pluginElement == null) {
+            return null;
+        }
+        // platform:/plugin/org.eclipse.emf.ecore/schema/generated_package.exsd
+        if (IPlatformGenModelConstants.PACKAGE_EXTENSION_CHILD.equals(pluginElement.getName()) == false) {
+            return null;
+        }
+        // the uri attribute is mandatory
+        IPluginAttribute uri = pluginElement.getAttribute(IPlatformGenModelConstants.PACKAGE_ATT_URI);
+        if (uri == null || uri.getValue() == null || uri.getValue().trim().length() == 0) {
+            return null;
+        }
+        // the class attribute is mandatory
+        IPluginAttribute className = pluginElement.getAttribute(IPlatformGenModelConstants.PACKAGE_ATT_CLASS);
+        if (className == null || className.getValue() == null || className.getValue().trim().length() == 0) {
+            return null;
+        }
+        // the genModel attribute is optional
+        IPluginAttribute genModel = pluginElement.getAttribute(IPlatformGenModelConstants.PACKAGE_ATT_GENMODEL);
+        // Create an IPlatformGenModel
+        return new PlatformGenModel(platformBundle, uri.getValue(), className.getValue(), genModel != null ? genModel.getValue() : null);
+    }
+}
