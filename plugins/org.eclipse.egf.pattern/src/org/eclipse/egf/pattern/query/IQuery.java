db6a0eb30ec1122ec501008b8ccdbe0231c7102a
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-04 16:32:42 +0000

    format update.

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/query/IQuery.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/query/IQuery.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
@@ -35,7 +35,9 @@ public interface IQuery {
     Helper INSTANCE = new Helper();
 
     public class ParameterDescription {
+
         private String name;
+
         private String type;
 
         public ParameterDescription(String name, String type) {
