e2f577e3346f17e00ca9b5981d6f4e9679e05301
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-07 10:34:07 +0000

    javadoc fix

--- plugins/org.eclipse.egf.application/src/org/eclipse/egf/application/internal/activator/EGFApplicationPlugin.java
+++ plugins/org.eclipse.egf.application/src/org/eclipse/egf/application/internal/activator/EGFApplicationPlugin.java
@@ -8,38 +8,38 @@ import org.osgi.framework.BundleContext;
  */
 public class EGFApplicationPlugin extends EGFAbstractPlugin {
 
-  // The shared instance
-  private static EGFApplicationPlugin _plugin;
+    // The shared instance
+    private static EGFApplicationPlugin _plugin;
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void start(BundleContext context) throws Exception {
-    super.start(context);
-    _plugin = this;
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        _plugin = this;
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void stop(BundleContext context) throws Exception {
-    _plugin = null;
-    super.stop(context);
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        _plugin = null;
+        super.stop(context);
+    }
 
-  /**
-   * Returns the shared instance
-   * 
-   * @return the shared instance
-   */
-  public static EGFApplicationPlugin getDefault() {
-    return _plugin;
-  }
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static EGFApplicationPlugin getDefault() {
+        return _plugin;
+    }
 
 }
