9d239b68ebfa5d72c600f3617ad9cde2f8f27c78
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 15:25:58 +0000

    remove unnecessary cast

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/strategy/AbstractStrategy.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/strategy/AbstractStrategy.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
@@ -28,6 +28,7 @@ public abstract class AbstractStrategy implements Strategy {
     protected PatternElement[] patternElements;
 
     public void setPatternElements(List<PatternElement> patterns) {
-        this.patternElements = (PatternElement[]) patterns.toArray(new PatternElement[patterns.size()]);
+        this.patternElements = patterns.toArray(new PatternElement[patterns.size()]);
     }
+
 }
