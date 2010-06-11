8d60eb0df86ffb156248ecadfdc0c2be7b5dd331
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 11:50:08 +0000

    prevent a stack overflow if a proxy is not resolved

--- plugins/org.eclipse.egf.model.edit/src/org/eclipse/egf/model/pattern/provider/PatternCustomItemProvider.java
+++ plugins/org.eclipse.egf.model.edit/src/org/eclipse/egf/model/pattern/provider/PatternCustomItemProvider.java
@@ -25,54 +25,54 @@ import org.eclipse.emf.edit.provider.IItemLabelProvider;
  */
 public class PatternCustomItemProvider extends PatternItemProvider {
 
-  /**
-   * This constructs an instance from a factory and a notifier.
-   */
-  public PatternCustomItemProvider(AdapterFactory adapterFactory) {
-    super(adapterFactory);
-  }
+    /**
+     * This constructs an instance from a factory and a notifier.
+     */
+    public PatternCustomItemProvider(AdapterFactory adapterFactory) {
+        super(adapterFactory);
+    }
 
-  /**
-   * This returns the label text for the adapted class.
-   */
-  @Override
-  public String getText(Object object) {
-    Pattern pattern = (Pattern) object;
-    EObject root = EcoreUtil.getRootContainer(pattern, true);
-    // Activity
-    String activity = null;
-    if (root != null) {
-      IItemLabelProvider provider = (IItemLabelProvider) (((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory()).adapt(root, IItemLabelProvider.class);
-      if (provider == null) {
-        activity = EMFHelper.getText(root);
-      } else {
-        activity = provider.getText(root);
-      }
+    /**
+     * This returns the label text for the adapted class.
+     */
+    @Override
+    public String getText(Object object) {
+        Pattern pattern = (Pattern) object;
+        EObject root = EcoreUtil.getRootContainer(pattern, true);
+        // Activity
+        String activity = null;
+        if (root != null) {
+            IItemLabelProvider provider = (IItemLabelProvider) (((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory()).adapt(root, IItemLabelProvider.class);
+            if (provider == null || pattern == root) {
+                activity = EMFHelper.getText(root);
+            } else {
+                activity = provider.getText(root);
+            }
+        }
+        // Library
+        String container = null;
+        if (pattern.getContainer() != null) {
+            IItemLabelProvider provider = (IItemLabelProvider) (((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory()).adapt(pattern.getContainer(), IItemLabelProvider.class);
+            if (provider == null) {
+                container = EMFHelper.getText(pattern.getContainer());
+            } else {
+                container = provider.getText(pattern.getContainer());
+            }
+        }
+        // Name
+        String name = pattern.getName();
+        // Build label
+        String label = "[" + getString("_UI_Pattern_type") + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        if (name != null && name.trim().length() != 0) {
+            label = name + " " + label; //$NON-NLS-1$
+        }
+        if (container != null && container.trim().length() != 0) {
+            label = container + " -> " + label; //$NON-NLS-1$
+        }
+        if (activity != null && activity.trim().length() != 0) {
+            label = activity + " -> " + label; //$NON-NLS-1$
+        }
+        return label;
     }
-    // Library
-    String container = null;
-    if (pattern.getContainer() != null) {
-      IItemLabelProvider provider = (IItemLabelProvider) (((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory()).adapt(pattern.getContainer(), IItemLabelProvider.class);
-      if (provider == null) {
-        container = EMFHelper.getText(pattern.getContainer());
-      } else {
-        container = provider.getText(pattern.getContainer());
-      }
-    }
-    // Name
-    String name = pattern.getName();
-    // Build label
-    String label = "[" + getString("_UI_Pattern_type") + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    if (name != null && name.trim().length() != 0) {
-      label = name + " " + label; //$NON-NLS-1$
-    }
-    if (container != null && container.trim().length() != 0) {
-      label = container + " -> " + label; //$NON-NLS-1$
-    }
-    if (activity != null && activity.trim().length() != 0) {
-      label = activity + " -> " + label; //$NON-NLS-1$
-    }
-    return label;
-  }
 
 }
