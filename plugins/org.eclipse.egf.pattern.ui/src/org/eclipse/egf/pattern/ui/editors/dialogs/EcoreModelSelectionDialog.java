6d1fa72ff9f9db00d4d2264befc32a5e67b27c23
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 13:15:39 +0000

    target platform aware selection dialog

--- plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/dialogs/EcoreModelSelectionDialog.java
+++ plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/dialogs/EcoreModelSelectionDialog.java
@@ -1,42 +1,35 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
- *      XiaoRu Chen, Soyatec 
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ * XiaoRu Chen, Soyatec
  * 
  * </copyright>
  */
 
 package org.eclipse.egf.pattern.ui.editors.dialogs;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.egf.common.helper.EMFHelper;
+import org.eclipse.egf.core.EGFCorePlugin;
+import org.eclipse.egf.core.domain.EGFResourceSet;
+import org.eclipse.egf.core.genmodel.IPlatformGenModel;
+import org.eclipse.egf.pattern.ui.Messages;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EPackage.Registry;
-import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.presentation.EcoreEditorPlugin;
 import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.ui.action.LoadResourceAction.LoadResourceDialog;
@@ -61,47 +54,9 @@ import org.eclipse.ui.dialogs.ElementListSelectionDialog;
  * 
  */
 public class EcoreModelSelectionDialog extends LoadResourceDialog {
-    protected Set<EPackage> registeredPackages = new LinkedHashSet<EPackage>();
 
     public EcoreModelSelectionDialog(Shell parent, EditingDomain domain) {
         super(parent, domain);
-    }
-
-    @Override
-    protected boolean processResource(Resource resource) {
-        // Put all static package in the package registry.
-        //
-        ResourceSet resourceSet = domain.getResourceSet();
-        if (!resourceSet.getResources().contains(resource)) {
-            Registry packageRegistry = resourceSet.getPackageRegistry();
-            for (EPackage ePackage : getAllPackages(resource)) {
-                packageRegistry.put(ePackage.getNsURI(), ePackage);
-                registeredPackages.add(ePackage);
-            }
-        }
-        return true;
-    }
-
-    public Set<EPackage> getRegisteredPackages() {
-        return registeredPackages;
-    }
-
-    protected Collection<EPackage> getAllPackages(Resource resource) {
-        List<EPackage> result = new ArrayList<EPackage>();
-        for (TreeIterator<?> j = new EcoreUtil.ContentTreeIterator<Object>(resource.getContents()) {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            protected Iterator<? extends EObject> getEObjectChildren(EObject eObject) {
-                return eObject instanceof EPackage ? ((EPackage) eObject).getESubpackages().iterator() : Collections.<EObject> emptyList().iterator();
-            }
-        }; j.hasNext();) {
-            Object content = j.next();
-            if (content instanceof EPackage) {
-                result.add((EPackage) content);
-            }
-        }
-        return result;
     }
 
     @Override
@@ -109,12 +64,11 @@ public class EcoreModelSelectionDialog extends LoadResourceDialog {
         Composite composite = (Composite) super.createDialogArea(parent);
         Composite buttonComposite = (Composite) composite.getChildren()[0];
         Button browseRegisteredPackagesButton = new Button(buttonComposite, SWT.PUSH);
-        browseRegisteredPackagesButton.setText(EcoreEditorPlugin.INSTANCE.getString("_UI_BrowseRegisteredPackages_label"));
+        browseRegisteredPackagesButton.setText(EcoreEditorPlugin.INSTANCE.getString("_UI_BrowseRegisteredPackages_label")); //$NON-NLS-1$
         prepareBrowseRegisteredPackagesButton(browseRegisteredPackagesButton);
         {
             FormData data = new FormData();
             Control[] children = buttonComposite.getChildren();
-            data.left = new FormAttachment(0, 0);
             data.right = new FormAttachment(children[0], -CONTROL_OFFSET);
             browseRegisteredPackagesButton.setLayoutData(data);
         }
@@ -123,42 +77,50 @@ public class EcoreModelSelectionDialog extends LoadResourceDialog {
 
     protected void prepareBrowseRegisteredPackagesButton(Button browseRegisteredPackagesButton) {
         browseRegisteredPackagesButton.addSelectionListener(new SelectionAdapter() {
+
             @Override
             public void widgetSelected(SelectionEvent event) {
                 RegisteredPackageDialog registeredPackageDialog = new RegisteredPackageDialog(getShell());
                 registeredPackageDialog.open();
                 Object[] result = registeredPackageDialog.getResult();
-                if (result != null) {
-                    List<?> nsURIs = Arrays.asList(result);
-                    if (registeredPackageDialog.isDevelopmentTimeVersion()) {
-                        ResourceSet resourceSet = new ResourceSetImpl();
-                        resourceSet.getURIConverter().getURIMap().putAll(EcorePlugin.computePlatformURIMap());
-                        StringBuffer uris = new StringBuffer();
-                        Map<String, URI> ePackageNsURItoGenModelLocationMap = EcorePlugin.getEPackageNsURIToGenModelLocationMap();
-                        for (int i = 0, length = result.length; i < length; i++) {
-                            URI location = ePackageNsURItoGenModelLocationMap.get(result[i]);
-                            Resource resource = resourceSet.getResource(location, true);
-                            EcoreUtil.resolveAll(resource);
+                if (result == null) {
+                    return;
+                }
+                StringBuffer uris = new StringBuffer();
+                if (registeredPackageDialog.isTarget()) {
+                    ResourceSet resourceSet = new EGFResourceSet();
+                    Map<String, URI> ePackageNsURItoGenModelLocationMap = EGFCorePlugin.getEPackageNsURIToGenModelLocationMap();
+                    for (int i = 0, length = result.length; i < length; i++) {
+                        IPlatformGenModel genmodel = (IPlatformGenModel) result[i];
+                        // TODO: nothing is done yet when no genmodel resource is associated
+                        if (genmodel.getURI() == null) {
+                            continue;
                         }
-                        for (Resource resource : resourceSet.getResources()) {
-                            for (EPackage ePackage : getAllPackages(resource)) {
-                                if (nsURIs.contains(ePackage.getNsURI())) {
+                        URI location = ePackageNsURItoGenModelLocationMap.get(genmodel.getURI().toString());
+                        Resource resource = resourceSet.getResource(location, true);
+                        EcoreUtil.resolveAll(resource);
+                    }
+                    for (Resource resource : resourceSet.getResources()) {
+                        LOOP: for (EPackage ePackage : EMFHelper.getAllPackages(resource)) {
+                            for (int i = 0, length = result.length; i < length; i++) {
+                                if (((IPlatformGenModel) result[i]).getId().compareTo(ePackage.getNsURI()) == 0) {
                                     URI resourceUri = resource.getURI();
                                     uris.append(resourceUri);
-                                    uris.append("  ");
-                                    break;
+                                    uris.append("  "); //$NON-NLS-1$
+                                    continue LOOP;
                                 }
                             }
                         }
-                        uriField.setText((uriField.getText() + "  " + uris.toString()).trim());
-                    } else {
-                        StringBuffer uris = new StringBuffer();
-                        for (int i = 0, length = result.length; i < length; i++) {
-                            uris.append(result[i]);
-                            uris.append("  ");
-                        }
-                        uriField.setText((uriField.getText() + "  " + uris.toString()).trim());
                     }
+                    uriField.setText(""); //$NON-NLS-1$
+                    uriField.setText((uriField.getText() + "  " + uris.toString()).trim()); //$NON-NLS-1$
+                } else {
+                    for (int i = 0, length = result.length; i < length; i++) {
+                        uris.append(result[i]);
+                        uris.append("  "); //$NON-NLS-1$
+                    }
+                    uriField.setText(""); //$NON-NLS-1$
+                    uriField.setText((uriField.getText() + "  " + uris.toString()).trim()); //$NON-NLS-1$
                 }
             }
         });
@@ -166,37 +128,46 @@ public class EcoreModelSelectionDialog extends LoadResourceDialog {
     }
 
     public static class RegisteredPackageDialog extends ElementListSelectionDialog {
-        protected boolean isDevelopmentTimeVersion = true;
+
+        protected boolean isTarget = true;
 
         public RegisteredPackageDialog(Shell parent) {
             super(parent, new LabelProvider() {
+
+                @Override
+                public String getText(Object element) {
+                    if (element == null || element instanceof IPlatformGenModel == false) {
+                        return element == null ? "" : element.toString();//$NON-NLS-1$
+                    }
+                    return ((IPlatformGenModel) element).getURI().toString();
+                }
+
                 @Override
                 public Image getImage(Object element) {
-                    return ExtendedImageRegistry.getInstance().getImage(EcoreEditPlugin.INSTANCE.getImage("full/obj16/EPackage"));
+                    return ExtendedImageRegistry.getInstance().getImage(EcoreEditPlugin.INSTANCE.getImage("full/obj16/EPackage")); //$NON-NLS-1$
                 }
+
             });
 
             setMultipleSelection(true);
-            setMessage(EcoreEditorPlugin.INSTANCE.getString("_UI_SelectRegisteredPackageURI"));
-            setFilter("*");
-            setTitle(EcoreEditorPlugin.INSTANCE.getString("_UI_PackageSelection_label"));
+            setMessage(EcoreEditorPlugin.INSTANCE.getString("_UI_SelectRegisteredPackageURI")); //$NON-NLS-1$
+            setFilter("*"); //$NON-NLS-1$
+            setTitle(EcoreEditorPlugin.INSTANCE.getString("_UI_PackageSelection_label")); //$NON-NLS-1$
         }
 
-        public boolean isDevelopmentTimeVersion() {
-            return isDevelopmentTimeVersion;
+        public boolean isTarget() {
+            return isTarget;
         }
 
         protected void updateElements() {
-            if (isDevelopmentTimeVersion) {
-                Map<String, URI> ePackageNsURItoGenModelLocationMap = EcorePlugin.getEPackageNsURIToGenModelLocationMap();
-                Object[] result = ePackageNsURItoGenModelLocationMap.keySet().toArray(new Object[ePackageNsURItoGenModelLocationMap.size()]);
-                Arrays.sort(result);
-                setListElements(result);
+            Object[] result = null;
+            if (isTarget) {
+                result = EGFCorePlugin.getPlatformGenModels();
             } else {
-                Object[] result = EPackage.Registry.INSTANCE.keySet().toArray(new Object[EPackage.Registry.INSTANCE.size()]);
-                Arrays.sort(result);
-                setListElements(result);
+                result = EPackage.Registry.INSTANCE.keySet().toArray(new Object[EPackage.Registry.INSTANCE.size()]);
             }
+            Arrays.sort(result);
+            setListElements(result);
         }
 
         @Override
@@ -206,21 +177,21 @@ public class EcoreModelSelectionDialog extends LoadResourceDialog {
             GridLayout layout = new GridLayout();
             layout.numColumns = 2;
             buttonGroup.setLayout(layout);
-            final Button developmentTimeVersionButton = new Button(buttonGroup, SWT.RADIO);
-            developmentTimeVersionButton.addSelectionListener(new SelectionAdapter() {
+            final Button targetButton = new Button(buttonGroup, SWT.RADIO);
+            targetButton.addSelectionListener(new SelectionAdapter() {
+
                 @Override
                 public void widgetSelected(SelectionEvent event) {
-                    isDevelopmentTimeVersion = developmentTimeVersionButton.getSelection();
+                    isTarget = targetButton.getSelection();
                     updateElements();
                 }
+
             });
-            developmentTimeVersionButton.setText(EcoreEditorPlugin.INSTANCE.getString("_UI_DevelopmentTimeVersion_label"));
+            targetButton.setText(Messages._UI_TargetPlatform_label);
             Button runtimeTimeVersionButton = new Button(buttonGroup, SWT.RADIO);
-            runtimeTimeVersionButton.setText(EcoreEditorPlugin.INSTANCE.getString("_UI_RuntimeVersion_label"));
-            developmentTimeVersionButton.setSelection(true);
-
+            runtimeTimeVersionButton.setText(Messages._UI_RuntimeVersion_label);
+            targetButton.setSelection(true);
             updateElements();
-
             return result;
         }
     }
