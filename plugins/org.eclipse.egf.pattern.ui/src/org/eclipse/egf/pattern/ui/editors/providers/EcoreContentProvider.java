90e6624a802ba3f9a4560d18a02b588ae10b3634
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 13:18:00 +0000

    IPlatformGenModel aware label and content provider.

--- plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/providers/EcoreContentProvider.java
+++ plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/providers/EcoreContentProvider.java
@@ -1,15 +1,15 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
- *      XiaoRu Chen, Soyatec 
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ * XiaoRu Chen, Soyatec
  * 
  * </copyright>
  */
@@ -30,6 +30,7 @@ import org.eclipse.jface.viewers.Viewer;
  * 
  */
 public class EcoreContentProvider implements ITreeContentProvider {
+
     public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
     }
 
@@ -55,10 +56,9 @@ public class EcoreContentProvider implements ITreeContentProvider {
         if (parentElement instanceof Resource) {
             Resource res = (Resource) parentElement;
             return res.getContents().toArray();
-        }
-        if (parentElement instanceof List<?>)
+        } else if (parentElement instanceof List<?>) {
             return ((List<?>) parentElement).toArray();
-        if (parentElement instanceof EPackage) {
+        } else if (parentElement instanceof EPackage) {
             EPackage ePack = (EPackage) parentElement;
             return ePack.eContents().toArray();
         }
--- plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/providers/EcoreLabelProvider.java
+++ plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/providers/EcoreLabelProvider.java
@@ -1,15 +1,15 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
- *      XiaoRu Chen, Soyatec 
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S. and other
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ * XiaoRu Chen, Soyatec
  * 
  * </copyright>
  */
@@ -45,7 +45,7 @@ public class EcoreLabelProvider implements ITableLabelProvider, ILabelProvider {
             ENamedElement eNamedElement = (ENamedElement) element;
             return eNamedElement.getName();
         }
-        return "";
+        return ""; //$NON-NLS-1$
     }
 
     public void addListener(ILabelProviderListener listener) {
