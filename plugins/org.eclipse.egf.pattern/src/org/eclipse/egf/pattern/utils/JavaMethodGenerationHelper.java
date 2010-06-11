e135f27a850c9b58d8e4d1e3ce5e10f6642b4dfa
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 15:23:21 +0000

    add missing nls

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/utils/JavaMethodGenerationHelper.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/utils/JavaMethodGenerationHelper.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
@@ -26,6 +26,7 @@ import org.eclipse.egf.pattern.extension.PatternFactory;
 public class JavaMethodGenerationHelper {
 
     private final String bufferName;
+
     private StringBuilder content;
 
     public JavaMethodGenerationHelper(String bufferName) {
@@ -33,15 +34,15 @@ public class JavaMethodGenerationHelper {
     }
 
     public void addCallStatement(PatternMethod called) {
-        content.append("method_").append(called.getName()).append("(ictx.getBuffer(), ictx);");
+        content.append("method_").append(called.getName()).append("(ictx.getBuffer(), ictx);"); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
     public void addSignature(PatternMethod called) {
-        content.append("protected void method_").append(called.getName()).append("(final StringBuffer ").append(bufferName).append(", final PatternContext ctx)throws Exception ");
+        content.append("protected void method_").append(called.getName()).append("(final StringBuffer ").append(bufferName).append(", final PatternContext ctx)throws Exception "); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     }
 
     public void addConditionSignature(PatternMethod called) {
-        content.append("public boolean ").append(PatternFactory.PRECONDITION_METHOD_NAME).append("() throws Exception ");
+        content.append("public boolean ").append(PatternFactory.PRECONDITION_METHOD_NAME).append("() throws Exception "); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
     public void setContent(StringBuilder content) {
@@ -50,6 +51,7 @@ public class JavaMethodGenerationHelper {
 
     public static String getSetterMethod(NamedModelElement paramOrVariable) {
         String name = paramOrVariable.getName();
-        return "set_" + name;
+        return "set_" + name; //$NON-NLS-1$
     }
+
 }
