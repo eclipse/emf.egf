3fc4b8c20c6c83edce3a53939c71a9c99111ef21
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 15:35:26 +0000

    remove useless cast

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/execution/ExecutionContext.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/execution/ExecutionContext.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
@@ -31,7 +31,7 @@ public class ExecutionContext extends DefaultInternalPatternContext {
     }
 
     public ExecutionContext(InternalPatternContext parent) {
-        super((InternalPatternContext) parent);
+        super(parent);
     }
 
     public void setReporter(PatternExecutionReporter reporter) {
