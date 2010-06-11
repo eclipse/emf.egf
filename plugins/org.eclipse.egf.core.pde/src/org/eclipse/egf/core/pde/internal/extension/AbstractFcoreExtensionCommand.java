120e841e8f14d1b5e6c413f31a621f12bfc9eb08
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 12:50:51 +0000

    remove useless dependency

--- plugins/org.eclipse.egf.core.pde/src/org/eclipse/egf/core/pde/internal/extension/AbstractFcoreExtensionCommand.java
+++ plugins/org.eclipse.egf.core.pde/src/org/eclipse/egf/core/pde/internal/extension/AbstractFcoreExtensionCommand.java
@@ -13,7 +13,6 @@ package org.eclipse.egf.core.pde.internal.extension;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.egf.common.helper.ExtensionPointHelper;
 import org.eclipse.egf.common.helper.URIHelper;
 import org.eclipse.egf.core.fcore.IPlatformFcoreConstants;
 import org.eclipse.egf.core.pde.plugin.AbstractExtensionChangesCommand;
@@ -26,68 +25,68 @@ import org.eclipse.emf.common.util.URI;
  */
 public abstract class AbstractFcoreExtensionCommand extends AbstractExtensionChangesCommand {
 
-  public IPath _path;
+    public IPath _path;
 
-  /**
-   * Constructor.
-   * 
-   * @param uri
-   */
-  protected AbstractFcoreExtensionCommand(IPath path) throws CoreException {
-    super(ResourcesPlugin.getWorkspace().getRoot().getProject(path.segment(0)));
-    _path = path;
-  }
-
-  /**
-   * @see org.eclipse.egf.core.pde.plugin.AbstractExtensionChangesCommand#getExtensionChildName()
-   */
-  @Override
-  protected String getExtensionChildName() {
-    return IPlatformFcoreConstants.FCORE_EXTENSION_CHILD;
-  }
-
-  /**
-   * @see org.eclipse.egf.core.pde.plugin.AbstractExtensionChangesCommand#getExtensionPointId()
-   */
-  @Override
-  protected String getExtensionPointId() {
-    return IPlatformFcoreConstants.FULLY_QUALIFIED_EXTENSION_POINT_ID;
-  }
-
-  /**
-   * Get the id attribute of the children for the extension.
-   * 
-   * @return
-   */
-  @Override
-  protected String getExtensionChildAttribute() {
-    return ExtensionPointHelper.ATT_ID;
-  }
-
-  @Override
-  protected boolean matchValue(String value) {
-    if (value == null || value.trim().length() == 0) {
-      return false;
+    /**
+     * Constructor.
+     * 
+     * @param uri
+     */
+    protected AbstractFcoreExtensionCommand(IPath path) throws CoreException {
+        super(ResourcesPlugin.getWorkspace().getRoot().getProject(path.segment(0)));
+        _path = path;
     }
-    URI uri = URIHelper.getPlatformURI(getBundleId(), URI.decode(value), false);
-    if (uri.equals(getURI())) {
-      return true;
+
+    /**
+     * @see org.eclipse.egf.core.pde.plugin.AbstractExtensionChangesCommand#getExtensionChildName()
+     */
+    @Override
+    protected String getExtensionChildName() {
+        return IPlatformFcoreConstants.FCORE_EXTENSION_CHILD;
     }
-    return false;
-  }
 
-  @Override
-  public String getValue() {
-    return _path.removeFirstSegments(1).makeRelative().toString();
-  }
+    /**
+     * @see org.eclipse.egf.core.pde.plugin.AbstractExtensionChangesCommand#getExtensionPointId()
+     */
+    @Override
+    protected String getExtensionPointId() {
+        return IPlatformFcoreConstants.FULLY_QUALIFIED_EXTENSION_POINT_ID;
+    }
 
-  /**
-   * Get the Fcore URI.
-   * 
-   * @return
-   */
-  protected URI getURI() {
-    return URIHelper.getPlatformURI(_path);
-  }
+    /**
+     * Get the id attribute of the children for the extension.
+     * 
+     * @return
+     */
+    @Override
+    protected String getExtensionChildAttribute() {
+        return "id"; //$NON-NLS-1$
+    }
+
+    @Override
+    protected boolean matchValue(String value) {
+        if (value == null || value.trim().length() == 0) {
+            return false;
+        }
+        URI uri = URIHelper.getPlatformURI(getBundleId(), URI.decode(value), false);
+        if (uri.equals(getURI())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getValue() {
+        return _path.removeFirstSegments(1).makeRelative().toString();
+    }
+
+    /**
+     * Get the Fcore URI.
+     * 
+     * @return
+     */
+    protected URI getURI() {
+        return URIHelper.getPlatformURI(_path);
+    }
 
 }
