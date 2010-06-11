9e7a85f0432d0e237cd58ce93d91f5e702b0b0fc
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 14:17:36 +0000

    remove useless messages.

--- plugins/org.eclipse.egf.model.editor/src/org/eclipse/egf/model/editor/l10n/ModelEditorMessages.java
+++ plugins/org.eclipse.egf.model.editor/src/org/eclipse/egf/model/editor/l10n/ModelEditorMessages.java
@@ -16,35 +16,41 @@ import org.eclipse.osgi.util.NLS;
 
 public class ModelEditorMessages {
 
-  private static final String BUNDLE_NAME = "org.eclipse.egf.model.editor.l10n.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "org.eclipse.egf.model.editor.l10n.messages"; //$NON-NLS-1$
 
-  private ModelEditorMessages() {
-    // Do not instantiate
-  }
+    private ModelEditorMessages() {
+        // Do not instantiate
+    }
 
-  static {
-    // load message values from bundle file
-    NLS.initializeMessages(BUNDLE_NAME, ModelEditorMessages.class);
-  }
+    static {
+        // load message values from bundle file
+        NLS.initializeMessages(BUNDLE_NAME, ModelEditorMessages.class);
+    }
 
-  public static String ActivityMenuContributor_openAction_label;
-  public static String ActivityMenuContributor_selectAction_label;
-  public static String ActivityMenuContributor_invokeAction_label;
+    public static String ActivityMenuContributor_openAction_label;
 
-  public static String ContractMenuContributor_openAction_label;
-  public static String ContractMenuContributor_selectAction_label;
+    public static String ActivityMenuContributor_selectAction_label;
 
-  public static String TaskMenuContributor_openAction_label;
-  public static String TaskMenuContributor_selectAction_label;
+    public static String ActivityMenuContributor_invokeAction_label;
 
-  public static String URIMenuContributor_openAction_label;
-  public static String URIMenuContributor_selectAction_label;
+    public static String ContractMenuContributor_openAction_label;
 
-  public static String InvokeActivityWizard_Activity_title;
-  public static String InvokeActivityWizard_Activity_description;
-  public static String FilteredItemsSelectionDialog_patternLabel;
+    public static String ContractMenuContributor_selectAction_label;
 
-  public static String _UI_InvokeCommand_label;
-  public static String _UI_InvokeCommand_description;
+    public static String TaskMenuContributor_openAction_label;
+
+    public static String TaskMenuContributor_selectAction_label;
+
+    public static String URIMenuContributor_openAction_label;
+
+    public static String URIMenuContributor_selectAction_label;
+
+    public static String InvokeActivityWizard_Activity_title;
+
+    public static String InvokeActivityWizard_Activity_description;
+
+    public static String _UI_InvokeCommand_label;
+
+    public static String _UI_InvokeCommand_description;
 
 }
