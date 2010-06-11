eac7792526851007d04663ea3c370464b3c2f604
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-11 15:24:52 +0000

    add missing suppresswarning

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/strategy/modeldriven/ModelDrivenStrategy.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/strategy/modeldriven/ModelDrivenStrategy.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
@@ -34,6 +34,7 @@ import org.eclipse.egf.pattern.utils.SubstitutionHelper;
  */
 public class ModelDrivenStrategy extends AbstractStrategy {
 
+    @SuppressWarnings("unchecked")
     public void execute(PatternContext context, Object parameter) throws PatternException, MissingExtensionException {
         List<Pattern> result = new ArrayList<Pattern>(100);
         PatternCollector.INSTANCE.collect(patternElements, result);
