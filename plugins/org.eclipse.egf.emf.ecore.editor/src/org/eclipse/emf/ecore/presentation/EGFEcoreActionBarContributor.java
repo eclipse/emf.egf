2462bd471e6c5ecdef48df6ac12bd42563d2337e
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-14 08:22:21 +0000

    ecore target platform edition support

---
+++ plugins/org.eclipse.egf.emf.ecore.editor/src/org/eclipse/emf/ecore/presentation/EGFEcoreActionBarContributor.java
@@ -0,0 +1,97 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2002-2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * IBM - Initial API and implementation
+ * Xavier Maysonnave - Thales Corporate Services S.A.S - Target Platform contribution
+ * 
+ * </copyright>
+ * 
+ * $Id: EcoreActionBarContributor.java,v 1.19 2008/12/22 14:25:27 emerks Exp $
+ */
+package org.eclipse.emf.ecore.presentation;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.eclipse.egf.common.helper.EMFHelper;
+import org.eclipse.egf.core.ui.dialogs.LoadEcoreDialog;
+import org.eclipse.emf.common.ui.dialogs.DiagnosticDialog;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EPackage.Registry;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.ui.action.LoadResourceAction;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * This is the action bar contributor for the Ecore model editor.
+ */
+public class EGFEcoreActionBarContributor extends EcoreActionBarContributor {
+
+    public static class EGFExtendedLoadResourceAction extends LoadResourceAction {
+
+        @Override
+        public void run() {
+            Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+            EGFExtendedLoadResourceDialog loadResourceDialog = new EGFExtendedLoadResourceDialog(shell, domain);
+            if (loadResourceDialog.open() == Window.OK && loadResourceDialog.getRegisteredPackages().isEmpty() == false) {
+                String source = EcoreEditorPlugin.INSTANCE.getSymbolicName();
+                BasicDiagnostic diagnosic = new BasicDiagnostic(Diagnostic.INFO, source, 0, EcoreEditorPlugin.INSTANCE.getString("_UI_RuntimePackageDetail_message"), null); //$NON-NLS-1$
+                for (EPackage ePackage : loadResourceDialog.getRegisteredPackages()) {
+                    diagnosic.add(new BasicDiagnostic(Diagnostic.INFO, source, 0, ePackage.getNsURI(), null));
+                }
+                new DiagnosticDialog(shell, EcoreEditorPlugin.INSTANCE.getString("_UI_Information_title"), EcoreEditorPlugin.INSTANCE.getString("_UI_RuntimePackageHeader_message"), diagnosic, Diagnostic.INFO).open(); //$NON-NLS-1$  //$NON-NLS-2$
+            }
+        }
+
+    }
+
+    public static class EGFExtendedLoadResourceDialog extends LoadEcoreDialog {
+
+        protected Set<EPackage> registeredPackages = new LinkedHashSet<EPackage>();
+
+        public EGFExtendedLoadResourceDialog(Shell parent, EditingDomain domain) {
+            super(parent, domain);
+        }
+
+        @Override
+        protected boolean processResource(Resource resource) {
+            // Put all static package in the package registry.
+            //
+            ResourceSet resourceSet = domain.getResourceSet();
+            if (resourceSet.getResources().contains(resource) == false) {
+                Registry packageRegistry = resourceSet.getPackageRegistry();
+                for (EPackage ePackage : EMFHelper.getAllPackages(resource)) {
+                    packageRegistry.put(ePackage.getNsURI(), ePackage);
+                    registeredPackages.add(ePackage);
+                }
+            }
+            return true;
+        }
+
+        public Set<EPackage> getRegisteredPackages() {
+            return registeredPackages;
+        }
+    }
+
+    /**
+     * This creates an instance of the contributor.
+     */
+    public EGFEcoreActionBarContributor() {
+        super();
+        loadResourceAction = new EGFExtendedLoadResourceAction();
+    }
+
+}
---
+++ plugins/org.eclipse.egf.emf.ecore.editor/src/org/eclipse/emf/ecore/presentation/EGFEcoreModelWizard.java
@@ -0,0 +1,134 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2004-2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * IBM - Initial API and implementation
+ * Xavier Maysonnave - Thales Corporate Services S.A.S - Target Platform contribution
+ * 
+ * </copyright>
+ * 
+ * $Id: EcoreModelWizard.java,v 1.17 2008/04/27 20:30:36 davidms Exp $
+ */
+package org.eclipse.emf.ecore.presentation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egf.core.EGFCorePlugin;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.part.ISetSelectionTarget;
+
+/**
+ * @author Xavier Maysonnave
+ * 
+ */
+public class EGFEcoreModelWizard extends EcoreModelWizard {
+
+    /**
+     * Do the work after everything is specified.
+     */
+    @Override
+    public boolean performFinish() {
+        try {
+            // Remember the file.
+            //
+            final IFile modelFile = getModelFile();
+
+            // Do the work within an operation.
+            //
+            WorkspaceModifyOperation operation = new WorkspaceModifyOperation() {
+
+                @Override
+                protected void execute(IProgressMonitor progressMonitor) {
+                    try {
+                        // Create a resource set
+                        //
+                        ResourceSet resourceSet = new ResourceSetImpl();
+                        resourceSet.setURIConverter(EGFCorePlugin.getPlatformURIConverter());
+
+                        // Get the URI of the model file.
+                        //
+                        URI fileURI = URI.createPlatformResourceURI(modelFile.getFullPath().toString(), true);
+
+                        // Create a resource for this file. Don't specify a content type, as it could be Ecore or EMOF.
+                        //
+                        Resource resource = resourceSet.createResource(fileURI);
+
+                        // Add the initial model object to the contents.
+                        //
+                        EObject rootObject = createInitialModel();
+                        if (rootObject != null) {
+                            resource.getContents().add(rootObject);
+                        }
+
+                        // Save the contents of the resource to the file system.
+                        //
+                        Map<Object, Object> options = new HashMap<Object, Object>();
+                        options.put(XMLResource.OPTION_ENCODING, initialObjectCreationPage.getEncoding());
+                        resource.save(options);
+                    } catch (Exception exception) {
+                        EcoreEditorPlugin.INSTANCE.log(exception);
+                    } finally {
+                        progressMonitor.done();
+                    }
+                }
+            };
+
+            getContainer().run(false, false, operation);
+
+            // Select the new file resource in the current view.
+            //
+            IWorkbenchWindow workbenchWindow = workbench.getActiveWorkbenchWindow();
+            IWorkbenchPage page = workbenchWindow.getActivePage();
+            final IWorkbenchPart activePart = page.getActivePart();
+            if (activePart instanceof ISetSelectionTarget) {
+                final ISelection targetSelection = new StructuredSelection(modelFile);
+                getShell().getDisplay().asyncExec(new Runnable() {
+
+                    public void run() {
+                        ((ISetSelectionTarget) activePart).selectReveal(targetSelection);
+                    }
+                });
+            }
+
+            // Open an editor on the new file.
+            //
+            try {
+                IEditorDescriptor defaultEditor = workbench.getEditorRegistry().getDefaultEditor(modelFile.getFullPath().toString());
+                page.openEditor(new FileEditorInput(modelFile), defaultEditor == null ? "org.eclipse.emf.ecore.presentation.EGFEcoreEditorID" : defaultEditor.getId()); //$NON-NLS-1$
+            } catch (PartInitException exception) {
+                MessageDialog.openError(workbenchWindow.getShell(), EcoreEditorPlugin.INSTANCE.getString("_UI_OpenEditorError_label"), exception.getMessage()); //$NON-NLS-1$
+                return false;
+            }
+
+            return true;
+        } catch (Exception exception) {
+            EcoreEditorPlugin.INSTANCE.log(exception);
+            return false;
+        }
+    }
+
+}
