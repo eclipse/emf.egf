bbfb02d2483a7888fe08dc667e7a2bac84a2c4d4
Thomas Guiu <thomas.guiu@soyatec.com>
2010-06-23 07:04:06 +0000

    fix translation for java pattern: a method was missing in the generated class

--- plugins/org.eclipse.egf.pattern.java/src/org/eclipse/egf/pattern/java/engine/JavaTranslationEnhancer.java
+++ plugins/org.eclipse.egf.pattern.java/src/org/eclipse/egf/pattern/java/engine/JavaTranslationEnhancer.java
@@ -27,15 +27,21 @@ import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.FieldAccess;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.ParameterizedType;
+import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.text.edits.TextEdit;
@@ -92,6 +98,70 @@ public class JavaTranslationEnhancer {
     }
 
     private void addGetParametersMethod(TypeDeclaration type) {
+        AST ast = type.getAST();
+        MethodDeclaration getter = ast.newMethodDeclaration();
+        getter.modifiers().addAll(ast.newModifiers(Modifier.PUBLIC));
+        getter.setName(ast.newSimpleName("getParameters"));
+
+        // 1 - return type
+        getter.setReturnType2(createMapType(ast));
+
+        // 2 - body
+        Block body = ast.newBlock();
+        getter.setBody(body);
+        type.bodyDeclarations().add(getter);
+
+        // 3 - body statements
+
+        // 3.1 - result declaration and initialisation
+        VariableDeclarationFragment paramsFragment = ast.newVariableDeclarationFragment();
+        paramsFragment.setName(ast.newSimpleName("parameters"));
+        ClassInstanceCreation initializer = ast.newClassInstanceCreation();
+        initializer.setType(createHashMapType(ast));
+
+        paramsFragment.setInitializer(initializer);
+        VariableDeclarationStatement params = ast.newVariableDeclarationStatement(paramsFragment);
+        params.setType(createMapType(ast));
+        body.statements().add(params);
+
+        // 3.2 - statement for parameter addition in the map
+        for (PatternParameter parameter : pattern.getAllParameters()) {
+            String name = parameter.getName();
+            MethodInvocation invocation = ast.newMethodInvocation();
+            invocation.setName(ast.newSimpleName("put"));
+            StringLiteral literal = ast.newStringLiteral();
+            literal.setLiteralValue(name);
+            invocation.arguments().add(literal);
+            FieldAccess fieldAccess = ast.newFieldAccess();
+            fieldAccess.setName(ast.newSimpleName(name));
+            fieldAccess.setExpression(ast.newThisExpression());
+            invocation.arguments().add(fieldAccess);
+            invocation.setExpression(ast.newSimpleName("parameters"));
+            body.statements().add(ast.newExpressionStatement(invocation));
+
+        }
+
+        // 3.3 - return statement
+        ReturnStatement returnStatement = ast.newReturnStatement();
+        returnStatement.setExpression(ast.newSimpleName("parameters"));
+        body.statements().add(returnStatement);
+
+    }
+
+    private ParameterizedType createMapType(AST ast) {
+        return createType(ast, "Map");
+    }
+
+    private ParameterizedType createHashMapType(AST ast) {
+        return createType(ast, "HashMap");
+    }
+
+    private ParameterizedType createType(AST ast, String type) {
+        SimpleType mapType = ast.newSimpleType(ast.newName(type));
+        ParameterizedType parameterizedType = ast.newParameterizedType(mapType);
+        parameterizedType.typeArguments().add(ast.newSimpleType(ast.newName("String")));
+        parameterizedType.typeArguments().add(ast.newSimpleType(ast.newName("Object")));
+        return parameterizedType;
     }
 
     protected void addVariableFieldsAndSetter(TypeDeclaration type) {
