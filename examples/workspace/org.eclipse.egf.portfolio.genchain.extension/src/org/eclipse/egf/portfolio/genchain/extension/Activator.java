9d11820a605de34908d7ec37b439e91cbfdb4fc2
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-08-23 12:24:38 +0000

    add copyright information

--- examples/workspace/org.eclipse.egf.portfolio.genchain.extension/src/org/eclipse/egf/portfolio/genchain/extension/Activator.java
+++ examples/workspace/org.eclipse.egf.portfolio.genchain.extension/src/org/eclipse/egf/portfolio/genchain/extension/Activator.java
@@ -1,30 +1,41 @@
+/**
+ * <copyright>
+ *
+ *  Copyright (c) 2010 Thales Corporate Services S.A.S.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *      Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ * </copyright>
+ */
+
 package org.eclipse.egf.portfolio.genchain.extension;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+/**
+ * @author Thomas Guiu
+ * 
+ */
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+    private static BundleContext context;
 
-	static BundleContext getContext() {
-		return context;
-	}
+    static BundleContext getContext() {
+        return context;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
 
 }
