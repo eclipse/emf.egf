c77e379e189d70820f21d1daeb6ac7ffa1a5e276
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-08 12:24:46 +0000

    remove useless non-nls

--- plugins/org.eclipse.egf.pattern.jet/src/org/eclipse/egf/pattern/jet/engine/JetTagsCompiler.java
+++ plugins/org.eclipse.egf.pattern.jet/src/org/eclipse/egf/pattern/jet/engine/JetTagsCompiler.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
@@ -133,17 +133,17 @@ public class JetTagsCompiler extends JETCompiler {
             String line = ""; //$NON-NLS-1$
             while ((line = bufferedReader.readLine()) != null) {
                 buffer.append("//"); //$NON-NLS-1$
-                buffer.append(line); //$NON-NLS-1$
+                buffer.append(line);
                 buffer.append("\n"); //$NON-NLS-1$
             }
         } catch (IOException e) {
-            //ignore me
+            // ignore me
         } finally {
             if (bufferedReader != null)
                 try {
                     bufferedReader.close();
                 } catch (IOException e) {
-                    //ignore me
+                    // ignore me
                 }
         }
     }
