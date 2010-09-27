5ae68144af8e432e58e1ebc731ca8ebd82ab277d
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-27 14:46:37 +0000

    1 - implement IPlatformFcoreProvider to expose its IPlatformFcore IPlatformFcore is reset to null when the resource is unloaded 2 - call fcoreProcessor extension-point when the resource is unloaded and saved

--- plugins/org.eclipse.egf.model/generated/org/eclipse/egf/model/fcore/util/FcoreResourceImpl.java
+++ plugins/org.eclipse.egf.model/generated/org/eclipse/egf/model/fcore/util/FcoreResourceImpl.java
@@ -1,45 +1,87 @@
 /**
- * 
  * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
  * Contributors:
  * Thales Corporate Services S.A.S - initial API and implementation
- * 
  */
 package org.eclipse.egf.model.fcore.util;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.egf.common.helper.EMFHelper;
+import org.eclipse.egf.core.EGFCorePlugin;
+import org.eclipse.egf.core.fcore.IPlatformFcore;
+import org.eclipse.egf.core.fcore.IPlatformFcoreProvider;
 import org.eclipse.egf.core.platform.uri.PlatformXMLURIHandler;
+import org.eclipse.egf.core.processor.IFcoreProcessor;
+import org.eclipse.egf.model.EGFModelPlugin;
+import org.eclipse.egf.model.l10n.EGFModelMessages;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.change.ChangeDescription;
+import org.eclipse.emf.ecore.change.util.ChangeRecorder;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.XMLHelper;
 import org.eclipse.emf.ecore.xmi.XMLParserPool;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.XMLSave.XMLTypeInfo;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLParserPoolImpl;
 import org.eclipse.emf.ecore.xml.type.XMLTypePackage;
+import org.eclipse.osgi.util.NLS;
 
 /**
  * <!-- begin-user-doc -->
  * The <b>Resource </b> associated with the package.
+ * @implements IPlatformFcoreProvider
  * <!-- end-user-doc -->
  * @see org.eclipse.egf.model.fcore.util.FcoreResourceFactoryImpl
  * @generated
  */
-public class FcoreResourceImpl extends XMIResourceImpl {
+public class FcoreResourceImpl extends XMIResourceImpl implements IPlatformFcoreProvider {
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    private ChangeRecorder _recorder;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    private ChangeDescription _changeDescription;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    private IPlatformFcore _fcore;
 
     /**
      * <!-- begin-user-doc -->
@@ -69,11 +111,14 @@ public class FcoreResourceImpl extends XMIResourceImpl {
      * Creates an instance of the resource.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @param uri the URI of the new resource.
+     * 
+     * @param uri
+     *        the URI of the new resource.
      * @generated
      */
     public FcoreResourceImpl(URI uri) {
         super(uri);
+        _recorder = new ChangeRecorder(this);
     }
 
     /**
@@ -99,16 +144,39 @@ public class FcoreResourceImpl extends XMIResourceImpl {
     }
 
     /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    public IPlatformFcore getIPlatformFcore() {
+        if (_fcore == null) {
+            _fcore = EGFCorePlugin.getPlatformFcore(this);
+        }
+        return _fcore;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    @Override
+    protected XMLHelper createXMLHelper() {
+        return new FcoreXMIHelperImpl(this);
+    }
+
+    /**
      * Called when the object is unloaded.
-     * This implementation
-     * {@link InternalEObject#eSetProxyURI sets} the object to be a proxy
+     * This implementation {@link InternalEObject#eSetProxyURI sets} the object to be a proxy
      * and clears the {@link #eAdapters adapters}.
      * https://bugs.eclipse.org/bugs/show_bug.cgi?id=292866
      * Fix available in HEAD: 2.6.0.I200911020144
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
      * 
-     * @generated	 
+     * @generated
      */
     @Override
     protected void unloaded(InternalEObject internalEObject) {
@@ -132,12 +200,13 @@ public class FcoreResourceImpl extends XMIResourceImpl {
         setTrackingModification(true);
         setEncoding("UTF-8"); //$NON-NLS-1$
         setXMIVersion("2.1"); //$NON-NLS-1$
-        // Save Options		
+        // Save Options
         getDefaultSaveOptions().put(XMLResource.OPTION_CONFIGURATION_CACHE, Boolean.TRUE);
         getDefaultSaveOptions().put(XMLResource.OPTION_USE_CACHED_LOOKUP_TABLE, lookupTable);
         getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
         getDefaultSaveOptions().put(Resource.OPTION_SAVE_ONLY_IF_CHANGED, Resource.OPTION_SAVE_ONLY_IF_CHANGED_MEMORY_BUFFER);
         getDefaultSaveOptions().put(XMLResource.OPTION_SAVE_TYPE_INFORMATION, new XMLTypeInfo() {
+
             public boolean shouldSaveType(EClass objectType, EClassifier featureType, EStructuralFeature feature) {
                 return objectType != featureType && objectType != XMLTypePackage.Literals.ANY_TYPE;
             }
@@ -150,7 +219,7 @@ public class FcoreResourceImpl extends XMIResourceImpl {
         getDefaultSaveOptions().put(XMLResource.OPTION_URI_HANDLER, new PlatformXMLURIHandler());
         getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
         getDefaultSaveOptions().put(XMIResource.OPTION_USE_XMI_TYPE, Boolean.TRUE);
-        // Load Options		
+        // Load Options
         getDefaultLoadOptions().put(XMLResource.OPTION_URI_HANDLER, new PlatformXMLURIHandler());
         getDefaultLoadOptions().put(XMLResource.OPTION_DEFER_ATTACHMENT, Boolean.FALSE);
         getDefaultLoadOptions().put(XMLResource.OPTION_DEFER_IDREF_RESOLUTION, Boolean.FALSE);
@@ -160,6 +229,126 @@ public class FcoreResourceImpl extends XMIResourceImpl {
         getDefaultLoadOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
         getDefaultLoadOptions().put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
         getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    @Override
+    protected void doUnload() {
+        try {
+            // ChangeDescription snapshot
+            _changeDescription = _recorder.endRecording();
+            // Post-Processing
+            if (_changeDescription != null && (_changeDescription.getResourceChanges().isEmpty() == false || _changeDescription.getObjectsToDetach().isEmpty() == false || _changeDescription.getObjectChanges().isEmpty() == false)) {
+                callPreUnloadProcessors(this, getIPlatformFcore(), _changeDescription);
+            }
+            // Unload
+            super.doUnload();
+        } finally {
+            // reset
+            _fcore = null;
+            _recorder.dispose();
+            _recorder.beginRecording(Collections.singleton(this));
+        }
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @generated NOT
+     */
+    @Override
+    public void save(Map<?, ?> options) throws IOException {
+        try {
+            // ChangeDescription snapshot
+            _changeDescription = _recorder.endRecording();
+            // Save
+            try {
+                super.save(options);
+            } catch (IOException ioe) {
+                throw ioe;
+            }
+            // Post-Processing
+            if (_changeDescription != null && (_changeDescription.getResourceChanges().isEmpty() == false || _changeDescription.getObjectsToAttach().isEmpty() == false || _changeDescription.getObjectChanges().isEmpty() == false)) {
+                callPostSaveProcessors(this, getIPlatformFcore(), _changeDescription);
+            }
+        } finally {
+            _recorder.dispose();
+            _recorder.beginRecording(Collections.singleton(this));
+        }
+    }
+
+    protected static void callPreUnloadProcessors(final Resource resource, final IPlatformFcore fcore, final ChangeDescription changeDescription) {
+        final List<IFcoreProcessor> processors = EGFCorePlugin.getIFcoreProcessors();
+        if (processors.isEmpty() == false) {
+            WorkspaceJob job = new WorkspaceJob(EGFModelMessages.FcoreResource_processor) {
+
+                @Override
+                public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+                    SubMonitor subMonitor = SubMonitor.convert(monitor, 100 * processors.size());
+                    MultiStatus statii = new MultiStatus(EGFModelPlugin.getPlugin().getSymbolicName(), IStatus.ERROR, EGFModelMessages.FcoreResource_processors_execute_exception, null);
+                    try {
+                        for (IFcoreProcessor processor : processors) {
+                            try {
+                                processor.processPreUnload(subMonitor.newChild(100, SubMonitor.SUPPRESS_NONE), fcore, changeDescription);
+                            } catch (Throwable t) {
+                                if (t instanceof CoreException) {
+                                    statii.add(((CoreException) t).getStatus());
+                                } else {
+                                    statii.add(EGFModelPlugin.getPlugin().newStatus(Status.ERROR, NLS.bind(EGFModelMessages.FcoreResource_processor_execute_exception, processor.getClass().getName()), t));
+                                }
+                            }
+                        }
+                    } finally {
+                        monitor.done();
+                    }
+                    return statii.getChildren().length != 0 ? statii : Status.OK_STATUS;
+                }
+
+            };
+            job.setRule(EMFHelper.getProject(resource));
+            job.schedule();
+        }
+
+    }
+
+    protected static void callPostSaveProcessors(final Resource resource, final IPlatformFcore fcore, final ChangeDescription changeDescription) {
+        final List<IFcoreProcessor> processors = EGFCorePlugin.getIFcoreProcessors();
+        if (processors.isEmpty() == false) {
+            WorkspaceJob job = new WorkspaceJob(EGFModelMessages.FcoreResource_processor) {
+
+                @Override
+                public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+                    SubMonitor subMonitor = SubMonitor.convert(monitor, 100 * processors.size());
+                    MultiStatus statii = new MultiStatus(EGFModelPlugin.getPlugin().getSymbolicName(), IStatus.ERROR, EGFModelMessages.FcoreResource_processors_execute_exception, null);
+                    try {
+                        for (IFcoreProcessor processor : processors) {
+                            try {
+                                processor.processPostSave(subMonitor.newChild(100, SubMonitor.SUPPRESS_NONE), fcore, changeDescription);
+                            } catch (Throwable t) {
+                                if (t instanceof CoreException) {
+                                    statii.add(((CoreException) t).getStatus());
+                                } else {
+                                    statii.add(EGFModelPlugin.getPlugin().newStatus(Status.ERROR, NLS.bind(EGFModelMessages.FcoreResource_processor_execute_exception, processor.getClass().getName()), t));
+                                }
+                            }
+                        }
+                    } finally {
+                        monitor.done();
+                    }
+                    return statii.getChildren().length != 0 ? statii : Status.OK_STATUS;
+                }
+
+            };
+            job.setRule(EMFHelper.getProject(resource));
+            job.schedule();
+        }
+
     }
 
 } // FcoreResourceImpl
