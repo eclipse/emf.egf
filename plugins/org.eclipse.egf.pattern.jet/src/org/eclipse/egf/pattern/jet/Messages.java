be3b788c05c7a94f08fe225397f83fa63b79e67c
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-28 15:25:16 +0000

    1 - add missing copyright 2 - remove useless messages

--- plugins/org.eclipse.egf.pattern.jet/src/org/eclipse/egf/pattern/jet/Messages.java
+++ plugins/org.eclipse.egf.pattern.jet/src/org/eclipse/egf/pattern/jet/Messages.java
@@ -1,3 +1,15 @@
+/**
+ * 
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ */
 package org.eclipse.egf.pattern.jet;
 
 import org.eclipse.osgi.util.NLS;
@@ -5,14 +17,9 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 
     // assembly
-    public static String assembly_error1;
-    public static String assembly_error2;
-    public static String assembly_error3;
-    public static String assembly_error4;
-    public static String assembly_error5;
     public static String assembly_error6;
+
     public static String assembly_error8;
-    public static String assembly_error9;
 
     private static final String BUNDLE_NAME = "org.eclipse.egf.pattern.jet.JetPatternMessages";//$NON-NLS-1$
     static {
