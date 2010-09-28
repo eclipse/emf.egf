f00ade33c13f1611248a395526d3a8fc3d9c075e
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-28 13:20:28 +0000

    1 - Messages and Activator refactored in EGFPatternMessages and EGFPatternPlugin 2 - NLS.bind use 3 - format update 4 - inner filename moved to TemplateFileHelper and/or TemplateModelFileHelper 5 - getFullLibraryName has been simplified as a library for now on only own Pattern 6 - library has an eopposite to retrieve its PatternViewpoint, so retrieving a FactoryComponent from a library is facilitated

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/engine/PatternHelper.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/engine/PatternHelper.java
@@ -1,15 +1,12 @@
 /**
  * <copyright>
- * 
  * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
  * Contributors:
  * Thales Corporate Services S.A.S - initial API and implementation
- * 
  * </copyright>
  */
 
@@ -23,84 +20,44 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.egf.common.constant.EGFCommonConstants;
 import org.eclipse.egf.core.EGFCorePlugin;
 import org.eclipse.egf.core.domain.EGFResourceSet;
 import org.eclipse.egf.core.fcore.IPlatformFcore;
 import org.eclipse.egf.model.fcore.NamedModelElement;
-import org.eclipse.egf.model.fcore.Viewpoint;
-import org.eclipse.egf.model.fcore.ViewpointContainer;
 import org.eclipse.egf.model.pattern.Pattern;
 import org.eclipse.egf.model.pattern.PatternElement;
 import org.eclipse.egf.model.pattern.PatternLibrary;
 import org.eclipse.egf.model.pattern.PatternMethod;
-import org.eclipse.egf.pattern.Messages;
-import org.eclipse.egf.pattern.PatternConstants;
-import org.eclipse.egf.pattern.PatternPreferences;
+import org.eclipse.egf.pattern.EGFPatternPlugin;
 import org.eclipse.egf.pattern.collector.PatternCollector;
 import org.eclipse.egf.pattern.collector.PatternElementCollector;
 import org.eclipse.egf.pattern.collector.PatternLibraryCollector;
-import org.eclipse.egf.pattern.extension.PatternFactory;
+import org.eclipse.egf.pattern.extension.PatternExtensionFactory;
+import org.eclipse.egf.pattern.l10n.EGFPatternMessages;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.osgi.util.NLS;
 
 /**
  * @author Thomas Guiu
- * 
  */
 public class PatternHelper {
 
     private final ResourceSet resourceSet;
 
-    /**
-     * This method returns a string from the libraries who contain the pattern.
-     * For example: myLib.mySubLib
-     */
     public static String getFullLibraryName(Pattern pattern) {
-        List<PatternLibrary> libs = new ArrayList<PatternLibrary>();
-        PatternLibrary lib = pattern.getContainer();
-        while (lib != null) {
-            libs.add(lib);
-            lib = lib.getContainer();
-        }
-        if (libs.isEmpty())
-            return EGFCommonConstants.EMPTY_STRING;
-        if (libs.size() == 1)
-            return libs.get(0).getName();
-        StringBuffer buf = new StringBuffer();
-        for (PatternLibrary mylib : libs) {
-            buf.append(mylib.getName()).append('.');
-        }
-        return buf.deleteCharAt(buf.length() - 1).toString();
+        return pattern.getContainer().getName();
     }
 
-    public static IPlatformFcore getPlatformFcore(PatternElement pattern) {
-        return EGFCorePlugin.getPlatformFcore(pattern.eResource());
+    public static String getFactoryComponentName(PatternLibrary library) {
+        return library.getPatternViewpoint().getViewpointContainer().getFactoryComponent().getName();
     }
 
-    public static String getFactoryConponentName(PatternElement element) {
-        PatternElement container = element.getContainer() == null ? element : element.getContainer();
-        while (container.getContainer() != null)
-            container = container.getContainer();
-        // TODO: update the model so that the library knows about its viewpoint
-        Viewpoint vp = (Viewpoint) container.eContainer();
-        ViewpointContainer vpc = vp.getViewpointContainer();
-
-        return vpc.getFactoryComponent().getName();
-    }
-
-    /**
-     * Return the IProject object who contains the pattern.
-     * 
-     * @return null if the FC is deployed in the runtime environment.
-     */
-    public static IProject getProject(Pattern pattern) {
-        IPlatformFcore fcore = getPlatformFcore(pattern);
-        return fcore.getPlatformBundle().getProject();
+    public static String getFactoryComponentName(Pattern pattern) {
+        return getFactoryComponentName(pattern.getContainer());
     }
 
     // TODO quick implementation ...
@@ -126,7 +83,7 @@ public class PatternHelper {
                 Resource res = resourceSet.getResource(uri, true);
                 PatternElementCollector.INSTANCE.collect(res.getContents(), result, ids);
             } catch (Exception e) {
-                org.eclipse.egf.pattern.Activator.getDefault().logError(Messages.bind(Messages.collect_error2, uri.toString()), e);
+                EGFPatternPlugin.getDefault().logError(NLS.bind(EGFPatternMessages.collect_error2, uri.toString()), e);
             }
         }
         Map<String, PatternElement> map = new HashMap<String, PatternElement>(200);
@@ -141,8 +98,9 @@ public class PatternHelper {
      * given ids if any. If the ids set is null all patterns are returned.
      */
     public List<Pattern> getPatterns(IProject project, Set<String> ids) {
-        if (ids == null)
+        if (ids == null) {
             ids = new HashSet<String>();
+        }
         List<Pattern> result = new ArrayList<Pattern>();
         IPlatformFcore[] platformFcores = EGFCorePlugin.getPlatformFcores(project);
         for (IPlatformFcore platformFcore : platformFcores) {
@@ -162,7 +120,7 @@ public class PatternHelper {
             try {
                 collectPatterns(uri, PatternCollector.EMPTY_ID_SET, result);
             } catch (Exception e) {
-                org.eclipse.egf.pattern.Activator.getDefault().logError(Messages.bind(Messages.collect_error1, uri.toString()), e);
+                EGFPatternPlugin.getDefault().logError(NLS.bind(EGFPatternMessages.collect_error1, uri.toString()), e);
             }
         }
         return result;
@@ -180,7 +138,7 @@ public class PatternHelper {
                 Resource res = resourceSet.getResource(uri, true);
                 PatternLibraryCollector.INSTANCE.collect(res.getContents(), result, PatternCollector.EMPTY_ID_SET);
             } catch (Exception e) {
-                org.eclipse.egf.pattern.Activator.getDefault().logError(Messages.bind(Messages.collect_error2, uri.toString()), e);
+                EGFPatternPlugin.getDefault().logError(NLS.bind(EGFPatternMessages.collect_error2, uri.toString()), e);
             }
         }
         return result;
@@ -195,10 +153,6 @@ public class PatternHelper {
         return EcoreUtil.generateUUID().replaceAll("\\W", ""); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
-    public static String dropNonWordCharacter(String value) {
-        return value.replaceAll("\\W", ""); //$NON-NLS-1$ //$NON-NLS-2$
-    }
-
     public static String uniqueName(NamedModelElement parameter) {
         return parameter.getName().replaceAll("\\W", "") + "_" + parameter.getID().replaceAll("\\W", ""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
     }
@@ -211,7 +165,7 @@ public class PatternHelper {
         ArrayList<PatternMethod> result = new ArrayList<PatternMethod>();
         for (PatternMethod m : pattern.getMethods()) {
             String name = m.getName();
-            if (PatternFactory.isSpecialMethod(name))
+            if (PatternExtensionFactory.isSpecialMethod(name))
                 continue;
             result.add(m);
 
@@ -237,54 +191,6 @@ public class PatternHelper {
 
     public static final PatternHelper createCollector() {
         return new PatternHelper(new EGFResourceSet());
-    }
-
-    public static class FilenameFormatException extends Exception {
-
-        private static final long serialVersionUID = 1L;
-
-        private FilenameFormatException(String message) {
-            super(message);
-
-        }
-
-    }
-
-    /**
-     * This class manage the relationship between pt file paths and patterns.
-     * 
-     * @author Thomas Guiu
-     * 
-     */
-
-    public static class Filename {
-
-        private static final String PATTERN_TOKEN = "pattern."; //$NON-NLS-1$
-
-        private static final String METHOD_TOKEN = "method."; //$NON-NLS-1$
-
-        public static URI computeFileURI(PatternMethod method) {
-            return URI.createFileURI(PatternPreferences.getTemplatesFolderName() + EGFCommonConstants.SLASH_CHARACTER + PATTERN_TOKEN + method.getPattern().getID() + EGFCommonConstants.SLASH_CHARACTER + METHOD_TOKEN + method.getID() + EGFCommonConstants.DOT_CHARACTER
-                    + PatternConstants.PATTERN_UNIT_FILE_EXTENSION);
-        }
-
-        public static String extractPatternId(IPath patternMethodPath) throws FilenameFormatException {
-            if (patternMethodPath == null)
-                throw new FilenameFormatException(Messages.PatternFilename_error1);
-
-            int segmentCount = patternMethodPath.segmentCount();
-            if (segmentCount < 3)
-                throw new FilenameFormatException(Messages.bind(Messages.PatternFilename_error2, patternMethodPath));
-            String segment = patternMethodPath.segment(segmentCount - 2);
-            if (segment == null || "".equals(segment) || !segment.startsWith(PATTERN_TOKEN) || PATTERN_TOKEN.length() == segment.length()) //$NON-NLS-1$
-                throw new FilenameFormatException(Messages.bind(Messages.PatternFilename_error3, segment, PATTERN_TOKEN));
-            return segment.substring(PATTERN_TOKEN.length());
-        }
-
-        private Filename() {
-            // Nothing to do
-        }
-
     }
 
 }
