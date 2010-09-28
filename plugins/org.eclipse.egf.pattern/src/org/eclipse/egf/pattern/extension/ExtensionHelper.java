1eabcfb338593020ae349ad4251fad50c34af36c
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-28 11:35:38 +0000

    updated to use the new PatternExtension registry

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/extension/ExtensionHelper.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/extension/ExtensionHelper.java
@@ -1,30 +1,26 @@
 /**
  * <copyright>
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
+ * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Thales Corporate Services S.A.S - initial API and implementation
  * 
  * </copyright>
  */
 
 package org.eclipse.egf.pattern.extension;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.egf.model.pattern.PatternNature;
-import org.eclipse.egf.pattern.Activator;
-import org.eclipse.egf.pattern.Messages;
+import org.eclipse.egf.pattern.EGFPatternPlugin;
+import org.eclipse.egf.pattern.l10n.EGFPatternMessages;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.osgi.util.NLS;
 
 /**
  * TODO for each call the extension point is read ...
@@ -34,33 +30,30 @@ import org.eclipse.emf.ecore.EClass;
  */
 public class ExtensionHelper {
 
-    public static Map<String, PatternExtension> getExtensions() {
-
-        Map<String, PatternExtension> result = new HashMap<String, PatternExtension>();
-        for (IConfigurationElement element : Platform.getExtensionRegistry().getConfigurationElementsFor(PatternExtension.EXTENSION_ID)) {
-            try {
-                PatternExtension pe = (PatternExtension) element.createExecutableExtension("class");
-                result.put(getName(pe.getNature()), pe);
-            } catch (CoreException e) {
-                Activator.getDefault().logError(e);
-            }
-        }
-        return result;
-    }
-
     public static PatternNature createNature(String name) {
-        PatternExtension patternExtension = getExtensions().get(name);
-        if (patternExtension == null)
-            throw new IllegalStateException();
+        PatternExtension patternExtension = null;
+        try {
+            patternExtension = EGFPatternPlugin.getPatternExtension(name);
+            if (patternExtension == null) {
+                throw new IllegalStateException(NLS.bind(EGFPatternMessages.extension_error1, name));
+            }
+        } catch (CoreException ce) {
+            throw new IllegalStateException(NLS.bind(EGFPatternMessages.extension_error1, name), ce);
+        }
         EClass eClass = patternExtension.getNature().eClass();
         return (PatternNature) eClass.getEPackage().getEFactoryInstance().create(eClass);
     }
 
     public static PatternExtension getExtension(PatternNature nature) throws MissingExtensionException {
-        Map<String, PatternExtension> extensions = getExtensions();
-        PatternExtension patternExtension = extensions.get(getName(nature));
-        if (patternExtension == null)
-            throw new MissingExtensionException(Messages.bind(Messages.extension_error1, getName(nature)));
+        PatternExtension patternExtension = null;
+        try {
+            patternExtension = EGFPatternPlugin.getPatternExtension(nature);
+            if (patternExtension == null) {
+                throw new MissingExtensionException(NLS.bind(EGFPatternMessages.extension_error1, getName(nature)));
+            }
+        } catch (CoreException ce) {
+            throw new MissingExtensionException(NLS.bind(EGFPatternMessages.extension_error1, getName(nature)), ce);
+        }
         return patternExtension;
 
     }
@@ -75,8 +68,12 @@ public class ExtensionHelper {
 
         private MissingExtensionException(String message) {
             super(message);
+        }
 
+        private MissingExtensionException(String message, Throwable t) {
+            super(message, t);
         }
 
     }
+
 }
