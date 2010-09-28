bcc10fe7b1839438d30ef8d090befa9e49e2870a
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-28 14:18:49 +0000

    1 - refactored PatternFactory to PatternExtensionFactory

--- plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/editor/MethodsComboBoxViewerCellEditor.java
+++ plugins/org.eclipse.egf.pattern.ui/src/org/eclipse/egf/pattern/ui/editors/editor/MethodsComboBoxViewerCellEditor.java
@@ -20,7 +20,7 @@ import java.text.MessageFormat;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.egf.model.pattern.PatternMethod;
-import org.eclipse.egf.pattern.extension.PatternFactory;
+import org.eclipse.egf.pattern.extension.PatternExtensionFactory;
 import org.eclipse.egf.pattern.ui.editors.pages.ImplementationPage;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
@@ -139,7 +139,7 @@ public class MethodsComboBoxViewerCellEditor extends ComboBoxViewerCellEditor {
                 isModify = true;
                 Button editButton = implementationPage.getEditButton();
                 if (editButton != null && !editButton.isDisposed()) {
-                    editButton.setEnabled(!PatternFactory.isSpecialMethod(text) && !("").equals(text));
+                    editButton.setEnabled(!PatternExtensionFactory.isSpecialMethod(text) && !("").equals(text));
                 }
             }
         });
