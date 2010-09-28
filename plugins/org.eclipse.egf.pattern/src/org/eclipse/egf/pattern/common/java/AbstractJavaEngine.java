61c4bbb9271683bb2e615f3162564eb3a535bd7c
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-28 13:35:12 +0000

    1 - Messages, Activator and PatternFactory refactored in EGFPatternMessages, EGFPatternPlugin and PatternExtensionFactory 2 - NLS.bind use 3 - Use IPlatformFcoreProvider to retrieve a pattern resource IPlatformFcore to get the current IProject

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/common/java/AbstractJavaEngine.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/common/java/AbstractJavaEngine.java
@@ -1,15 +1,12 @@
 /**
  * <copyright>
- * 
  * Copyright (c) 2009 Thales Corporate Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
  * Contributors:
  * Thales Corporate Services S.A.S - initial API and implementation
- * 
  * </copyright>
  */
 
@@ -22,34 +19,35 @@ import java.util.Map;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.egf.core.fcore.IPlatformFcore;
+import org.eclipse.egf.common.helper.EMFHelper;
+import org.eclipse.egf.common.l10n.EGFCommonMessages;
+import org.eclipse.egf.core.pattern.PatternPreferences;
 import org.eclipse.egf.model.pattern.Pattern;
 import org.eclipse.egf.model.pattern.PatternContext;
 import org.eclipse.egf.model.pattern.PatternException;
 import org.eclipse.egf.model.pattern.PatternExecutionReporter;
 import org.eclipse.egf.model.pattern.PatternParameter;
-import org.eclipse.egf.pattern.Messages;
-import org.eclipse.egf.pattern.PatternPreferences;
 import org.eclipse.egf.pattern.engine.AssemblyHelper;
 import org.eclipse.egf.pattern.engine.PatternEngine;
-import org.eclipse.egf.pattern.engine.PatternHelper;
 import org.eclipse.egf.pattern.execution.ConsoleReporter;
 import org.eclipse.egf.pattern.execution.InternalPatternContext;
-import org.eclipse.egf.pattern.extension.PatternFactory;
-import org.eclipse.egf.pattern.utils.FileHelper;
+import org.eclipse.egf.pattern.extension.PatternExtensionFactory;
+import org.eclipse.egf.pattern.l10n.EGFPatternMessages;
 import org.eclipse.egf.pattern.utils.JavaMethodGenerationHelper;
 import org.eclipse.egf.pattern.utils.ParameterTypeHelper;
+import org.eclipse.egf.pattern.utils.TemplateFileHelper;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.ToolFactory;
 import org.eclipse.jdt.core.formatter.CodeFormatter;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.text.edits.TextEdit;
 
 /**
  * @author Thomas Guiu
- * 
  */
 public abstract class AbstractJavaEngine extends PatternEngine {
 
@@ -65,13 +63,12 @@ public abstract class AbstractJavaEngine extends PatternEngine {
 
     @Override
     public boolean checkCondition(PatternContext context, Map<PatternParameter, Object> parameters) throws PatternException {
-        Object result = doExecute((InternalPatternContext) context, PatternFactory.PRECONDITION_METHOD_NAME, parameters);
+        Object result = doExecute((InternalPatternContext) context, PatternExtensionFactory.PRECONDITION_METHOD_NAME, parameters);
         return Boolean.TRUE.equals(result);
     }
 
     @Override
     public final void execute(PatternContext context) throws PatternException {
-
         setupExecutionReporter((InternalPatternContext) context);
         doExecute((InternalPatternContext) context, AssemblyHelper.GENERATE_METHOD, null);
     }
@@ -81,19 +78,21 @@ public abstract class AbstractJavaEngine extends PatternEngine {
             Class<?> templateClass = loadTemplateClass(context, getPatternClassname());
             Object template = templateClass.newInstance();
             Class<?>[] parameterClasses = new Class<?>[1];
-            Object[] parameterValues = new Object[] { context };
+            Object[] parameterValues = new Object[] {
+                context
+            };
 
             if (AssemblyHelper.GENERATE_METHOD.equals(methodName)) {
                 parameterClasses[0] = Object.class;
             } else if (AssemblyHelper.ORCHESTRATION_METHOD.equals(methodName)) {
                 parameterClasses[0] = PatternContext.class;
-            } else if (PatternFactory.PRECONDITION_METHOD_NAME.equals(methodName)) {
+            } else if (PatternExtensionFactory.PRECONDITION_METHOD_NAME.equals(methodName)) {
                 parameterClasses = new Class<?>[0];
                 parameterValues = new Object[0];
             } else
                 throw new IllegalStateException();
 
-            if (AssemblyHelper.ORCHESTRATION_METHOD.equals(methodName) || PatternFactory.PRECONDITION_METHOD_NAME.equals(methodName)) {
+            if (AssemblyHelper.ORCHESTRATION_METHOD.equals(methodName) || PatternExtensionFactory.PRECONDITION_METHOD_NAME.equals(methodName)) {
                 // setting up the state of the pattern
                 Class<?>[] setterClasses = new Class<?>[1];
                 Object[] setterValues = new Object[1];
@@ -112,7 +111,7 @@ public abstract class AbstractJavaEngine extends PatternEngine {
             try {
                 method = templateClass.getMethod(methodName, parameterClasses);
             } catch (NoSuchMethodException e) {
-                if (PatternFactory.PRECONDITION_METHOD_NAME.equals(methodName))
+                if (PatternExtensionFactory.PRECONDITION_METHOD_NAME.equals(methodName))
                     return Boolean.TRUE;
                 throw e;
             }
@@ -154,19 +153,16 @@ public abstract class AbstractJavaEngine extends PatternEngine {
 
     protected Class<?> loadTemplateClass(InternalPatternContext context, String type) throws PatternException, ClassNotFoundException {
         if (type == null)
-            throw new PatternException(Messages.assembly_error1);
+            throw new PatternException(EGFPatternMessages.assembly_error1);
         Class<?> templateClass = context.getBundle(getBundleId()).loadClass(type);
         return templateClass;
     }
 
     protected void writeContent(Pattern pattern, IPath outputPath, String content) throws Exception {
-        IPlatformFcore platformFcore = PatternHelper.getPlatformFcore(pattern);
-        if (platformFcore == null)
-            throw new PatternException(Messages.bind(Messages.assembly_error4, pattern.getName(), pattern.getID()));
-        IProject project = platformFcore.getPlatformBundle().getProject();
-        if (project == null)
-            throw new PatternException(Messages.bind(Messages.assembly_error5, pattern.getName(), pattern.getID()));
-
+        IProject project = EMFHelper.getProject(pattern.eResource());
+        if (project == null) {
+            throw new PatternException(NLS.bind(EGFCommonMessages.No_associated_project, EcoreUtil.getURI(pattern)));
+        }
         // format code
         IJavaProject javaProject = JavaCore.create(project);
         if (javaProject != null) {
@@ -180,6 +176,6 @@ public abstract class AbstractJavaEngine extends PatternEngine {
             }
         }
 
-        FileHelper.setContent(project.getFile(outputPath), content, false);
+        TemplateFileHelper.setContent(project.getFile(outputPath), content, false);
     }
 }
