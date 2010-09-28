461c1cfa98879e23d260c1423015f7e9f95ca8af
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-28 11:39:06 +0000

    1 - Messages and PatternFactory refactored in EGFPatternMessages and PatternExtensionFactory 2 - NLS.bind use 3 - deleteJavaFile moved in common JavaHelper

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/extension/PatternExtension.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/extension/PatternExtension.java
@@ -1,15 +1,12 @@
 /**
  * <copyright>
- * 
  * Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
  * Contributors:
  * Thales Corporate Services S.A.S - initial API and implementation
- * 
  * </copyright>
  */
 
@@ -17,25 +14,23 @@ package org.eclipse.egf.pattern.extension;
 
 import java.net.URL;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.egf.common.helper.JavaHelper;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.egf.common.helper.EMFHelper;
+import org.eclipse.egf.common.l10n.EGFCommonMessages;
 import org.eclipse.egf.model.pattern.Pattern;
 import org.eclipse.egf.model.pattern.PatternException;
 import org.eclipse.egf.model.pattern.PatternNature;
-import org.eclipse.egf.pattern.Messages;
 import org.eclipse.egf.pattern.engine.PatternEngine;
-import org.eclipse.egf.pattern.engine.PatternHelper;
+import org.eclipse.egf.pattern.l10n.EGFPatternMessages;
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.osgi.util.NLS;
 
 /**
  * @author Thomas Guiu
- * 
  */
 public abstract class PatternExtension {
 
@@ -43,7 +38,7 @@ public abstract class PatternExtension {
 
     public abstract PatternNature getNature();
 
-    public abstract PatternFactory getFactory();
+    public abstract IPatternExtensionFactory getFactory();
 
     protected abstract AdapterFactory getAdapterFactory();
 
@@ -51,7 +46,7 @@ public abstract class PatternExtension {
 
     public PatternEngine createEngine(Pattern pattern) throws PatternException {
         if (!matchNature(pattern))
-            throw new PatternException(Messages.bind(Messages.extension_error2, getNature().eClass().getName(), pattern.getNature().eClass().getName()));
+            throw new PatternException(NLS.bind(EGFPatternMessages.extension_error2, getNature().eClass().getName(), pattern.getNature().eClass().getName()));
         return doCreateRunner(pattern);
     }
 
@@ -59,7 +54,7 @@ public abstract class PatternExtension {
 
     public PatternInitializer createInitializer(IProject project, Pattern pattern) throws PatternException {
         if (!matchNature(pattern))
-            throw new PatternException(Messages.bind(Messages.extension_error2, getNature().eClass().getName(), pattern.getNature().eClass().getName()));
+            throw new PatternException(NLS.bind(EGFPatternMessages.extension_error2, getNature().eClass().getName(), pattern.getNature().eClass().getName()));
         return doCreateInitializer(project, pattern);
     }
 
@@ -84,12 +79,14 @@ public abstract class PatternExtension {
      *         problem
      */
     public String canTranslate(Pattern pattern) {
-        IProject project = PatternHelper.getProject(pattern);
-        if (project == null)
-            return Messages.assembly_error4;
-        IFile file = project.getFile(pattern.getHeaderMethod().getPatternFilePath().path());
-        if (!file.exists())
-            return Messages.assembly_error5;
+        IProject project = EMFHelper.getProject(pattern.eResource());
+        if (project == null) {
+            return NLS.bind(EGFCommonMessages.No_associated_project, EcoreUtil.getURI(pattern).trimFragment());
+        }
+        IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(pattern.getHeaderMethod().getPatternFilePath().toPlatformString(true));
+        if (resource == null) {
+            return EGFPatternMessages.assembly_error5;
+        }
         return null; // no problem
     }
 
@@ -102,31 +99,6 @@ public abstract class PatternExtension {
     public String canExecute(Pattern pattern) {
         // todo it should be in a validation rule...
         return null; // no problem
-    }
-
-    protected static void deleteJavaFile(IProject project, String packageName, String className) {
-        if (project == null || packageName == null || className == null) {
-            return;
-        }
-        // IJavaProject lookup
-        IJavaProject javaProject = null;
-        try {
-            if (project.isAccessible() && project.hasNature(JavaCore.NATURE_ID)) {
-                javaProject = JavaCore.create(project);
-            }
-            if (javaProject != null) {
-                IPath path = new Path(org.eclipse.egf.common.helper.FileHelper.convertPackageNameToFolderPath(packageName));
-                JavaHelper.deleteJavaFile(javaProject, path.append(PatternHelper.dropNonWordCharacter(className) + ".java"), false); //$NON-NLS-1$
-            }
-        } catch (Throwable t) {
-            // Ignore
-        } finally {
-            try {
-                javaProject.close();
-            } catch (Throwable t) {
-                // Ignore
-            }
-        }
     }
 
 }
