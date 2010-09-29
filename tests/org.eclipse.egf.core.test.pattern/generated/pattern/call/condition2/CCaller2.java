2bd4b8c3f267a578b898d06731a3844d786a4cab
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-09-29 14:36:49 +0000

    1 - fix broken test due to bad patternfilepath format 2 - upgrade header .pt to use CLASS and PACKAGE token

---
+++ tests/org.eclipse.egf.core.test.pattern/generated/pattern/call/condition2/CCaller2.java
@@ -0,0 +1,110 @@
+package pattern.call.condition2;
+
+import org.eclipse.egf.common.helper.*;
+import java.util.*;
+import org.eclipse.emf.ecore.*;
+import org.eclipse.egf.model.pattern.*;
+import org.eclipse.egf.pattern.execution.*;
+import org.eclipse.egf.pattern.query.*;
+
+public class CCaller2 {
+
+    protected static String nl;
+
+    public static synchronized CCaller2 create(String lineSeparator) {
+        nl = lineSeparator;
+        CCaller2 result = new CCaller2();
+        nl = null;
+        return result;
+    }
+
+    public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+
+    protected final String TEXT_1 = "Message from caller" + NL;
+
+    protected final String TEXT_2 = NL;
+
+    protected final String TEXT_3 = NL;
+
+    public CCaller2() {
+        //Here is the constructor
+        StringBuffer stringBuffer = new StringBuffer();
+
+        // add initialisation of the pattern variables (declaration has been already done).
+
+    }
+
+    public String generate(Object argument) throws Exception {
+        final StringBuffer stringBuffer = new StringBuffer();
+
+        InternalPatternContext ctx = (InternalPatternContext) argument;
+        Map<String, String> queryCtx = null;
+        IQuery.ParameterDescription paramDesc = null;
+
+        paramDesc = new IQuery.ParameterDescription("parameter", "http://www.eclipse.org/emf/2002/Ecore#//EPackage");
+        queryCtx = new HashMap<String, String>();
+        List<Object> parameterList = QueryHelper.load(ctx, "org.eclipse.egf.pattern.basic.query").execute(paramDesc, queryCtx, ctx);
+
+        for (Object parameterParameter : parameterList) {
+
+            this.parameter = (org.eclipse.emf.ecore.EPackage) parameterParameter;
+
+            if (preCondition())
+                orchestration(ctx);
+
+        }
+        if (ctx.useReporter()) {
+            ctx.getReporter().executionFinished(ctx.getExecutionBuffer().toString(), ctx);
+            ctx.clearBuffer();
+        }
+
+        stringBuffer.append(TEXT_2);
+        stringBuffer.append(TEXT_3);
+        return stringBuffer.toString();
+    }
+
+    public String orchestration(PatternContext ctx) throws Exception {
+        InternalPatternContext ictx = (InternalPatternContext) ctx;
+        int executionIndex = ictx.getExecutionBuffer().length();
+
+        method_body(ictx.getBuffer(), ictx);
+        {
+            ExecutionContext ctx_local = new ExecutionContext(ictx);
+            ctx_local.setValue(PatternContext.INJECTED_CONTEXT, parameter);
+            CallHelper.executeWithContextInjection("platform:/plugin/org.eclipse.egf.core.test.pattern/fc/patternCall_Condition2.fcore#_r-OdkHeQEd-ent-ImclT8w", ctx_local);
+        }
+
+        String loop = ictx.getBuffer().toString();
+        if (ictx.useReporter()) {
+            ictx.getExecutionBuffer().append(ictx.getBuffer().substring(ictx.getExecutionCurrentIndex()));
+            ictx.setExecutionCurrentIndex(0);
+            Map<String, Object> parameterValues = new HashMap<String, Object>();
+            parameterValues.put("parameter", this.parameter);
+            String outputWithCallBack = ictx.getExecutionBuffer().substring(executionIndex);
+            ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+            ictx.clearBuffer();
+        }
+        return loop;
+    }
+
+    protected org.eclipse.emf.ecore.EPackage parameter = null;
+
+    public void set_parameter(org.eclipse.emf.ecore.EPackage object) {
+        this.parameter = object;
+    }
+
+    public Map<String, Object> getParameters() {
+        final Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("parameter", this.parameter);
+        return parameters;
+    }
+
+    protected void method_body(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
+
+        stringBuffer.append(TEXT_1);
+    }
+
+    public boolean preCondition() throws Exception {
+        return true;
+    }
+}
