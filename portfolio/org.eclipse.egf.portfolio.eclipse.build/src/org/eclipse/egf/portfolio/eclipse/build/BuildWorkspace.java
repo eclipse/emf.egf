3f4b5a71e1e2480a5c4412b2cb41f43bf035a503
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-10-05 08:58:09 +0000

    build portfolio : add task to build workspace with egf (will also regenerate patterns)

---
+++ portfolio/org.eclipse.egf.portfolio.eclipse.build/src/org/eclipse/egf/portfolio/eclipse/build/BuildWorkspace.java
@@ -0,0 +1,57 @@
+/**
+ * <copyright>
+ *
+ *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *      Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ * </copyright>
+ */
+
+package org.eclipse.egf.portfolio.eclipse.build;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.egf.core.producer.InvocationException;
+import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
+import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
+
+/**
+ * @author Matthieu Helleboid
+ * 
+ */
+public class BuildWorkspace implements ITaskProduction {
+
+    public void preExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+    }
+
+    public void doExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        IWorkspace ws = ResourcesPlugin.getWorkspace();
+        IWorkspaceRoot wsRoot = ws.getRoot();
+
+        IProgressMonitor progressMonitor = new NullProgressMonitor();
+
+        try {
+            wsRoot.refreshLocal(IResource.DEPTH_INFINITE, progressMonitor);
+            ws.build(IncrementalProjectBuilder.CLEAN_BUILD, progressMonitor);
+            ws.build(IncrementalProjectBuilder.FULL_BUILD, progressMonitor);
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void postExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+    }
+
+}
