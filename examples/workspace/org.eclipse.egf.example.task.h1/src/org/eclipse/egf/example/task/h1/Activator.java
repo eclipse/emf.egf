451cf03181c29eef524b684ac938c8721c51071a
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-10-11 09:32:02 +0000

    format update

--- examples/workspace/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/Activator.java
+++ examples/workspace/org.eclipse.egf.example.task.h1/src/org/eclipse/egf/example/task/h1/Activator.java
@@ -10,51 +10,51 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends EGFAbstractPlugin {
 
-  // The shared instance
-  private static Activator plugin;
+    // The shared instance
+    private static Activator plugin;
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * 
-   * 
-   * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-   * )
-   */
-  @Override
-  public void start(BundleContext context) throws Exception {
-    super.start(context);
-    plugin = this;
-    logInfo("org.eclipse.egf.example.task.h1 is starting"); //$NON-NLS-1$
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * 
-   * 
-   * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-   * )
-   */
-  @Override
-  public void stop(BundleContext context) throws Exception {
-    Bundle bundle = Platform.getBundle("org.eclipse.swt"); //$NON-NLS-1$
-    if (bundle.getState() == Bundle.ACTIVE) {
-      logInfo("org.eclipse.egf.example.task.h1 is stopping"); //$NON-NLS-1$
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * 
+     * 
+     * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+     * )
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+        logInfo("org.eclipse.egf.example.task.h1 is starting"); //$NON-NLS-1$
     }
-    plugin = null;
-    super.stop(context);
-  }
 
-  /**
-   * Returns the shared instance
-   * 
-   * @return the shared instance
-   */
-  public static Activator getDefault() {
-    return plugin;
-  }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * 
+     * 
+     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+     * )
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        Bundle bundle = Platform.getBundle("org.eclipse.swt"); //$NON-NLS-1$
+        if (bundle.getState() == Bundle.ACTIVE) {
+            logInfo("org.eclipse.egf.example.task.h1 is stopping"); //$NON-NLS-1$
+        }
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
 
 }
