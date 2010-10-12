76454050b986ed26efb597c2bcbd3911b4a6387e
Benoit Langlois <benoit.langlois@thalesgroup.com>
2010-10-12 15:53:39 +0000

    remove the according uri map entry when a resource is removed.

--- plugins/org.eclipse.egf.core/src/org/eclipse/egf/core/domain/EGFResourceSet.java
+++ plugins/org.eclipse.egf.core/src/org/eclipse/egf/core/domain/EGFResourceSet.java
@@ -11,8 +11,10 @@
 package org.eclipse.egf.core.domain;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.egf.core.EGFCorePlugin;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -22,6 +24,28 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
  * 
  */
 public class EGFResourceSet extends ResourceSetImpl {
+
+    protected class EGFResourcesEList<E extends Object & Resource> extends ResourcesEList<E> {
+
+        private static final long serialVersionUID = 1L;
+
+        private Map<URI, Resource> _uriResourceMap;
+
+        public EGFResourcesEList(Map<URI, Resource> uriResourceMap) {
+            super();
+            _uriResourceMap = uriResourceMap;
+        }
+
+        @Override
+        public E remove(int i) {
+            E object = super.remove(i);
+            if (object != null) {
+                _uriResourceMap.remove(((Resource) object).getURI());
+            }
+            return object;
+        }
+
+    }
 
     public EGFResourceSet() {
         super();
@@ -35,6 +59,14 @@ public class EGFResourceSet extends ResourceSetImpl {
             return null;
         }
         return super.getResource(URI.createURI(URI.decode(uri.toString())), loadOnDemand);
+    }
+
+    @Override
+    public EList<Resource> getResources() {
+        if (resources == null) {
+            resources = new EGFResourcesEList<Resource>(getURIResourceMap());
+        }
+        return resources;
     }
 
 }
