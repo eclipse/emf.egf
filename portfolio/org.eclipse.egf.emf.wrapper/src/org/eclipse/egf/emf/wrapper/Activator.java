212e57ea845cc82f1b32d6b9ea0d98703a475d2e
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-10-29 09:46:30 +0000

    remove a warning

--- portfolio/org.eclipse.egf.emf.wrapper/src/org/eclipse/egf/emf/wrapper/Activator.java
+++ portfolio/org.eclipse.egf.emf.wrapper/src/org/eclipse/egf/emf/wrapper/Activator.java
@@ -18,38 +18,37 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends EGFAbstractPlugin {
 
-  // The shared instance
-  private static Activator _plugin;
+    // The shared instance
+    private static Activator _plugin;
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void start(BundleContext context) throws Exception {
-    super.start(context);
-    _plugin = this;
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        _plugin = this;
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void stop(BundleContext context) throws Exception {
-    _plugin = null;
-    super.stop(context);
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        _plugin = null;
+        super.stop(context);
+    }
 
-  /**
-   * Returns the shared instance
-   * 
-   * @return the shared instance
-   */
-  public static Activator getDefault() {
-    return _plugin;
-  }
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return _plugin;
+    }
 
 }
