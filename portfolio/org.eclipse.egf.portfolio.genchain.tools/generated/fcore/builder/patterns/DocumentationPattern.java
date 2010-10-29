2445dab847a9e888260932cc82b2737b58ec4ebe
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-10-29 10:00:23 +0000

    1 - use TargetPlatformResourceSet rather than EGFResourceSet 2 - use PACKAGE and CLASS variables 3 - remove dead templates.

--- portfolio/org.eclipse.egf.portfolio.genchain.tools/generated/fcore/builder/patterns/DocumentationPattern.java
+++ portfolio/org.eclipse.egf.portfolio.genchain.tools/generated/fcore/builder/patterns/DocumentationPattern.java
@@ -15,74 +15,74 @@ import org.eclipse.egf.portfolio.genchain.tools.utils.DocumentationHelper;
 
 public class DocumentationPattern {
 
-	public DocumentationPattern() {
-		//Here is the constructor
-		// add initialisation of the pattern variables (declaration has been already done).
-	}
+    public DocumentationPattern() {
+        //Here is the constructor
+        // add initialisation of the pattern variables (declaration has been already done).
+    }
 
-	public void generate(Object argument) throws Exception {
-		InternalPatternContext ctx = (InternalPatternContext) argument;
-		IQuery.ParameterDescription paramDesc = null;
-		Map<String, String> queryCtx = null;
-		List<Object> parameterList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+    public void generate(Object argument) throws Exception {
+        InternalPatternContext ctx = (InternalPatternContext) argument;
+        IQuery.ParameterDescription paramDesc = null;
+        Map<String, String> queryCtx = null;
+        List<Object> parameterList = null;
+        //this pattern can only be called by another (i.e. it's not an entry point in execution)
 
-		for (Object parameterParameter : parameterList) {
+        for (Object parameterParameter : parameterList) {
 
-			this.parameter = (org.eclipse.egf.portfolio.genchain.generationChain.DocumentationGeneration) parameterParameter;
+            this.parameter = (org.eclipse.egf.portfolio.genchain.generationChain.DocumentationGeneration) parameterParameter;
 
-			if (preCondition())
-				orchestration((PatternContext) argument);
+            if (preCondition())
+                orchestration((PatternContext) argument);
 
-		}
-		if (ctx.useReporter()) {
-			ctx.getReporter().executionFinished(ctx.getExecutionBuffer().toString(), ctx);
-			ctx.clearBuffer();
-		}
-	}
+        }
+        if (ctx.useReporter()) {
+            ctx.getReporter().executionFinished(ctx.getExecutionBuffer().toString(), ctx);
+            ctx.clearBuffer();
+        }
+    }
 
-	public String orchestration(PatternContext ctx) throws Exception {
-		InternalPatternContext ictx = (InternalPatternContext) ctx;
-		int executionIndex = ictx.getExecutionBuffer().length();
-		method_body(ictx.getBuffer(), ictx);
+    public String orchestration(PatternContext ctx) throws Exception {
+        InternalPatternContext ictx = (InternalPatternContext) ctx;
+        int executionIndex = ictx.getExecutionBuffer().length();
+        method_body(ictx.getBuffer(), ictx);
 
-		String loop = ictx.getBuffer().toString();
-		if (ictx.useReporter()) {
-			ictx.getExecutionBuffer().append(ictx.getBuffer().substring(ictx.getExecutionCurrentIndex()));
-			ictx.setExecutionCurrentIndex(0);
-			Map<String, Object> parameterValues = new HashMap<String, Object>();
-			parameterValues.put("parameter", this.parameter);
-			String outputWithCallBack = ictx.getExecutionBuffer().substring(executionIndex);
-			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
-			ictx.clearBuffer();
-		}
-		return loop;
-	}
+        String loop = ictx.getBuffer().toString();
+        if (ictx.useReporter()) {
+            ictx.getExecutionBuffer().append(ictx.getBuffer().substring(ictx.getExecutionCurrentIndex()));
+            ictx.setExecutionCurrentIndex(0);
+            Map<String, Object> parameterValues = new HashMap<String, Object>();
+            parameterValues.put("parameter", this.parameter);
+            String outputWithCallBack = ictx.getExecutionBuffer().substring(executionIndex);
+            ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+            ictx.clearBuffer();
+        }
+        return loop;
+    }
 
-	protected void method_body(final StringBuffer out, final PatternContext ctx) throws Exception {
-		Map<GenerationElement, FactoryComponent> fcs = (Map<GenerationElement, FactoryComponent>) ctx.getValue(FcoreBuilderConstants.CURRENT_FCORE);
+    protected void method_body(final StringBuffer out, final PatternContext ctx) throws Exception {
+        Map<GenerationElement, FactoryComponent> fcs = (Map<GenerationElement, FactoryComponent>) ctx.getValue(FcoreBuilderConstants.CURRENT_FCORE);
 
-		FactoryComponent fc = fcs.get((GenerationElement) (parameter.eContainer()));
+        FactoryComponent fc = fcs.get((GenerationElement) (parameter.eContainer()));
 
-		// add invocation to documentation generation FC
-		DocumentationHelper.addDocumentationInvocation(fc, parameter.getModelPath(), parameter.getOutputDirectoryPath(), parameter.getPluginName());
+        // add invocation to documentation generation FC
+        DocumentationHelper.addDocumentationInvocation(fc, parameter.getModelPath(), parameter.getOutputDirectoryPath(), parameter.getPluginName());
 
-	}
+    }
 
-	public boolean preCondition() throws Exception {
-		return true;
-	}
+    public boolean preCondition() throws Exception {
+        return true;
+    }
 
-	protected org.eclipse.egf.portfolio.genchain.generationChain.DocumentationGeneration parameter;
+    protected org.eclipse.egf.portfolio.genchain.generationChain.DocumentationGeneration parameter;
 
-	public void set_parameter(org.eclipse.egf.portfolio.genchain.generationChain.DocumentationGeneration parameter) {
-		this.parameter = parameter;
-	}
+    public void set_parameter(org.eclipse.egf.portfolio.genchain.generationChain.DocumentationGeneration parameter) {
+        this.parameter = parameter;
+    }
 
-	public Map<String, Object> getParameters() {
-		Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("parameter", this.parameter);
-		return parameters;
-	}
+    public Map<String, Object> getParameters() {
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("parameter", this.parameter);
+        return parameters;
+    }
 
 }
