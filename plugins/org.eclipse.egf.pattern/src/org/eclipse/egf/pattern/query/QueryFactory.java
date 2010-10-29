d8105867becc375089bad523395c00477a91ece4
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-10-29 09:27:22 +0000

    runtime management support

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/query/QueryFactory.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/query/QueryFactory.java
@@ -15,7 +15,10 @@
 
 package org.eclipse.egf.pattern.query;
 
+import org.eclipse.core.internal.registry.Handle;
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
 import org.eclipse.egf.core.platform.pde.IPlatformBundle;
 import org.eclipse.egf.core.platform.pde.IPlatformExtensionPointFactory;
 import org.eclipse.egf.core.platform.pde.PlatformExtensionPointURI;
@@ -34,30 +37,65 @@ public class QueryFactory implements IPlatformExtensionPointFactory<QueryKind> {
 
     private static final String NAME_ATTR = "name"; //$NON-NLS-1$
 
-    public QueryKind createExtensionPoint(IPlatformBundle platformBundle, IPluginElement pluginElement) {
-        if (pluginElement == null) {
+    public QueryKind createExtensionPoint(IPlatformBundle platformBundle, Object object) {
+        if (object instanceof IPluginElement) {
+            return create(platformBundle, (IPluginElement) object);
+        } else if (object instanceof IConfigurationElement) {
+            return create(platformBundle, (IConfigurationElement) object);
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    private QueryKind create(IPlatformBundle platformBundle, IPluginElement element) {
+        if (element == null) {
             return null;
         }
-        IPluginAttribute id = pluginElement.getAttribute(ID_ATTR);
+        IPluginAttribute id = element.getAttribute(ID_ATTR);
         if (id == null || id.getValue() == null || id.getValue().trim().length() == 0) {
             return null;
         }
-        IPluginAttribute className = pluginElement.getAttribute(CLASS_ATTR);
+        IPluginAttribute className = element.getAttribute(CLASS_ATTR);
         if (className == null || className.getValue() == null || className.getValue().trim().length() == 0) {
             return null;
         }
-        IPluginAttribute name = pluginElement.getAttribute(NAME_ATTR);
+        IPluginAttribute name = element.getAttribute(NAME_ATTR);
         if (name == null || name.getValue() == null || name.getValue().trim().length() == 0) {
             return null;
         }
         return new QueryKindImpl(platformBundle, id.getValue(), className.getValue(), name.getValue());
     }
 
+    private QueryKind create(IPlatformBundle platformBundle, IConfigurationElement element) {
+        if (element == null) {
+            return null;
+        }
+        // Store identifier
+        IExtension declaringExtension = element.getDeclaringExtension();
+        String uniqueIdentifier = declaringExtension.getUniqueIdentifier();
+        int handleId = -1;
+        if (declaringExtension instanceof Handle) {
+            handleId = ((Handle) declaringExtension).getId();
+        }
+        String id = element.getAttribute(ID_ATTR);
+        if (id == null || id.trim().length() == 0) {
+            return null;
+        }
+        String className = element.getAttribute(CLASS_ATTR);
+        if (className == null || className.trim().length() == 0) {
+            return null;
+        }
+        String name = element.getAttribute(NAME_ATTR);
+        if (name == null || name.trim().length() == 0) {
+            return null;
+        }
+        return new QueryKindImpl(platformBundle, id, uniqueIdentifier, handleId, className, name);
+    }
+
     private static class QueryKindImpl extends PlatformExtensionPointURI implements QueryKind {
 
-        private final String className;
+        private final String _className;
 
-        private final String name;
+        private final String _name;
 
         public QueryKindImpl(IPlatformBundle bundle, String id, String className, String name) {
             super(bundle, id);
@@ -65,16 +103,26 @@ public class QueryFactory implements IPlatformExtensionPointFactory<QueryKind> {
             Assert.isLegal(className.trim().length() != 0);
             Assert.isNotNull(name);
             Assert.isLegal(name.trim().length() != 0);
-            this.className = className.trim();
-            this.name = name.trim();
+            _className = className.trim();
+            _name = name.trim();
+        }
+
+        public QueryKindImpl(IPlatformBundle bundle, String id, String uniqueIdentifier, int handleId, String className, String name) {
+            super(bundle, id, uniqueIdentifier, handleId);
+            Assert.isNotNull(className);
+            Assert.isLegal(className.trim().length() != 0);
+            Assert.isNotNull(name);
+            Assert.isLegal(name.trim().length() != 0);
+            _className = className.trim();
+            _name = name.trim();
         }
 
         public String getClassName() {
-            return className;
+            return _className;
         }
 
         public String getName() {
-            return name;
+            return _name;
         }
 
         @Override
@@ -96,4 +144,5 @@ public class QueryFactory implements IPlatformExtensionPointFactory<QueryKind> {
         }
 
     }
+
 }
