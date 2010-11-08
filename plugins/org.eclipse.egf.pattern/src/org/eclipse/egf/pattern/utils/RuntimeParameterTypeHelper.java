a4adefbf4b82dadf6450fb82efc1fa75fbc42fa6
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-08 09:43:59 +0000

    api cleaning

--- plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/utils/RuntimeParameterTypeHelper.java
+++ plugins/org.eclipse.egf.pattern/src/org/eclipse/egf/pattern/utils/RuntimeParameterTypeHelper.java
@@ -19,7 +19,6 @@ import org.eclipse.egf.common.helper.EMFHelper;
 import org.eclipse.egf.core.EGFCorePlugin;
 import org.eclipse.egf.pattern.l10n.EGFPatternMessages;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.edit.domain.EditingDomain;
@@ -43,38 +42,43 @@ public class RuntimeParameterTypeHelper {
         if (type == null || type.trim().length() == 0) {
             throw new IllegalArgumentException();
         }
+        // Java Type
         int index = type.indexOf('#');
-        if (index > 0) {
-            return loadEClass(type, index);
+        if (index == -1) {
+            try {
+                return getClass().getClassLoader().loadClass(type);
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException(e);
+            }
         }
+        // URI Type
+        return loadEClass(type);
+    }
+
+    private Object loadEClass(String type) {
+        String instanceClassName = EMFHelper.getInstanceClassName(getEPackage(type), URI.createURI(type));
+        if (instanceClassName == null) {
+            return null;
+        }
+        Class<?> clazz = null;
         try {
-            return getClass().getClassLoader().loadClass(type);
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException(e);
+            clazz = getClass().getClassLoader().loadClass(instanceClassName);
+        } catch (ClassNotFoundException cnfe) {
+            // Just Ignore
         }
+        if (clazz == null) {
+            throw new IllegalStateException(NLS.bind(EGFPatternMessages.classloader_error3, instanceClassName, type));
+        }
+        return clazz;
     }
 
-    private EClass loadEClass(String type, int index) {
-        String className = ParameterTypeHelper.getClassName(type, index);
-        EPackage ePackage = getEPackage(type, index);
-        EClass eClassifier = (EClass) ePackage.getEClassifier(className);
-        if (eClassifier == null) {
-            throw new IllegalStateException(NLS.bind(EGFPatternMessages.classloader_error3, className, getNsURI(type, index)));
-        }
-        return eClassifier;
-    }
-
-    private EPackage getEPackage(String type, int index) {
+    private EPackage getEPackage(String type) {
         EditingDomain editingDomain = TransactionalEditingDomain.Registry.INSTANCE.getEditingDomain(EGFCorePlugin.EDITING_DOMAIN_ID);
         EObject eObject = editingDomain.getResourceSet().getEObject(URI.createURI(type), true);
         if (eObject == null) {
             throw new IllegalStateException(NLS.bind(EGFPatternMessages.assembly_error7, type));
         }
         return EMFHelper.getEPackage(eObject);
-    }
-
-    private String getNsURI(String type, int index) {
-        return type.substring(0, index);
     }
 
     private RuntimeParameterTypeHelper() {
