406c3cb2a4302b90f7f6446d21e591d2006e6868
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-10 14:52:56 +0000

    new messages

--- plugins/org.eclipse.egf.application/src/org/eclipse/egf/application/internal/l10n/ApplicationMessages.java
+++ plugins/org.eclipse.egf.application/src/org/eclipse/egf/application/internal/l10n/ApplicationMessages.java
@@ -16,31 +16,41 @@ import org.eclipse.osgi.util.NLS;
 
 public class ApplicationMessages {
 
-  private static final String BUNDLE_NAME = "org.eclipse.egf.application.internal.l10n.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "org.eclipse.egf.application.internal.l10n.messages"; //$NON-NLS-1$
 
-  private ApplicationMessages() {
-    // Do not instantiate
-  }
+    private ApplicationMessages() {
+        // Do not instantiate
+    }
 
-  static {
-    // load message values from bundle file
-    NLS.initializeMessages(BUNDLE_NAME, ApplicationMessages.class);
-  }
+    static {
+        // load message values from bundle file
+        NLS.initializeMessages(BUNDLE_NAME, ApplicationMessages.class);
+    }
 
-  public static String ActivityApplication_Arguments_Usage;
-  public static String ActivityApplication_Invalid_URI_Argument;
-  public static String ActivityApplication_EObject_Loading_Error;
-  public static String ActivityApplication_Invalid_Activity_Argument;
+    public static String ActivityApplication_Arguments_Usage;
 
-  public static String ActivityRunner_NullActivity;
-  public static String ActivityRunner_ActivityRunner_Exception;
-  public static String ActivityRunner_PreInvokeProblems_message;
-  public static String ActivityRunner_PostInvokeProblems_message;
+    public static String ActivityApplication_Invalid_URI_Argument;
 
-  public static String EGFTaskLaunchConfigurationDelegate_error_no_socket;
+    public static String ActivityApplication_EObject_Loading_Error;
 
-  public static String NestedActivity_Invalid_URI_Attribute;
-  public static String NestedActivity_EObject_Loading_Error;
-  public static String NestedActivity_Invalid_Activity_Argument;
+    public static String ActivityApplication_Invalid_Activity_Argument;
+
+    public static String ActivityRunner_Run;
+
+    public static String ActivityRunner_NullActivity;
+
+    public static String ActivityRunner_Exception;
+
+    public static String ActivityRunner_PreInvokeProblems_message;
+
+    public static String ActivityRunner_PostInvokeProblems_message;
+
+    public static String EGFTaskLaunchConfigurationDelegate_error_no_socket;
+
+    public static String NestedActivity_Invalid_URI_Attribute;
+
+    public static String NestedActivity_EObject_Loading_Error;
+
+    public static String NestedActivity_Invalid_Activity_Argument;
 
 }
