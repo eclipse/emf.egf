8ec93d75548aee3918edddee3d3b53d25d1d2fca
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-10 14:19:27 +0000

    format update

--- plugins/org.eclipse.egf.pattern.ftask/src/org/eclipse/egf/pattern/ftask/tasks/FilterStrategyTask.java
+++ plugins/org.eclipse.egf.pattern.ftask/src/org/eclipse/egf/pattern/ftask/tasks/FilterStrategyTask.java
@@ -35,4 +35,5 @@ public class FilterStrategyTask extends AbstractStrategyTask {
         super.preExecute(context, monitor);
         parameter = context.getInputValue(FILTER_NAME_PARAMETER, String.class);
     }
+
 }
