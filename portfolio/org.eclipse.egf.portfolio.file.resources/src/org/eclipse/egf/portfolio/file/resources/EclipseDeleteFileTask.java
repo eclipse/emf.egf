200a1d22bd247038c5bef2e1c8f6567a333f8f76
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-15 14:51:14 +0000

    better exception handling

--- portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/EclipseDeleteFileTask.java
+++ portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/EclipseDeleteFileTask.java
@@ -16,11 +16,12 @@
 package org.eclipse.egf.portfolio.file.resources;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.egf.core.producer.InvocationException;
 import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
 import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
-
 
 /**
  * @author Benoit Langlois
@@ -28,30 +29,29 @@ import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
  */
 public class EclipseDeleteFileTask implements ITaskProduction {
 
-	public void preExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing to do
-	}
+    public void preExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing to do
+    }
 
-	public void doExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
-		String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
-		String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
+    public void doExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
+        String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
+        String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
+        try {
+            IFile file = FileUtil.getExistingFile(projectName, folder, fileName);
+            if (file.exists()) {
+                file.delete(false, true, monitor);
+            }
+        } catch (Throwable t) {
+            if (t instanceof CoreException) {
+                throw new InvocationException(t);
+            }
+            throw new InvocationException(new CoreException(EGFFileResourcesActivator.getDefault().newStatus(IStatus.ERROR, "Unexpected Exception", t))); //$NON-NLS-1$
+        }
+    }
 
-		try {
-			IFile file = FileUtil.getExistingFile(projectName, folder, fileName);
-			if (file.exists()) {
-				file.delete(false, true, null);
-			}
-		} catch (Exception e) {
-			EGFFileResourcesActivator.getDefault().logError(e);
-		}
-	}
-
-	public void postExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing to do
-	}
+    public void postExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing to do
+    }
 
 }
