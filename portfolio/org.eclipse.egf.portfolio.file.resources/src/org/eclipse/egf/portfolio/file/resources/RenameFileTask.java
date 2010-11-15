673038f65e7c906f0f7d5238df0e868f6547bb04
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-15 14:52:46 +0000

    format update

--- portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/RenameFileTask.java
+++ portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/RenameFileTask.java
@@ -20,44 +20,40 @@ import org.eclipse.egf.core.producer.InvocationException;
 import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
 import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
 
-
 /**
  * @author Benoit Langlois
  *
  */
 public class RenameFileTask implements ITaskProduction {
 
-	public void preExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing to do
-	}
+    public void preExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing to do
+    }
 
-	public void doExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
-		String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
-		String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
-		String newFolder = productionContext.getInputValue(FileConstants.NEW_FOLDER_CONTRACT, String.class);
-		String newFileName = productionContext.getInputValue(FileConstants.NEW_FILE_NAME_CONTRACT, String.class);
+    public void doExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
+        String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
+        String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
+        String newFolder = productionContext.getInputValue(FileConstants.NEW_FOLDER_CONTRACT, String.class);
+        String newFileName = productionContext.getInputValue(FileConstants.NEW_FILE_NAME_CONTRACT, String.class);
 
-		// Not implemented
-//		try {
-//			IFile file = FileUtil.getExistingFile(projectName, folder, fileName);
-//			if (file.exists()) {
-//				IFile newFile = FileUtil.getExistingFile(projectName, folder, newFileName);
-//				if (newFile.exists()) {
-//					newFile.delete(false, false, null);
-//				}
-//				...
-//			}
-//		} catch (Exception e) {
-//			EGFFileResourcesActivator.getDefault().logError(e);
-//		}
-	}
+        // Not implemented
+        //		try {
+        //			IFile file = FileUtil.getExistingFile(projectName, folder, fileName);
+        //			if (file.exists()) {
+        //				IFile newFile = FileUtil.getExistingFile(projectName, folder, newFileName);
+        //				if (newFile.exists()) {
+        //					newFile.delete(false, false, null);
+        //				}
+        //				...
+        //			}
+        //		} catch (Exception e) {
+        //			EGFFileResourcesActivator.getDefault().logError(e);
+        //		}
+    }
 
-	public void postExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing to do
-	}
+    public void postExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing to do
+    }
 
 }
