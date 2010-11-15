f518e15bb7ccac9ef5c5210033bb51ad2268b1d3
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-15 14:52:01 +0000

    better exception handling

--- portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/ReadFileTask.java
+++ portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/ReadFileTask.java
@@ -20,11 +20,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.egf.core.producer.InvocationException;
 import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
 import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
-
 
 /**
  * @author Benoit Langlois
@@ -32,50 +33,49 @@ import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
  */
 public class ReadFileTask implements ITaskProduction {
 
-	public void preExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing to do
-	}
+    public void preExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing to do
+    }
 
-	public void doExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		String contents = new String();
-		String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
-		String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
-		String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
+    public void doExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        String contents = new String();
+        String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
+        String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
+        String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
 
-		BufferedReader bufferedReader = null;
-		try {
-			IFile file = FileUtil.getExistingFile(projectName, folder, fileName);
-			if (file.exists()) {
-				StringBuffer stringBuffer = new StringBuffer();
-				bufferedReader = new BufferedReader(new InputStreamReader(file.getContents()));
-				String line = null;
-				while ((line = bufferedReader.readLine()) != null) {
-					stringBuffer.append(line);
-					stringBuffer.append("\n"); //$NON-NLS-1$
-				}
-				contents = stringBuffer.toString();
-			}
-		} catch (Exception e) {
-			EGFFileResourcesActivator.getDefault().logError(e);
-		} finally {
-			if (bufferedReader != null) {
-				try {
-					bufferedReader.close();
-				} catch (IOException e) {
-					EGFFileResourcesActivator.getDefault().logError(e);
-				}
-			}
-		}
+        BufferedReader bufferedReader = null;
+        try {
+            IFile file = FileUtil.getExistingFile(projectName, folder, fileName);
+            if (file.exists()) {
+                StringBuffer stringBuffer = new StringBuffer();
+                bufferedReader = new BufferedReader(new InputStreamReader(file.getContents()));
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(line);
+                    stringBuffer.append("\n"); //$NON-NLS-1$
+                }
+                contents = stringBuffer.toString();
+            }
+        } catch (Throwable t) {
+            if (t instanceof CoreException) {
+                throw new InvocationException(t);
+            }
+            throw new InvocationException(new CoreException(EGFFileResourcesActivator.getDefault().newStatus(IStatus.ERROR, "Unexpected Exception", t))); //$NON-NLS-1$
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    // Ignore
+                }
+            }
+        }
+        productionContext.setOutputValue(FileConstants.CONTENTS_NAME_CONTRACT, contents);
 
-		productionContext.setOutputValue(FileConstants.CONTENTS_NAME_CONTRACT, contents);
+    }
 
-	}
-
-	public void postExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing to do
-	}
+    public void postExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing to do
+    }
 
 }
