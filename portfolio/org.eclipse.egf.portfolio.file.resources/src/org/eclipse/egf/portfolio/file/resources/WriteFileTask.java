1585f7f9fa11a784f8e571f9bc0a5c2cea51ffa7
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-15 14:48:56 +0000

    better exception handling

--- portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/WriteFileTask.java
+++ portfolio/org.eclipse.egf.portfolio.file.resources/src/org/eclipse/egf/portfolio/file/resources/WriteFileTask.java
@@ -20,10 +20,10 @@ import java.io.ByteArrayInputStream;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.egf.core.producer.InvocationException;
 import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
 import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
-
 
 /**
  * @author Benoit Langlois
@@ -31,43 +31,41 @@ import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
  */
 public class WriteFileTask implements ITaskProduction {
 
-	public void preExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing done
-	}
+    public void preExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing done
+    }
 
-	public void doExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		
-		String contents = productionContext.getInputValue(FileConstants.CONTENTS_NAME_CONTRACT, String.class);
-		String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
-		String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
-		String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
-		Boolean appendMode = productionContext.getInputValue(FileConstants.APPEND_MODE_CONTRACT, Boolean.class);
+    public void doExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
 
-		ByteArrayInputStream fileContents = new ByteArrayInputStream(contents.getBytes());
-		try {
-			IFile file = FileUtil.getNewFile(projectName, folder, fileName);
-			if (file.exists()) {
-				if (appendMode == true) {
-					file.appendContents(fileContents, false, false, null);
-				}
-				else {
-					file.setContents(fileContents, true, false, null);
-				}
-			}
-			else {
-				file.create(fileContents, true, null);
-			}
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
+        String contents = productionContext.getInputValue(FileConstants.CONTENTS_NAME_CONTRACT, String.class);
+        String projectName = productionContext.getInputValue(FileConstants.PROJECT_NAME_CONTRACT, String.class);
+        String folder = productionContext.getInputValue(FileConstants.FOLDER_CONTRACT, String.class);
+        String fileName = productionContext.getInputValue(FileConstants.FILE_NAME_CONTRACT, String.class);
+        Boolean appendMode = productionContext.getInputValue(FileConstants.APPEND_MODE_CONTRACT, Boolean.class);
 
-	}
+        ByteArrayInputStream fileContents = new ByteArrayInputStream(contents.getBytes());
+        try {
+            IFile file = FileUtil.getNewFile(projectName, folder, fileName);
+            if (file.exists()) {
+                if (appendMode == true) {
+                    file.appendContents(fileContents, false, false, null);
+                } else {
+                    file.setContents(fileContents, true, false, null);
+                }
+            } else {
+                file.create(fileContents, true, null);
+            }
+        } catch (Throwable t) {
+            if (t instanceof CoreException) {
+                throw new InvocationException(t);
+            }
+            throw new InvocationException(new CoreException(EGFFileResourcesActivator.getDefault().newStatus(IStatus.ERROR, "Unexpected Exception", t))); //$NON-NLS-1$
+        }
 
-	public void postExecute(ITaskProductionContext productionContext,
-			IProgressMonitor monitor) throws InvocationException {
-		// Nothing done
-	}
+    }
+
+    public void postExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+        // Nothing done
+    }
 
 }
