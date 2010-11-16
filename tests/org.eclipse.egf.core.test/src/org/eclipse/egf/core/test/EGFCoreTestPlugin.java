1397f29a0400cced072ac857918d087c5f15591c
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-16 09:24:51 +0000

    format update

--- tests/org.eclipse.egf.core.test/src/org/eclipse/egf/core/test/EGFCoreTestPlugin.java
+++ tests/org.eclipse.egf.core.test/src/org/eclipse/egf/core/test/EGFCoreTestPlugin.java
@@ -18,38 +18,38 @@ import org.osgi.framework.BundleContext;
  */
 public class EGFCoreTestPlugin extends EGFAbstractPlugin {
 
-  // The shared instance
-  private static EGFCoreTestPlugin __plugin;
+    // The shared instance
+    private static EGFCoreTestPlugin __plugin;
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.ui.plugin.Plugin#start(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void start(BundleContext context) throws Exception {
-    super.start(context);
-    __plugin = this;
-  }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.Plugin#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        __plugin = this;
+    }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.ui.plugin.Plugin#stop(org.osgi.framework.BundleContext)
-   */
-  @Override
-  public void stop(BundleContext context) throws Exception {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
         super.stop(context);
-    __plugin = null;
-  }
+        __plugin = null;
+    }
 
-  /**
-   * Returns the shared instance
-   * 
-   * @return the shared instance
-   */
-  public static EGFCoreTestPlugin getDefault() {
-    return __plugin;
-  }
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static EGFCoreTestPlugin getDefault() {
+        return __plugin;
+    }
 
 }
