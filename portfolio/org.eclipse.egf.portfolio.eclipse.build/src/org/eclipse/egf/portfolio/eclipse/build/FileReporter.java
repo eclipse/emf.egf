2dc69ee5df293fc6a5b479b262f21b4755c7d294
Xavier Maysonnave <xavier.maysonnave@soyatec.com>
2010-11-24 14:58:02 +0000

    build portfolio : fileReporter now support platform:/resource/... as a generation path

--- portfolio/org.eclipse.egf.portfolio.eclipse.build/src/org/eclipse/egf/portfolio/eclipse/build/FileReporter.java
+++ portfolio/org.eclipse.egf.portfolio.eclipse.build/src/org/eclipse/egf/portfolio/eclipse/build/FileReporter.java
@@ -14,12 +14,19 @@
  */
 package org.eclipse.egf.portfolio.eclipse.build;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.egf.model.pattern.PatternContext;
 import org.eclipse.egf.model.pattern.PatternExecutionReporter;
@@ -42,22 +49,56 @@ public class FileReporter implements PatternExecutionReporter {
 		if (fileNameString == null || filePathString == null || generationPath == null)
 			return;
 
-		IPath dirPath = new Path(generationPath);
-		dirPath = dirPath.append(filePathString);
-
-		File dirFile = dirPath.toFile();
-		if (!dirFile.exists())
-			dirFile.mkdirs();
-
+		IPath dirPath = new Path(generationPath).append(filePathString);
 		IPath filePath = dirPath.append(fileNameString);
-		
+
 		try {
-			FileWriter fileWriter = new FileWriter(filePath.toFile());
-			fileWriter.write(outputWithCallBack);
-			fileWriter.close();
-		} catch (IOException e) {
-			throw new RuntimeException("Unable to generate file " + filePath, e); //$NON-NLS-1$
-		}
+            if (generationPath.startsWith("platform:/resource/")) 
+                handleWorkspaceFile(outputWithCallBack, filePath);
+            else {
+                handleFile(outputWithCallBack, filePath);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to generate file " + filePath, e); //$NON-NLS-1$
+        }
 	}
+
+    protected void handleFile(String outputWithCallBack, IPath filePath) throws IOException {
+        File fileFile = filePath.toFile();
+        if (!fileFile.getParentFile().exists())
+            fileFile.getParentFile().mkdirs();
+
+        FileWriter fileWriter = new FileWriter(fileFile);
+        fileWriter.write(outputWithCallBack);
+        fileWriter.close();
+    }
+
+    protected void handleWorkspaceFile(String outputWithCallBack, IPath filePath) throws CoreException {
+        String projectName = filePath.segment(1);
+        IPath projectRelativePath = filePath.removeFirstSegments(2).setDevice(null);
+        
+        IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+        NullProgressMonitor monitor = new NullProgressMonitor();
+        
+        if (!project.exists()) 
+            project.create(monitor);
+        
+        if (!project.isOpen())
+            project.open(monitor);
+        
+        for(int i = projectRelativePath.segmentCount() - 1 ; i >= 1 ; i--) {
+            IFolder folder = project.getFolder(projectRelativePath.removeLastSegments(i));
+            if (!folder.exists())
+                folder.create(true, true, monitor);
+        }
+        
+        IFile file = project.getFile(projectRelativePath);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputWithCallBack.getBytes());
+        
+        if (!file.exists())
+            file.create(inputStream, true, monitor);
+        else
+            file.setContents(inputStream, true, true, monitor);
+    }
 
 }
