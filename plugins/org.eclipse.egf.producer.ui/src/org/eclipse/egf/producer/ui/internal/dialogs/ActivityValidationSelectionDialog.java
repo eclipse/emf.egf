16a4a404464e73d8df48ca047b9d65f72b04ca00
Thomas Guiu <thomas.guiu@soyatec.com>
2011-01-03 10:18:29 +0000

    fix preference checkbox behavior

--- plugins/org.eclipse.egf.producer.ui/src/org/eclipse/egf/producer/ui/internal/dialogs/ActivityValidationSelectionDialog.java
+++ plugins/org.eclipse.egf.producer.ui/src/org/eclipse/egf/producer/ui/internal/dialogs/ActivityValidationSelectionDialog.java
@@ -33,8 +33,6 @@ import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -42,149 +40,145 @@ import org.eclipse.swt.widgets.Shell;
 
 public class ActivityValidationSelectionDialog extends AbstractCheckboxSelectionDialog {
 
-  private final String SETTINGS_ID = EGFProducerUIPlugin.getDefault().getPluginID() + ".SCOPED_SAVE_SELECTION_DIALOG"; //$NON-NLS-1$
+    private final String SETTINGS_ID = EGFProducerUIPlugin.getDefault().getPluginID() + ".SCOPED_SAVE_SELECTION_DIALOG"; //$NON-NLS-1$
 
-  private Button _savePreferences;
+    private Button _savePreferences;
 
-  private List<Activity> _activities;
+    private List<Activity> _activities;
 
-  private ComposedAdapterFactory _adapterFactory;
+    private ComposedAdapterFactory _adapterFactory;
 
-  public ActivityValidationSelectionDialog(Shell parentShell, List<Activity> activities) {
-    super(parentShell);
-    _activities = activities;
-    setTitle(ProducerUIMessages.ActivityValidationSelectionDialog_Title);
-    setShellStyle(getShellStyle() | SWT.RESIZE);
-    setShowSelectAllButtons(true);
-    // Create an adapter factory that yields item providers.
-    _adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
-    _adapterFactory.addAdapterFactory(new FcoreResourceItemProviderAdapterFactory());
-    _adapterFactory.addAdapterFactory(new FprodItemProviderAdapterFactory());
-    _adapterFactory.addAdapterFactory(new FcoreItemProviderAdapterFactory());
-    _adapterFactory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
-  }
+    public ActivityValidationSelectionDialog(Shell parentShell, List<Activity> activities) {
+        super(parentShell);
+        _activities = activities;
+        setTitle(ProducerUIMessages.ActivityValidationSelectionDialog_Title);
+        setShellStyle(getShellStyle() | SWT.RESIZE);
+        setShowSelectAllButtons(true);
+        // Create an adapter factory that yields item providers.
+        _adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+        _adapterFactory.addAdapterFactory(new FcoreResourceItemProviderAdapterFactory());
+        _adapterFactory.addAdapterFactory(new FprodItemProviderAdapterFactory());
+        _adapterFactory.addAdapterFactory(new FcoreItemProviderAdapterFactory());
+        _adapterFactory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getContentProvider()
-   */
-  @Override
-  protected IContentProvider getContentProvider() {
-    return new AdapterFactoryContentProvider(_adapterFactory);
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getContentProvider()
+     */
+    @Override
+    protected IContentProvider getContentProvider() {
+        return new AdapterFactoryContentProvider(_adapterFactory);
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getLabelProvider()
-   */
-  @Override
-  protected ILabelProvider getLabelProvider() {
-    return new LabelProvider() {
-      ILabelProvider _labelProvider = new AdapterFactoryLabelProvider(_adapterFactory);
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getLabelProvider()
+     */
+    @Override
+    protected ILabelProvider getLabelProvider() {
+        return new LabelProvider() {
+            ILabelProvider _labelProvider = new AdapterFactoryLabelProvider(_adapterFactory);
 
-      @Override
-      public String getText(Object object) {
-        return _labelProvider.getText(object);
-      }
+            @Override
+            public String getText(Object object) {
+                return _labelProvider.getText(object);
+            }
 
-      @Override
-      public Image getImage(Object object) {
-        return _labelProvider.getImage(object);
-      }
-    };
-  }
+            @Override
+            public Image getImage(Object object) {
+                return _labelProvider.getImage(object);
+            }
+        };
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getDialogSettingsId()
-   */
-  @Override
-  protected String getDialogSettingsId() {
-    return SETTINGS_ID;
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getDialogSettingsId()
+     */
+    @Override
+    protected String getDialogSettingsId() {
+        return SETTINGS_ID;
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getViewerInput()
-   */
-  @Override
-  protected Object getViewerInput() {
-    return new ItemProvider(_adapterFactory, _activities);
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getViewerInput()
+     */
+    @Override
+    protected Object getViewerInput() {
+        return new ItemProvider(_adapterFactory, _activities);
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getViewerLabel()
-   */
-  @Override
-  protected String getViewerLabel() {
-    return ProducerUIMessages.ActivityValidationSelectionDialog_Select;
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#getViewerLabel()
+     */
+    @Override
+    protected String getViewerLabel() {
+        return ProducerUIMessages.ActivityValidationSelectionDialog_Select;
+    }
 
-  /**
-   * Returns the list of initial element selections.
-   * 
-   * @return List
-   */
-  @Override
-  protected List<?> getInitialElementSelections() {
-    return _activities;
-  }
+    /**
+     * Returns the list of initial element selections.
+     * 
+     * @return List
+     */
+    @Override
+    protected List<?> getInitialElementSelections() {
+        return _activities;
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog# addCustomFooterControls(org.eclipse.swt.widgets.Composite)
-   */
-  @Override
-  protected void addCustomFooterControls(Composite parent) {
-    super.addCustomFooterControls(parent);
-    _savePreferences = new Button(parent, SWT.CHECK);
-    _savePreferences.setText(ProducerUIMessages.ActivityValidationSelectionDialog_Never_Validate);
-    _savePreferences.addSelectionListener(new SelectionAdapter() {
-      @Override
-      public void widgetSelected(SelectionEvent e) {
-        getCheckBoxTableViewer().setAllChecked(_savePreferences.getSelection());
-      }
-    });
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#
+     *      addCustomFooterControls(org.eclipse.swt.widgets.Composite)
+     */
+    @Override
+    protected void addCustomFooterControls(Composite parent) {
+        super.addCustomFooterControls(parent);
+        _savePreferences = new Button(parent, SWT.CHECK);
+        _savePreferences.setText(ProducerUIMessages.ActivityValidationSelectionDialog_Never_Validate);
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#okPressed()
-   */
-  @Override
-  protected void okPressed() {
-    IPreferenceStore store = EGFCoreUIPlugin.getDefault().getPreferenceStore();
-    String val = (_savePreferences.getSelection() ? MessageDialogWithToggle.NEVER : MessageDialogWithToggle.PROMPT);
-    store.setValue(IEGFModelConstants.VALIDATE_MODEL_INSTANCES_BEFORE_LAUNCH, val);
-    super.okPressed();
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#okPressed()
+     */
+    @Override
+    protected void okPressed() {
+        IPreferenceStore store = EGFCoreUIPlugin.getDefault().getPreferenceStore();
+        String val = (_savePreferences.getSelection() ? MessageDialogWithToggle.NEVER : MessageDialogWithToggle.PROMPT);
+        store.setValue(IEGFModelConstants.VALIDATE_MODEL_INSTANCES_BEFORE_LAUNCH, val);
+        super.okPressed();
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog# addViewerListeners(org.eclipse.jface.viewers.StructuredViewer)
-   */
-  @Override
-  protected void addViewerListeners(StructuredViewer viewer) {
-    // Override to remove listener that affects the ok button
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#
+     *      addViewerListeners(org.eclipse.jface.viewers.StructuredViewer)
+     */
+    @Override
+    protected void addViewerListeners(StructuredViewer viewer) {
+        // Override to remove listener that affects the ok button
+    }
 
-  /**
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#isValid()
-   */
-  @Override
-  protected boolean isValid() {
-    return true;
-  }
+    /**
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.egf.core.ui.dialogs.AbstractCheckboxSelectionDialog#isValid()
+     */
+    @Override
+    protected boolean isValid() {
+        return true;
+    }
 
 }
