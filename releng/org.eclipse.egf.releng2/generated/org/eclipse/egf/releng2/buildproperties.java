7443bef186117eb5e786b5ac739ebb720ea2c8e3
Thomas Guiu <thomas.guiu@soyatec.com>
2011-01-20 16:25:02 +0000

    releng : regenerate egf releng

--- releng/org.eclipse.egf.releng2/generated/org/eclipse/egf/releng2/buildproperties.java
+++ releng/org.eclipse.egf.releng2/generated/org/eclipse/egf/releng2/buildproperties.java
@@ -7,22 +7,22 @@ import org.eclipse.egf.model.pattern.*;
 import org.eclipse.egf.pattern.execution.*;
 import org.eclipse.egf.pattern.query.*;
 
-public class signingarea extends org.eclipse.egf.portfolio.eclipse.build.buckminster.call.buildpropertiesadd {
+public class buildproperties extends org.eclipse.egf.portfolio.eclipse.build.buckminster.call.buildpropertiesadd {
     protected static String nl;
 
-    public static synchronized signingarea create(String lineSeparator) {
+    public static synchronized buildproperties create(String lineSeparator) {
         nl = lineSeparator;
-        signingarea result = new signingarea();
+        buildproperties result = new buildproperties();
         nl = null;
         return result;
     }
 
     public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-    protected final String TEXT_1 = "eclipse.staging.area=/shared/download-staging.priv/modeling/egf/" + NL;
+    protected final String TEXT_1 = "eclipse.staging.area=/shared/download-staging.priv/modeling/egf/" + NL + "eclipse.download.prefix=file:/home/data/httpd/download.eclipse.org" + NL + "target.os=*" + NL + "target.ws=*" + NL + "target.arch=*" + NL;
     protected final String TEXT_2 = NL;
     protected final String TEXT_3 = NL;
 
-    public signingarea() {
+    public buildproperties() {
         //Here is the constructor
         StringBuffer stringBuffer = new StringBuffer();
 
