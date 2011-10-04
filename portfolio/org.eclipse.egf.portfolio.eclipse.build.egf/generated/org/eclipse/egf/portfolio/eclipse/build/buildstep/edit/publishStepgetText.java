8b2bfe0acdb2dc49f12c6f816c199d0632940c3c
Thomas Guiu <thomas.guiu@soyatec.com>
2011-10-04 09:21:51 +0000

    update generated pattern classes

--- portfolio/org.eclipse.egf.portfolio.eclipse.build.egf/generated/org/eclipse/egf/portfolio/eclipse/build/buildstep/edit/publishStepgetText.java
+++ portfolio/org.eclipse.egf.portfolio.eclipse.build.egf/generated/org/eclipse/egf/portfolio/eclipse/build/buildstep/edit/publishStepgetText.java
@@ -8,110 +8,110 @@ import org.eclipse.egf.pattern.execution.*;
 import org.eclipse.egf.pattern.query.*;
 
 public class publishStepgetText extends org.eclipse.egf.emf.pattern.edit.call.ItemProvider.ItemProvidergetTextoverride {
-    protected static String nl;
+	protected static String nl;
 
-    public static synchronized publishStepgetText create(String lineSeparator) {
-        nl = lineSeparator;
-        publishStepgetText result = new publishStepgetText();
-        nl = null;
-        return result;
-    }
+	public static synchronized publishStepgetText create(String lineSeparator) {
+		nl = lineSeparator;
+		publishStepgetText result = new publishStepgetText();
+		nl = null;
+		return result;
+	}
 
-    public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-    protected final String TEXT_1 = "        PublishStep publishStep = (PublishStep)object;" + NL + "        if (publishStep.getComponent() != null)" + NL + "        \treturn getString(\"_UI_PublishStep_type\") + \" \" + publishStep.getComponent().getName();" + NL + "        return super.getText(object);" + NL;
-    protected final String TEXT_2 = NL;
-    protected final String TEXT_3 = NL;
+	public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+	protected final String TEXT_1 = "        PublishStep publishStep = (PublishStep)object;" + NL + "        if (publishStep.getComponent() != null)" + NL + "        \treturn getString(\"_UI_PublishStep_type\") + \" \" + publishStep.getComponent().getName();" + NL + "        return super.getText(object);" + NL;
+	protected final String TEXT_2 = NL;
+	protected final String TEXT_3 = NL;
 
-    public publishStepgetText() {
-        //Here is the constructor
-        StringBuffer stringBuffer = new StringBuffer();
+	public publishStepgetText() {
+		//Here is the constructor
+		StringBuffer stringBuffer = new StringBuffer();
 
-        // add initialisation of the pattern variables (declaration has been already done).
+		// add initialisation of the pattern variables (declaration has been already done).
 
-    }
+	}
 
-    public String generate(Object argument) throws Exception {
-        final StringBuffer stringBuffer = new StringBuffer();
+	public String generate(Object argument) throws Exception {
+		final StringBuffer stringBuffer = new StringBuffer();
 
-        InternalPatternContext ctx = (InternalPatternContext) argument;
-        Map<String, String> queryCtx = null;
-        IQuery.ParameterDescription paramDesc = null;
-        Node.Container currentNode = ctx.getNode();
+		InternalPatternContext ctx = (InternalPatternContext) argument;
+		Map<String, String> queryCtx = null;
+		IQuery.ParameterDescription paramDesc = null;
+		Node.Container currentNode = ctx.getNode();
 
-        List<Object> genClassList = null;
-        //this pattern can only be called by another (i.e. it's not an entry point in execution)
-        List<Object> genPackageList = null;
-        //this pattern can only be called by another (i.e. it's not an entry point in execution)
-        List<Object> genModelList = null;
-        //this pattern can only be called by another (i.e. it's not an entry point in execution)
-        List<Object> _ListList = null;
-        //this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> genClassList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> genPackageList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> genModelList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> _ListList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
 
-        for (Object genClassParameter : genClassList) {
-            for (Object genPackageParameter : genPackageList) {
-                for (Object genModelParameter : genModelList) {
-                    for (Object _ListParameter : _ListList) {
+		for (Object genClassParameter : genClassList) {
+			for (Object genPackageParameter : genPackageList) {
+				for (Object genModelParameter : genModelList) {
+					for (Object _ListParameter : _ListList) {
 
-                        this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
-                        this.genPackage = (org.eclipse.emf.codegen.ecore.genmodel.GenPackage) genPackageParameter;
-                        this.genModel = (org.eclipse.emf.codegen.ecore.genmodel.GenModel) genModelParameter;
-                        this._List = (java.lang.String) _ListParameter;
+						this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
+						this.genPackage = (org.eclipse.emf.codegen.ecore.genmodel.GenPackage) genPackageParameter;
+						this.genModel = (org.eclipse.emf.codegen.ecore.genmodel.GenModel) genModelParameter;
+						this._List = (java.lang.String) _ListParameter;
 
-                        if (preCondition()) {
-                            ctx.setNode(new Node.Container(currentNode, getClass()));
-                            orchestration(ctx);
-                        }
+						if (preCondition()) {
+							ctx.setNode(new Node.Container(currentNode, getClass()));
+							orchestration(ctx);
+						}
 
-                    }
-                }
-            }
-        }
-        ctx.setNode(currentNode);
-        if (ctx.useReporter()) {
-            ctx.getReporter().executionFinished(Node.flatten(ctx.getNode()), ctx);
-        }
+					}
+				}
+			}
+		}
+		ctx.setNode(currentNode);
+		if (ctx.useReporter()) {
+			ctx.getReporter().executionFinished(Node.flatten(ctx.getNode()), ctx);
+		}
 
-        stringBuffer.append(TEXT_2);
-        stringBuffer.append(TEXT_3);
-        return stringBuffer.toString();
-    }
+		stringBuffer.append(TEXT_2);
+		stringBuffer.append(TEXT_3);
+		return stringBuffer.toString();
+	}
 
-    public String orchestration(PatternContext ctx) throws Exception {
-        InternalPatternContext ictx = (InternalPatternContext) ctx;
+	public String orchestration(PatternContext ctx) throws Exception {
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
 
-        super.orchestration(new SuperOrchestrationContext(ictx));
+		super.orchestration(new SuperOrchestrationContext(ictx));
 
-        String loop = Node.flattenWithoutCallback(ictx.getNode());
-        if (ictx.useReporter()) {
-            Map<String, Object> parameterValues = new HashMap<String, Object>();
-            parameterValues.put("genClass", this.genClass);
-            parameterValues.put("genPackage", this.genPackage);
-            parameterValues.put("genModel", this.genModel);
-            parameterValues.put("_List", this._List);
-            String outputWithCallBack = Node.flatten(ictx.getNode());
-            ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
-            ;
-        }
-        return loop;
-    }
+		String loop = Node.flattenWithoutCallback(ictx.getNode());
+		if (ictx.useReporter()) {
+			Map<String, Object> parameterValues = new HashMap<String, Object>();
+			parameterValues.put("genClass", this.genClass);
+			parameterValues.put("genPackage", this.genPackage);
+			parameterValues.put("genModel", this.genModel);
+			parameterValues.put("_List", this._List);
+			String outputWithCallBack = Node.flatten(ictx.getNode());
+			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+			;
+		}
+		return loop;
+	}
 
-    public Map<String, Object> getParameters() {
-        final Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("genClass", this.genClass);
-        parameters.put("genPackage", this.genPackage);
-        parameters.put("genModel", this.genModel);
-        parameters.put("_List", this._List);
-        return parameters;
-    }
+	public Map<String, Object> getParameters() {
+		final Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("genClass", this.genClass);
+		parameters.put("genPackage", this.genPackage);
+		parameters.put("genModel", this.genModel);
+		parameters.put("_List", this._List);
+		return parameters;
+	}
 
-    protected void method_doGenerate(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
+	protected void method_doGenerate(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
 
-        stringBuffer.append(TEXT_1);
-        InternalPatternContext ictx = (InternalPatternContext) ctx;
-        new Node.DataLeaf(ictx.getNode(), getClass(), "org.eclipse.egf.model.pattern.impl.PatternMethodImpl@fc5092 (description: null) (name: doGenerate) (patternFilePath: platform:/plugin/org.eclipse.egf.portfolio.eclipse.build.egf/templates/pattern._fyInMKJGEeC7CagY5ZgTsg/method._fyInNaJGEeC7CagY5ZgTsg.pt)", stringBuffer.toString());
-    }
+		stringBuffer.append(TEXT_1);
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
+		new Node.DataLeaf(ictx.getNode(), getClass(), "doGenerate", stringBuffer.toString());
+	}
 
-    public boolean preCondition() throws Exception {
-        return "PublishStep".equals(genClass.getEcoreClass().getName());
-    }
+	public boolean preCondition() throws Exception {
+		return "PublishStep".equals(genClass.getEcoreClass().getName());
+	}
 }
