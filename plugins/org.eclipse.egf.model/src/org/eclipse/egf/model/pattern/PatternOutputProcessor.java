ec89024d89e9d915c97d0bc4b86b4a5bb2625205
Thomas Guiu <thomas.guiu@soyatec.com>
2011-10-10 11:41:24 +0000

    Add extension point for PatternOuputProcessor

---
+++ plugins/org.eclipse.egf.model/src/org/eclipse/egf/model/pattern/PatternOutputProcessor.java
@@ -0,0 +1,28 @@
+/**
+ * <copyright>
+ *
+ *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *      Thales Corporate Services S.A.S - initial API and implementation
+ * 
+ * </copyright>
+ */
+
+package org.eclipse.egf.model.pattern;
+
+/**
+ * @author Thomas Guiu
+ * 
+ */
+public interface PatternOutputProcessor {
+    String EXTENSION_ID = "org.eclipse.egf.model.trace.output.processor";
+
+    void execute(Node.Container root);
+
+    String getProcessorId();
+}
