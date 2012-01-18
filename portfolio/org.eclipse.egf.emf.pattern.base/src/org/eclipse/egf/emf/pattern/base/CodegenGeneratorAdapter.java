abd9757a7cbf06c0b338f93c47b0ed545e7e1ab9
Benoit Langlois <benoit.langlois@thalesgroup.com>
2012-01-18 10:45:36 +0000

    remove change who shouldn't have been commited

--- portfolio/org.eclipse.egf.emf.pattern.base/src/org/eclipse/egf/emf/pattern/base/CodegenGeneratorAdapter.java
+++ portfolio/org.eclipse.egf.emf.pattern.base/src/org/eclipse/egf/emf/pattern/base/CodegenGeneratorAdapter.java
@@ -80,7 +80,7 @@ public class CodegenGeneratorAdapter extends GenBaseGeneratorAdapter {
                 final IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(workspacePath.toFileString()));
                 try {
                     // use buf directly to avoid duplication of the array.
-                    if (!file.exists())
+                    if (file.exists())
                         file.setContents(new ByteArrayInputStream(buf, 0, count), IResource.KEEP_HISTORY | IResource.FORCE, null);
                     else
                         file.create(new ByteArrayInputStream(buf, 0, count), true, null);
