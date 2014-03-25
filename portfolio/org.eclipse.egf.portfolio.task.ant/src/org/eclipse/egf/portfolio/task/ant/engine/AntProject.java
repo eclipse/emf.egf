83e6538732b1aebc9dd5cf79ecbc4b50948a9254
Thomas Guiu <thomas.guiu@soyatec.com>
2014-03-25 15:42:33 +0000

    fix Bug 431129

--- portfolio/org.eclipse.egf.portfolio.task.ant/src/org/eclipse/egf/portfolio/task/ant/engine/AntProject.java
+++ portfolio/org.eclipse.egf.portfolio.task.ant/src/org/eclipse/egf/portfolio/task/ant/engine/AntProject.java
@@ -14,18 +14,33 @@
  */
 package org.eclipse.egf.portfolio.task.ant.engine;
 
+import java.io.IOException;
+import java.net.URL;
+
 import org.apache.tools.ant.AntClassLoader;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.Path;
+import org.eclipse.ant.core.AntCorePlugin;
+import org.eclipse.ant.core.AntCorePreferences;
+import org.eclipse.core.runtime.Platform;
 
 /**
  * @author xiaoru chen
  * 
  */
 public class AntProject extends Project {
-    @Override
-    public AntClassLoader createClassLoader(Path path) {
-        AntClassLoader antClassLoader = new AntClassLoader(getClass().getClassLoader(), this, path);
-        return antClassLoader;
-    }
+	@Override
+	public AntClassLoader createClassLoader(Path path) {
+		AntClassLoader antClassLoader = new AntClassLoader(getClass().getClassLoader(), this, path);
+		AntCorePreferences prefs = AntCorePlugin.getPlugin().getPreferences();
+		URL[] antClasspath = prefs.getURLs();
+		for (int i = 0; i < antClasspath.length; i++) {
+			try {
+				antClassLoader.addPathElement(Platform.asLocalURL(antClasspath[i]).getPath());
+			} catch (IOException e) {
+				continue;
+			}
+		}
+		return antClassLoader;
+	}
 }
