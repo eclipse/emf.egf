9026f33457ae295ecae2e76f92bc6d9b5a1cd876
Thomas Guiu <thomas.guiu@soyatec.com>
2018-04-17 20:30:55 +0200

    [533707] Error with "reverse codegen" action
    Bug: 533707
    
    Change-Id: I1dc88dc0f9b5366a7488d73d4b3f8cc9e0c15a51
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

--- tools/org.eclipse.egf.emf.pattern.codegen/src/org/eclipse/egf/emf/pattern/codegen/CodegenEngine.java
+++ tools/org.eclipse.egf.emf.pattern.codegen/src/org/eclipse/egf/emf/pattern/codegen/CodegenEngine.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
+ *  Copyright (c) 2009, 2018 Thales Corporate Services S.A.S.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -52,6 +52,7 @@ public class CodegenEngine extends SimpleEngine {
             StringBuffer buffer = new StringBuffer();
             buffer.append(IMPORTS);
             buffer.append("org.eclipse.egf.emf.pattern.base.* "); //$NON-NLS-1$
+            buffer.append("org.eclipse.emf.codegen.ecore.genmodel.util.GenModelUtil "); //$NON-NLS-1$
             buffer.append("org.eclipse.emf.codegen.ecore.genmodel.* "); //$NON-NLS-1$
             buffer.append("org.eclipse.emf.codegen.ecore.genmodel.impl.* "); //$NON-NLS-1$
             buffer.append("org.eclipse.emf.codegen.ecore.genmodel.generator.* "); //$NON-NLS-1$
