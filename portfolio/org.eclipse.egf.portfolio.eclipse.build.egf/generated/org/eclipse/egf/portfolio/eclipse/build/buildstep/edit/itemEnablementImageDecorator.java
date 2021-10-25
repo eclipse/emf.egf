daec942616d0e739ed6debc945e098905818217c
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-10-25 16:41:08 +0200

    #215 Update EGF sources
    Add missed EGF sources updates
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- portfolio/org.eclipse.egf.portfolio.eclipse.build.egf/generated/org/eclipse/egf/portfolio/eclipse/build/buildstep/edit/itemEnablementImageDecorator.java
+++ portfolio/org.eclipse.egf.portfolio.eclipse.build.egf/generated/org/eclipse/egf/portfolio/eclipse/build/buildstep/edit/itemEnablementImageDecorator.java
@@ -1,4 +1,4 @@
-//Generated with EGF 1.6.3.202110181143
+//Generated with EGF 1.6.3.202110221324
 package org.eclipse.egf.portfolio.eclipse.build.buildstep.edit;
 
 import org.eclipse.egf.common.helper.*;
@@ -66,6 +66,8 @@ public class itemEnablementImageDecorator
 		//this pattern can only be called by another (i.e. it's not an entry point in execution)
 		List<Object> _ListList = null;
 		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+		List<Object> hasSwitchList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
 
 		for (Object genClassParameter : genClassList) {
 			for (Object genPackageParameter : genPackageList) {
@@ -74,20 +76,23 @@ public class itemEnablementImageDecorator
 						for (Object forceDefaultCaseParameter : forceDefaultCaseList) {
 							for (Object indentDefaultCaseParameter : indentDefaultCaseList) {
 								for (Object _ListParameter : _ListList) {
+									for (Object hasSwitchParameter : hasSwitchList) {
 
-									this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
-									this.genPackage = (org.eclipse.emf.codegen.ecore.genmodel.GenPackage) genPackageParameter;
-									this.genModel = (org.eclipse.emf.codegen.ecore.genmodel.GenModel) genModelParameter;
-									this.isJDK50 = (java.lang.Boolean) isJDK50Parameter;
-									this.forceDefaultCase = (java.lang.Boolean) forceDefaultCaseParameter;
-									this.indentDefaultCase = (java.lang.String) indentDefaultCaseParameter;
-									this._List = (java.lang.String) _ListParameter;
+										this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
+										this.genPackage = (org.eclipse.emf.codegen.ecore.genmodel.GenPackage) genPackageParameter;
+										this.genModel = (org.eclipse.emf.codegen.ecore.genmodel.GenModel) genModelParameter;
+										this.isJDK50 = (java.lang.Boolean) isJDK50Parameter;
+										this.forceDefaultCase = (java.lang.Boolean) forceDefaultCaseParameter;
+										this.indentDefaultCase = (java.lang.String) indentDefaultCaseParameter;
+										this._List = (java.lang.String) _ListParameter;
+										this.hasSwitch = (java.lang.Boolean) hasSwitchParameter;
 
-									if (preCondition(ctx)) {
-										ctx.setNode(new Node.Container(currentNode, getClass()));
-										orchestration(ctx);
+										if (preCondition(ctx)) {
+											ctx.setNode(new Node.Container(currentNode, getClass()));
+											orchestration(ctx);
+										}
+
 									}
-
 								}
 							}
 						}
@@ -119,6 +124,7 @@ public class itemEnablementImageDecorator
 			parameterValues.put("forceDefaultCase", this.forceDefaultCase);
 			parameterValues.put("indentDefaultCase", this.indentDefaultCase);
 			parameterValues.put("_List", this._List);
+			parameterValues.put("hasSwitch", this.hasSwitch);
 			String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
 			String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
 			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
@@ -135,6 +141,7 @@ public class itemEnablementImageDecorator
 		parameters.put("forceDefaultCase", this.forceDefaultCase);
 		parameters.put("indentDefaultCase", this.indentDefaultCase);
 		parameters.put("_List", this._List);
+		parameters.put("hasSwitch", this.hasSwitch);
 		return parameters;
 	}
 
